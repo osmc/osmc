a0958295c5ada9572da138c169e02e90242f680b
Sam Nazarko <samnazarko@stmteam.com>
2014-06-08 11:46:40 +0100

    fix sed to remove archs

--- package/common.sh
+++ package/common.sh
@@ -5,7 +5,7 @@
 
 function fix_arch_ctl()
 {
-	sed '/Architecture/d' $1
+	sed '/Architecture/d' -i $1
 	test $(arch)x == i686x && echo "Architecture: i386" >> $1
 	test $(arch)x == armhfx && echo "Architecture: armhf" >> $1
 	test $(arch)x == amd64 && echo "Architecture: amd64" >> $1
