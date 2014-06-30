65703c198067edf923c090abc6cc63a114c9a202
Sam Nazarko <samnazarko@stmteam.com>
2014-06-30 01:20:39 +0100

    use SYSTEM_PKGS var instead

--- filesystem/common/funcs.sh
+++ filesystem/common/funcs.sh
@@ -7,8 +7,8 @@
 
 XBMC_MAN_PKGS="osmc-mediacenter"
 XBMC_MAN_PKGS_RBP="osmc-mediacenter-rbp"
-TOOLS_PKGS="wget apt-key"
-CHROOT_PKGS="base-files"
+SYSTEM_PKGS="wget apt-key base-files"
+CHROOT_PKGS="{SYSTEM_PKGS}"
 
 export CHROOT_PKGS
 export XBMC_MAN_PKGS
