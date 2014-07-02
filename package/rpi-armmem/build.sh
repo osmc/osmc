6f105278247d89c86bc549b270ef135eab4c79e8
Sam Nazarko <samnazarko@stmteam.com>
2014-07-02 01:03:04 +0100

    fix build verification for arrmem

--- package/rpi-armmem/build.sh
+++ package/rpi-armmem/build.sh
@@ -7,9 +7,9 @@ echo -e "Building armmem for Raspberry Pi"
 out=$(pwd)/files
 cd src
 make
+if [ $? != 0 ]; then echo "Error occured during build" && exit 1; fi
 strip_libs
 cp -ar libarmmem.so $out
 cp -ar libarmmem.a $out
-if [ $? != 0 ]; then echo "Error occured during build" && exit 1; fi
 cd ../
 dpkg -b files/ rpi-armmem.deb
