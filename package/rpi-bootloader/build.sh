3a135fce189e1eaef7cab2a181f24e7cc50b1a02
Sam Nazarko <samnazarko@stmteam.com>
2014-07-19 09:10:08 +0100

    add rpi-bootloader

---
+++ package/rpi-bootloader/build.sh
@@ -0,0 +1,23 @@
+# (c) 2014 Sam Nazarko
+# email@samnazarko.co.uk
+
+#!/bin/bash
+
+echo -e "Building package rpi-bootloader"
+BOOT="files/boot"
+FWFILES="LICENCE.broadcom
+start_x.elf
+fixup_x.dat
+bootcode.bin"
+
+if [ -f "${BOOT}" ]; then rm -rf "${BOOT}"
+mkdir -p "${BOOT}"
+
+for file in "${FWFILES}"
+do
+	wget --no-check-certificate https://raw.githubusercontent.com/raspberrypi/firmware/master/boot/"${file}" -O "${BOOT"}/"${file}"
+	if [ $? != 0 ]; then echo "Download failed" && exit 1; fi
+done
+
+dpkg -b files/ osmc-rpi-bootloader
+
