e5dd3c0c11955b038b813a446c9e08fa546b2443
Sam Nazarko <samnazarko@stmteam.com>
2014-07-19 01:46:56 +0100

    make /home first fix sudoers

--- filesystem/common/funcs.sh
+++ filesystem/common/funcs.sh
@@ -14,13 +14,14 @@ CHROOT_PKGS="${INIT_PKGS} ${SYSTEM_PKGS}"
 function setup_osmc_user()
 {
 	# Sets user and password to 'osmc'
+	mkdir ${1}/home/osmc
 	chroot ${1} useradd -p \$1\$P.ZH6EFu\$L08/1ZYI6FdHu3aw0us.u0 osmc -k /etc/skel -d /home/osmc -m -s /bin/bash
 	# Locks root
 	chroot ${1} passwd -l root
 	# Makes 'osmc' username and password never expire
 	chroot ${1} chage -I -1 -m 0 -M 99999 -E -1 osmc
 	# Adds 'osmc' to sudoers with no password prompt
-	echo "osmc     ALL= NOPASSWD: ALL" ${1}>/etc/sudoers.d/osmc
+	echo "osmc     ALL= NOPASSWD: ALL" >${1}/etc/sudoers.d/osmc
 	chmod 0440 ${1}/etc/sudoers.d/osmc
 }
 
