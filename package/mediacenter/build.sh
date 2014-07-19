66e68677403a485b9600f3aa82260723fb69d730
Sam Nazarko <samnazarko@stmteam.com>
2014-07-19 04:57:49 +0100

    csv dependencies

--- package/mediacenter/build.sh
+++ package/mediacenter/build.sh
@@ -41,6 +41,6 @@ if [ $? != 0 ]; then echo -e "Build failed!" && exit 1; fi
 make install DESTDIR=${out}
 cd ../
 # Set deps:
-test "$1" == atv && echo "Depends: libssh-4 libavahi-client3 python libsmbclient libpulse0 libtiff5 libjpeg8 libsqlite3-0 libflac8 libtinyxml2.6.2 libmicrohttpd10 libjasper1 libxrandr2 libyajl2 libmysqlclient18 libasound2 libxml2 libxslt1.1 libpng12-0 libsamplerate0 libtag1-vanilla libsdl-image1.2 libglew1.10 libfribidi0 liblzo2-2 libcdio13 libpcrecpp0" >> files/DEBIAN/control
+test "$1" == atv && echo "Depends: libssh-4,libavahi-client3,python,libsmbclient,libpulse0,libtiff5,libjpeg8,libsqlite3-0,libflac8,libtinyxml2.6.2,libmicrohttpd10,libjasper1,libxrandr2,libyajl2,libmysqlclient18,libasound2,libxml2,libxslt1.1,libpng12-0,libsamplerate0,libtag1-vanilla,libsdl-image1.2,libglew1.10,libfribidi0,liblzo2-2,libcdio13,libpcrecpp0"
 fix_arch_ctl "files/DEBIAN/control"
 dpkg -b files/ osmc-mediacenter.deb
