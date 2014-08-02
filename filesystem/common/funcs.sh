f9288bcb93638628b7bbe7d9233dbf2d10b870f2
Sam Nazarko <samnazarko@stmteam.com>
2014-08-02 03:09:20 +0100

    not sure we need consolekit; upower will be a subdep

--- filesystem/common/funcs.sh
+++ filesystem/common/funcs.sh
@@ -6,7 +6,7 @@
 . ../../scripts/common.sh
 
 INIT_PKGS="systemd systemd-sysv"
-SYSTEM_PKGS="base-files apt-utils openssh-server sudo module-init-tools connman net-tools ntp consolekit locales upower"
+SYSTEM_PKGS="base-files apt-utils openssh-server sudo module-init-tools connman net-tools ntp locales"
 CHROOT_PKGS="${INIT_PKGS} ${SYSTEM_PKGS}"
 
 function setup_osmc_user()
