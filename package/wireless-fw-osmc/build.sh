3f0cd1f42372b1e0b046bf9258fea479054754cb
Sam Nazarko <samnazarko@stmteam.com>
2014-08-04 02:21:47 +0100

    Add wireless-fw package to allow bundling of wireless firmware

---
+++ package/wireless-fw-osmc/build.sh
@@ -0,0 +1,31 @@
+# (c) 2014 Sam Nazarko
+# email@samnazarko.co.uk
+
+#!/bin/bash
+
+function install_fw()
+{
+	DEB_BASE="http://ftp.debian.org/debian/pool/non-free/f/firmware-nonfree/"
+	if [ -d $WD ]; then rm -rf $WD > /dev/null 2>&1; fi
+	WD="/tmp/fw-dump"
+	mkdir -p $WD
+	pushd $WD
+	wget ${DEB_BASE}$1
+	if [ $? != 0 ]; then echo "Firmware download failed" && exit 1; fi
+	dpkg -x ${1} .
+	popd
+	cp -ar $WD/lib/firmware/* files/lib/firmware
+}
+
+echo -e "Building package WiFi firmware"
+
+if [ -d files/lib/firmware ]; then echo "Removing old firmware" && rm -rf files/lib/firmware > /dev/null 2>&1; fi
+
+RALINK_DEB="firmware-ralink_0.43_all.deb"
+REALTEK_DEB="firmware-realtek_0.43_all.deb"
+
+mkdir -p files/lib/firmware
+install_fw ${RALINK_DEB}
+install_fw ${REALTEK_DEB}
+
+dpkg -b files/ osmc-wifi-firmware.deb
