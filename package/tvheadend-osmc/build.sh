bd220345fdac467b4de1e8a99fd19b650b0a7973
Sam Nazarko <samnazarko@stmteam.com>
2014-08-07 09:22:40 +0100

    [tvheadend] add tvheadend OSMC package, based on TVH 3.9, with dependency on dvb-fw-osmc

---
+++ package/tvheadend-osmc/build.sh
@@ -0,0 +1,28 @@
+# (c) 2014 Sam Nazarko
+# email@samnazarko.co.uk
+
+#!/bin/bash
+
+. ../common.sh
+
+REPOSITORY="https://github.com/tvheadend/tvheadend"
+TAG="v3.9"
+
+echo -e "Building TVHeadend"
+out=$(pwd)/files
+if [ -d files/usr ]; then rm -rf files/usr; fi
+if [ -d src/ ]; then rm -rf src; fi
+sed '/Package/d' -i files/DEBIAN/control
+test $1 == gen && echo "Package: tvheadend-osmc-app" >> files/DEBIAN/control
+test $1 == rbp && echo "Package: rbp-tvheadend-osmc-app" >> files/DEBIAN/control
+git clone $REPOSITORY src/
+cd src
+git checkout $TAG
+./configure --prefix=/usr
+$BUILD
+make install DESTDIR=${out}
+if [ $? != 0 ]; then echo "Error occured during build" && exit 1; fi
+strip_files "${out}"
+cd ../
+fix_arch_ctl "files/DEBIAN/control"
+dpkg -b files/ tvheadend-osmc.deb
