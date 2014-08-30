aa349a985d5a8be588c72ad5f5bd298f9790cf03
Sam Nazarko <email@samnazarko.co.uk>
2014-08-30 22:32:32 +0100

    [installer] [host] add Linux build scripts and Makefile

---
+++ installer/host/make_host_linux.sh
@@ -0,0 +1,31 @@
+# (c) 2014 Sam Nazarko
+# email@samnazarko.co.uk
+
+#!/bin/bash
+echo Building host installer for Linux
+TARGET="qt_host_installer"
+pushd ${TARGET}
+if [ -f Makefile ]; then echo "Cleaning" && make clean; fi
+echo Building installer
+qmake
+make
+strip ${TARGET}
+echo Packaging installer
+popd
+INSTALL="install"
+if [ -d ${INSTALL} ]; then echo "Cleaning old install directory " && rm -rf ${INSTALL}; fi
+mkdir -p ${INSTALL}
+mkdir -p ${INSTALL}/usr/bin/osmc
+mkdir -p ${INSTALL}/usr/share/applications
+cp ${TARGET}/${TARGET} ${INSTALL}/usr/bin/osmc/
+cp ${TARGET}/*.qm ${INSTALL}/usr/bin/osmc/ > /dev/null 2>&1
+cp ${TARGET}/osmcinstaller.desktop ${INSTALL}/usr/share/applications/
+cp ${TARGET}/icon.png ${INSTALL}/usr/bin/osmc/icon.png
+cp -ar DEBIAN/ ${INSTALL}/
+VERSION=$(cat ${TARGET}/${TARGET}.pro | grep VERSION | tail -n 1 | awk {'print $3'})
+echo "Version: ${VERSION}" >> ${INSTALL}/DEBIAN/control
+test $(arch)x == x86_64x && echo "Architecture: amd64" >> ${INSTALL}/DEBIAN/control
+test $(arch)x == i686 && echo "Architecture: i686" >> ${INSTALL}/DEBIAN/control
+dpkg -b ${INSTALL} osmc-installer.deb
+rm -rf ${INSTALL}
+echo Build complete
