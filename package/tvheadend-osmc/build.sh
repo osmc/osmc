71070b62afa3c039444d744f9f083e1fc5b0bf6f
Sam Nazarko <email@samnazarko.co.uk>
2014-08-31 02:00:15 +0100

    [package] [tvheadend] patch way source is pulled in tvheadend

--- package/tvheadend-osmc/build.sh
+++ package/tvheadend-osmc/build.sh
@@ -5,17 +5,14 @@
 
 . ../common.sh
 
-REPOSITORY="https://github.com/tvheadend/tvheadend"
-TAG="v3.9"
-
 echo -e "Building TVHeadend"
 out=$(pwd)/files
 sed '/Package/d' -i files/DEBIAN/control
 test $1 == gen && echo "Package: tvheadend-osmc-app" >> files/DEBIAN/control
 test $1 == rbp && echo "Package: rbp-tvheadend-osmc-app" >> files/DEBIAN/control
-git clone $REPOSITORY src/
+pull_source "https://github.com/tvheadend/tvheadend" "$(pwd)/src"
 cd src
-git checkout $TAG
+git checkout v3.9
 ./configure --prefix=/usr
 $BUILD
 make install DESTDIR=${out}
