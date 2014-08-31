e00364864d5219af23e21c6d31d4598bd82e9a15
Sam Nazarko <email@samnazarko.co.uk>
2014-08-31 01:36:50 +0100

    [package] [libshairplay] fix Makefile; use upstream source; don't need Depends: in Debian control files (generated at build time)

--- package/libshairplay-osmc/build.sh
+++ package/libshairplay-osmc/build.sh
@@ -13,15 +13,15 @@ sed '/Package/d' -i files-dev/DEBIAN/control
 sed '/Depends/d' -i files-dev/DEBIAN/control
 test $1 == gen && echo "Package: libshairplay-osmc" >> files/DEBIAN/control && echo "Package: libshairplaydev-osmc" >> files-dev/DEBIAN/control && echo "Depends: libshairplay-osmc" >> files-dev/DEBIAN/control
 test $1 == rbp && echo "Package: rbp-libshairplay-osmc" >> files/DEBIAN/control && echo "Package: rbp-libshairplaydev-osmc" >> files-dev/DEBIAN/control && echo "Depends: rbp-libshairplay-osmc" >> files-dev/DEBIAN/control
-cd src
-make clean
+pull_source "http://psg.mtu.edu/pub/xbmc/build-deps/sources/shairplay-139d5ef.tar.bz2" "$(pwd)/src"
+cd src/shairplay*
 ./autogen.sh
 ./configure --prefix=/usr
 $BUILD
 make install DESTDIR=${out}
 if [ $? != 0 ]; then echo "Error occured during build" && exit 1; fi
 strip_files "${out}"
-cd ../
+cd ../../
 mkdir -p files-dev/usr
 mv files/usr/include  files-dev/usr/
 fix_arch_ctl "files/DEBIAN/control"
