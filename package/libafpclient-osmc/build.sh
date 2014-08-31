56ee0ff42b22e4baa088d9357914cb73f0b22f9b
Sam Nazarko <email@samnazarko.co.uk>
2014-08-31 01:09:19 +0100

    [package] [libafpclient] use upstream source

--- package/libafpclient-osmc/build.sh
+++ package/libafpclient-osmc/build.sh
@@ -13,15 +13,16 @@ sed '/Package/d' -i files-dev/DEBIAN/control
 sed '/Depends/d' -i files-dev/DEBIAN/control
 test "$1" == gen && echo "Package: libafpclient-osmc" >> files/DEBIAN/control && echo "Package: libafpclientdev-osmc" >> files-dev/DEBIAN/control && echo "Depends: libafpclient-osmc" >> files-dev/DEBIAN/control
 test "$1" == rbp && echo "Package: rbp-libafpclient-osmc" >> files/DEBIAN/control && echo "Package: rbp-libafpclientdev-osmc" >> files-dev/DEBIAN/control && echo "Depends: rbp-libafpclient-osmc" >> files-dev/DEBIAN/control
-cd src
-make clean
+pull_source "https://github.com/Sky-git/afpfs-ng-fork" "$(pwd)/src"
+cd src/afpfs-ng
+chmod +x configure
 ./configure --prefix=/usr
 $BUILD
 make install DESTDIR=${out}
 # We always error
 #if [ $? != 0 ]; then echo "Error occured during build" && exit 1; fi
 strip_files "${out}"
-cd ../
+cd ../../
 mkdir -p files-dev/usr
 mv files/usr/include  files-dev/usr/
 fix_arch_ctl "files/DEBIAN/control"
--- package/libafpclient-osmc/src/cmdline/cmdline_testafp.h
+++
@@ -1,4 +0,0 @@
-#ifndef __CMDLINE_TESTAFP_H_
-int test_urls(void);
-#endif
-
