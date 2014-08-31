bce862a19e0feb64eb13284c772a3f3e9be120af
Sam Nazarko <email@samnazarko.co.uk>
2014-08-31 08:39:57 +0100

    [installer] [host] fix w32 build mess for good!

--- installer/host/build_win_with_mingw.cmd
+++ installer/host/build_win_with_mingw.cmd
@@ -3,5 +3,5 @@ REM email@samnazarko.co.uk
 
 REM Allows us to use standard Makefile and shell script to build. 
 
-set_mingw_path.cmd
+PATH=C:\MinGW\msys\1.0\bin;%PATH%
 make win
--- installer/host/set_mingw_path.cmd
+++
@@ -1,6 +0,0 @@
-REM (c) 2014 Sam Nazarko
-REM email@samnazarko.co.uk
-
-REM Allows us to use standard Makefile and shell script to build. 
-
-PATH=C:\MinGW\msys\1.0\bin;%PATH%
