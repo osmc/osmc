0560bf96c79137435b22a9251a00e3f362a636cc
Sam Nazarko <email@samnazarko.co.uk>
2014-09-02 04:05:33 +0100

    [installer] [host] fix architecture in DEBIAN control write arch in package name

--- installer/host/make_host_linux.sh
+++ installer/host/make_host_linux.sh
@@ -25,7 +25,7 @@ cp -ar DEBIAN/ ${INSTALL}/
 VERSION=$(cat ${TARGET}/${TARGET}.pro | grep VERSION | tail -n 1 | awk {'print $3'})
 echo "Version: ${VERSION}" >> ${INSTALL}/DEBIAN/control
 test $(arch)x == x86_64x && echo "Architecture: amd64" >> ${INSTALL}/DEBIAN/control
-test $(arch)x == i686 && echo "Architecture: i686" >> ${INSTALL}/DEBIAN/control
-dpkg -b ${INSTALL} osmc-installer.deb
+test $(arch)x == i686x && echo "Architecture: i686" >> ${INSTALL}/DEBIAN/control
+dpkg -b ${INSTALL} osmc-installer-$(arch).deb
 rm -rf ${INSTALL}
 echo Build complete
