6960b1cd1b3d20fed2e2cb9bd215b3249dacc1e0
Sam Nazarko <email@samnazarko.co.uk>
2014-09-06 23:25:33 +0100

    [installer] [host] add osx build script

---
+++ installer/host/make_host_osx.sh
@@ -0,0 +1,17 @@
+# (c) 2014 Sam Nazarko
+# email@samnazarko.co.uk
+
+#!/bin/bash
+echo Building host installer for OS X
+TARGET="qt_host_installer"
+pushd ${TARGET}
+if [ -f Makefile ]; then echo "Cleaning" && make clean; fi
+echo Building installer
+qmake
+make
+if [ $? != 0 ]; then echo "Build failed" && exit 1; fi
+macdeployqt ${TARGET}.app -dmg --no-plugins
+echo Packaging installer
+popd
+mv ${TARGET}/${TARGET}.dmg osmc-installer.dmg
+echo Build complete
