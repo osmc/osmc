e8b29cf28bed8991173b0636c771ba9cda0ec27b
Sam Nazarko <email@samnazarko.co.uk>
2014-09-11 01:31:52 +0100

    [package] [skin] add functional dependencies for the skin

--- package/mediacenter-skin/build.sh
+++ package/mediacenter-skin/build.sh
@@ -2,10 +2,14 @@
 # email@samnazarko.co.uk
 
 #!/bin/bash
+. ../common.sh
 
 echo "Building OSMC skin tarball"
 if [ ! -f /usr/bin/zip ]; then echo "Installing zip tools" && apt-get update && apt-get -y install zip; fi
 pushd src
+echo "Downloading dependencies"
+pull_source "https://github.com/unfledged/script.skinshortcuts" "$(pwd)/script.skinshortcuts"
+pull_source "https://github.com/BigNoid/service.library.data.provider" "$(pwd)/service.library.data.provider.git"
 zip -r skin.osmc.zip *
 popd
 mv src/skin.osmc.zip .
