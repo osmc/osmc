8568e568b2dc602f35d7953d5a01ff063785e298
Sam Nazarko <email@samnazarko.co.uk>
2014-09-11 01:39:32 +0100

    [package] [skin] fix directory name for library addon provider

--- package/mediacenter-skin/build.sh
+++ package/mediacenter-skin/build.sh
@@ -9,7 +9,7 @@ if [ ! -f /usr/bin/zip ]; then echo "Installing zip tools" && apt-get update && 
 pushd src
 echo "Downloading dependencies"
 pull_source "https://github.com/unfledged/script.skinshortcuts" "$(pwd)/script.skinshortcuts"
-pull_source "https://github.com/BigNoid/service.library.data.provider" "$(pwd)/service.library.data.provider.git"
+pull_source "https://github.com/BigNoid/service.library.data.provider" "$(pwd)/service.library.data.provider"
 zip -r skin.osmc.zip *
 popd
 mv src/skin.osmc.zip .
