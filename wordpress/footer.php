c4e5fd6d2da42e1476f272aee9a327a1a497c557
Mark Theis Madsen <marktheism@gmail.com>
2014-09-23 04:04:09 +0200

    new logo, favicons, footer info style

--- wordpress/footer.php
+++ wordpress/footer.php
@@ -24,55 +24,62 @@
 					</div>
 					</div>
 				</div>
-				<div style="clear-both"></div>
-				<div class="copyright">
-				<img src="<?php echo get_template_directory_uri(); ?>/library/images/logo-l.png">
-				</div>
-                             <?php
-                                        /* base footer */
-                                        $LOGO_STRING = "Logo by Simon Brunton";
-                                        $DESIGN_STRING = "Designed by Mark Theis Madsen";
-					$lang = array_shift(explode(".",$_SERVER['HTTP_HOST']));
-					switch ($lang)
-					{
-						case "fr":
-						$LOGO_STRING = "Logo réalisé par Simon Brunton";
-						$DESIGN_STRING = "Design réalisé par Mark Theis Madsen";
-						break;
-						case "da":
-						$LOGO_STRING = "Logo af Simon Brunton";
-						$DESIGN_STRING = "Designet af Mark Theis Madsen";
-						break;
-						case "sv":
-						$LOGO_STRING = "Logga av Simon Brunton";
-						$DESIGN_STRING = "Designad av Mark Theis Madsen";
-						break;
-						case "de":
-						$LOGO_STRING = "Logo von Simon Brunton";
-						$DESIGN_STRING = "Design von Simon Brunton";
-						break;
-						case "nl":
-						$LOGO_STRING = "Logo ontworpen door Simon Brunton";
-						$DESIGN_STRING = "Ontworpen door Mark Theis Madsen";
-						break;
-						case "es":
-						$LOGO_STRING = "Logo por Simon Brunton";
-						$DESIGN_STRING = "Diseño por Mark Theis Madsen";
-						break;
-						case "ru":
-						$LOGO_STRING = "Логотип - Саймон Брантон";
-						$DESIGN_STRING = "Дизайн от Марка Т. Мэдсена";
-						break;
-					}
-                                ?>
-				<br>
-                                <a href="http://simonbrunton.com" target="_blank" class="designed"><?echo $LOGO_STRING;?></a><br>
-                                <a href="http://markthe.is" target="_blank" class="designed"><?echo $DESIGN_STRING;?></a></br>
-				<p class="designed">(c) 2014 OSMC </p>
-				<br>
-				<a href="http://osmc.tv/about/corporate" class="designed">Corporate</a></br>
-				<br>
-				<a href="http://status.osmc.io" class="designed">System Status</a></br>
+				<div class="footerinfo pure-g-r">
+                  <div class="middle pure-u-1">
+                    <img src="<?php echo get_template_directory_uri(); ?>/library/images/logo-l.png">
+                      <?php
+                      /* base footer */
+                      $LOGO_STRING = "Logo by Simon Brunton";
+                      $DESIGN_STRING = "Designed by Mark Theis Madsen";
+                      $lang = array_shift(explode(".",$_SERVER['HTTP_HOST']));
+                      switch ($lang)
+                      {
+                          case "fr":
+                          $LOGO_STRING = "Logo réalisé par Simon Brunton";
+                          $DESIGN_STRING = "Design réalisé par Mark Theis Madsen";
+                          break;
+                          case "da":
+                          $LOGO_STRING = "Logo af Simon Brunton";
+                          $DESIGN_STRING = "Designet af Mark Theis Madsen";
+                          break;
+                          case "sv":
+                          $LOGO_STRING = "Logga av Simon Brunton";
+                          $DESIGN_STRING = "Designad av Mark Theis Madsen";
+                          break;
+                          case "de":
+                          $LOGO_STRING = "Logo von Simon Brunton";
+                          $DESIGN_STRING = "Design von Simon Brunton";
+                          break;
+                          case "nl":
+                          $LOGO_STRING = "Logo ontworpen door Simon Brunton";
+                          $DESIGN_STRING = "Ontworpen door Mark Theis Madsen";
+                          break;
+                          case "es":
+                          $LOGO_STRING = "Logo por Simon Brunton";
+                          $DESIGN_STRING = "Diseño por Mark Theis Madsen";
+                          break;
+                          case "ru":
+                          $LOGO_STRING = "Логотип - Саймон Брантон";
+                          $DESIGN_STRING = "Дизайн от Марка Т. Мэдсена";
+                          break;
+                      }
+                                  ?>
+                    <br>
+                    <a href="http://simonbrunton.com" target="_blank" class="designed"><?echo $LOGO_STRING;?></a>
+                    <br>
+                    <a href="http://markthe.is" target="_blank" class="designed"><?echo $DESIGN_STRING;?></a>
+                    <br><br>
+                    <p>&#169; 2014 OSMC </p>
+                    
+                    <div class="corporate">
+                      <a href="http://osmc.tv/about/corporate">Corporate</a>
+                    </div>
+                    <div class="status">
+                      <a target="_blank" href="http://status.osmc.io">System Status</a>
+                    </div>
+                  </div>
+                </div>
+                
 				</div> <?php // end #inner-footer ?>
 
 			</footer> <?php // end footer ?>
--- wordpress/header.php
+++ wordpress/header.php
@@ -20,24 +20,27 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
 		<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/library/images/favicons/favicon.ico">
-    <link rel="apple-touch-icon" sizes="57x57" href="<?php echo get_template_directory_uri(); ?>/library/images/favicons/apple-touch-icon-57x57.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_template_directory_uri(); ?>/library/images/favicons/apple-touch-icon-114x114.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="<?php echo get_template_directory_uri(); ?>/library/images/favicons/apple-touch-icon-72x72.png">
-    <link rel="apple-touch-icon" sizes="144x144" href="<?php echo get_template_directory_uri(); ?>/library/images/favicons/apple-touch-icon-144x144.png">
-    <link rel="apple-touch-icon" sizes="60x60" href="<?php echo get_template_directory_uri(); ?>/library/images/favicons/apple-touch-icon-60x60.png">
-    <link rel="apple-touch-icon" sizes="120x120" href="<?php echo get_template_directory_uri(); ?>/library/images/favicons/apple-touch-icon-120x120.png">
-    <link rel="apple-touch-icon" sizes="76x76" href="<?php echo get_template_directory_uri(); ?>/library/images/favicons/apple-touch-icon-76x76.png">
-    <link rel="apple-touch-icon" sizes="152x152" href="<?php echo get_template_directory_uri(); ?>/library/images/favicons/apple-touch-icon-152x152.png">
-    <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/library/images/favicons/favicon-160x160.png" sizes="160x160">
-    <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/library/images/favicons/favicon-96x96.png" sizes="96x96">
-    <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/library/images/favicons/favicon-16x16.png" sizes="16x16">
-    <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/library/images/favicons/favicon-32x32.png" sizes="32x32">
-    <meta name="msapplication-TileColor" content="#f0f0f0">
-    <meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/library/images/favicons/mstile-144x144.png">
-    <meta name="msapplication-square70x70logo" content="<?php echo get_template_directory_uri(); ?>/library/images/favicons/mstile-70x70.png">
-    <meta name="msapplication-square150x150logo" content="<?php echo get_template_directory_uri(); ?>/library/images/favicons/mstile-150x150.png">
-    <meta name="msapplication-square310x310logo" content="<?php echo get_template_directory_uri(); ?>/library/images/favicons/mstile-310x310.png">
-    <meta name="msapplication-wide310x150logo" content="<?php echo get_template_directory_uri(); ?>/library/images/favicons/mstile-310x150.png">
+        <link rel="apple-touch-icon" sizes="57x57" href="<?php echo get_template_directory_uri(); ?>/library/images/favicons/apple-touch-icon-57x57.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_template_directory_uri(); ?>/library/images/favicons/apple-touch-icon-114x114.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="<?php echo get_template_directory_uri(); ?>/library/images/favicons/apple-touch-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="144x144" href="<?php echo get_template_directory_uri(); ?>/library/images/favicons/apple-touch-icon-144x144.png">
+        <link rel="apple-touch-icon" sizes="60x60" href="<?php echo get_template_directory_uri(); ?>/library/images/favicons/apple-touch-icon-60x60.png">
+        <link rel="apple-touch-icon" sizes="120x120" href="<?php echo get_template_directory_uri(); ?>/library/images/favicons/apple-touch-icon-120x120.png">
+        <link rel="apple-touch-icon" sizes="76x76" href="<?php echo get_template_directory_uri(); ?>/library/images/favicons/apple-touch-icon-76x76.png">
+        <link rel="apple-touch-icon" sizes="152x152" href="<?php echo get_template_directory_uri(); ?>/library/images/favicons/apple-touch-icon-152x152.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri(); ?>/library/images/favicons/apple-touch-icon-180x180.png">
+        <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/library/images/favicons/favicon-192x192.png" sizes="192x192">
+        <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/library/images/favicons/favicon-160x160.png" sizes="160x160">
+        <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/library/images/favicons/favicon-96x96.png" sizes="96x96">
+        <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/library/images/favicons/favicon-16x16.png" sizes="16x16">
+        <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/library/images/favicons/favicon-32x32.png" sizes="32x32">
+        <meta name="msapplication-TileColor" content="#f0f0f0">
+        <meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/library/images/favicons/mstile-144x144.png">
+        <meta name="msapplication-square70x70logo" content="<?php echo get_template_directory_uri(); ?>/library/images/favicons/mstile-70x70.png">
+        <meta name="msapplication-square150x150logo" content="<?php echo get_template_directory_uri(); ?>/library/images/favicons/mstile-150x150.png">
+        <meta name="msapplication-square310x310logo" content="<?php echo get_template_directory_uri(); ?>/library/images/favicons/mstile-310x310.png">
+        <meta name="msapplication-wide310x150logo" content="<?php echo get_template_directory_uri(); ?>/library/images/favicons/mstile-310x150.png">
+        
 		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 		<?php wp_head(); ?>
 
