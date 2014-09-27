b2ed4397945639b07749deff879dd6fedf7ee57f
Sam Segers <sam.sgrs@gmail.com>
2014-09-27 16:58:18 +0200

    Fix moc not getting the #if defined right
    Slots were not configured in moc when there are inside #if statements
    Fix it with adding the slot on all platforms, but make it do nothing

--- installer/host/qt_host_installer/mainwindow.h
+++ installer/host/qt_host_installer/mainwindow.h
@@ -31,9 +31,7 @@ public slots:
     void setNetworkInitial(bool useWireless, bool advanced);
     void setNetworkAdvanced(QString ip, QString mask, QString gw, QString dns1, QString dns2);
     void setWiFiConfiguration(QString ssid, int key_type, QString key_value);
-    #if defined(Q_OS_LINUX) || defined(Q_OS_MAC)
     void selectNixDevice(NixDiskDevice *nd);
-    #endif
     void acceptLicense();
     void completeDownload(QString fileName);
     void showUpdate();
