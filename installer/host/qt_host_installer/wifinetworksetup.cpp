ccf3d607c9660209666c4972d3f434a299e2b275
srmo <srm@schakalacka.org>
2014-10-05 01:40:32 +0200

    [installer][host] fix WIFI setup validation

--- installer/host/qt_host_installer/wifinetworksetup.cpp
+++ installer/host/qt_host_installer/wifinetworksetup.cpp
@@ -20,10 +20,36 @@ WiFiNetworkSetup::~WiFiNetworkSetup()
 
 void WiFiNetworkSetup::on_networkoptionsnextButton_clicked()
 {
-    /* TODO: Add validation */
+    if (ui->keySelectionBox->currentText().startsWith("---"))
+    {
+        utils::displayError(tr("Networktype missing"), tr("You need to select the type of network encryption!"), false);
+        return;
+    }
+
     int key_type;
     key_type = (ui->keySelectionBox->currentText() == tr("Open Network")) ? utils::WIRELESS_ENCRYPTION_NONE : (ui->keySelectionBox->currentText() == tr("WPA/WPA2 PSK")) ? utils::WIRELESS_ENCRYPTION_WPAPSK : utils::WIRELESS_ENCRYPTION_WEP;
-    emit wifiNetworkConfigured(ui->ssidnamelineEdit->text(), key_type, ui->keylineEdit->text());
+
+    QString ssid = ui->ssidnamelineEdit->text();
+    QString key = ui->keylineEdit->text();
+
+    if (ssid.isEmpty())
+    {
+        utils::displayError(tr("Missing SSID"), tr("You need to provide a SSID!"), false);
+        return;
+    }
+
+    switch (key_type)
+    {
+    case (utils::WIRELESS_ENCRYPTION_WEP):
+    case (utils::WIRELESS_ENCRYPTION_WPAPSK):
+        if (key.isEmpty())
+        {
+            utils::displayError(tr("Missing Key"), tr("You need to provide a key!"), false);
+            return;
+        }
+    }
+
+    emit wifiNetworkConfigured(ssid, key_type, key);
 }
 
 void WiFiNetworkSetup::on_keySelectionBox_currentIndexChanged(const QString &arg1)
