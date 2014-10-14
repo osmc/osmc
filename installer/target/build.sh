adf5042615d93b7375d562a5b7e0deba564bc059
crpb <crpb@users.noreply.github.com>
2014-10-14 22:35:48 +0200

    less annoying
    Get rid of recurring apt-get update and the download of the buildroot.

--- installer/target/build.sh
+++ installer/target/build.sh
@@ -6,11 +6,17 @@
 echo -e "Building target side installer"
 BUILDROOT_VERSION="2014.05"
 echo -e "Installing dependencies"
-apt-get update
+#Check if we updated APT-Cache in the Last half hour
+if [ `stat --format=%Y /var/lib/apt/listchanges.db` -le $(( `date +%s` - 1800 )) ]; then 
+    apt-get update
+fi
 apt-get -y install build-essential rsync texinfo libncurses-dev whois unzip git subversion bc
-wget http://buildroot.uclibc.org/downloads/buildroot-${BUILDROOT_VERSION}.tar.gz -O buildroot.tar.gz
+#Check if already downloaded the buildroot
+if [ ! -e "buildroot-${BUILDROOT_VERSION}.tar.gz" ] ; then
+ wget http://buildroot.uclibc.org/downloads/buildroot-${BUILDROOT_VERSION}.tar.gz -O buildroot-${BUILDROOT_VERSION}.tar.gz
+fi
 if [ $? != 0 ]; then echo "Download failed" && exit 1; fi
-tar -xzvf buildroot.tar.gz
+tar -xzvf buildroot-${BUILDROOT_VERSION}.tar.gz
 if [ $? != 0 ]; then echo "Extraction failed" && exit 1; fi
 cp patches/* buildroot-${BUILDROOT_VERSION}
 pushd buildroot-${BUILDROOT_VERSION}
