50a45a1fc83ff9a9f31e5e6794a7311b424b9039
Sam Nazarko <email@samnazarko.co.uk>
2014-11-04 13:11:44 +0000

    [package] [mediacenter] remove old method of patching mediacenter package

--- package/mediacenter/patch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-PATCHES=$(find ../patches/ -name "${1}-*.patch" -printf '%P\n')
-for PATCH in $PATCHES
-do
-	cp ../patches/$PATCH .
-	echo Applying patch $PATCH
-	patch -p1 < $PATCH
-	rm $PATCH
-done
