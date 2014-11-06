0e47b7fd613867a632a9fb7e4e0e12643804ec2e
Sam Nazarko <email@samnazarko.co.uk>
2014-11-06 17:07:18 +0000

    [installer] [target] clean img.gz on make clean; make sure we download the latest filesystem for the target; add support for creating installer image. Add kpartx and dosfstools dependencies

--- installer/target/build.sh
+++ installer/target/build.sh
@@ -16,7 +16,9 @@ rsync
 texinfo
 libncurses5-dev
 whois
-bc"
+bc
+kpartx
+dosfstools"
 for package in $packages
 do
 	install_package $package
@@ -33,10 +35,39 @@ make
 if [ $? != 0 ]; then echo "Build failed" && exit 1; fi
 popd
 pushd buildroot-${BUILDROOT_VERSION}/output/images
-if [ $1 == rbp ]
+echo -e "Downloading latest filesystem"
+date=$(date +%Y%m%d)
+count=150
+while [ count > 0 ]; do wget --spider -q ${DOWNLOAD_URL}/filesystems/osmc-${1}-filesystem-${date}.tar.xz
+       if [ "$?" -eq 0 ]; then
+			wget ${DOWNLOAD_URL}/filesystems/osmc-${1}-filesystem-${date}.tar.xz -O filesystem.tar.xz
+            break
+       fi
+       date=$(date +%Y%m%d --date "yesterday $date")
+       let count=count-1
+done
+if [ ! -f filesystem.tar.xz ]; then echo -e "No filesystem available for target" && exit 1; fi
+echo -e "Building disk image"
+if [ "$1" == "rbp" ]; then size=256; fi
+date=$(date +%Y%m%d)
+dd if=/dev/zero of=OSMC_TGT_${1}_${date}.img bs=1M count=${size}
+parted -s OSMC_TGT_${1}_${date}.img mklabel msdos
+parted -s OSMC_TGT_${1}_${date}.img mkpart primary fat32 1M 256M
+kpartx -a OSMC_TGT_${1}_${date}.img
+mkfs.vfat -F32 /dev/mapper/loop0p1
+mount /dev/mapper/loop0p1 /mnt
+if [ "$1" == "rbp" ]
 then
-	echo "Packaging build for Pi"
-	mv zImage rpi-firmware
+	echo -e "Installing Pi files"
+	mv output/images/zImage /mnt
+	mv output/images/INSTALLER/* /mnt
 fi
+echo -e "Installing filesystem"
+mv filesystem.tar.xz /mnt/
+umount /mnt
+sync
+kpartx -d OSMC_TGT_${1}_${date}.img
+echo -e "Compressing image"
+gzip OSMC_TGT_PI_${date}.img
 popd
 echo -e "Build completed"
