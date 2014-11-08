774650aeb7104b15c781314f7e2214568c74f99a
Sam Nazarko <email@samnazarko.co.uk>
2014-11-08 14:37:51 +0000

    [package] [splash-osmc] don't need to make clean fresh checkout

--- package/splash-osmc/build.sh
+++ package/splash-osmc/build.sh
@@ -13,7 +13,6 @@ test "$1" == gen && echo "Package: splash-osmc" >> files/DEBIAN/control
 test "$1" == rbp && echo "Package: rbp-splash-osmc" >> files/DEBIAN/control
 pull_source "https://github.com/samnazarko/plymouth-lite" "$(pwd)/src"
 cd src
-make clean
 $BUILD
 if [ $? != 0 ]; then echo "Error occured during build" && exit 1; fi
 mkdir -p ${out}/usr/bin
