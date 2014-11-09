9bfe08ad64d9d082ac63377def9d98cdce712cb4
Sam Nazarko <email@samnazarko.co.uk>
2014-11-09 15:46:27 +0000

    [package] [dvb-firmware-osmc] use patch system instead of patch -p1 in build script

--- package/dvb-firmware-osmc/build.sh
+++ package/dvb-firmware-osmc/build.sh
@@ -10,7 +10,7 @@ FW_SCRIPT="https://www.kernel.org/doc/Documentation/dvb/get_dvb_firmware"
 make clean
 wget $FW_SCRIPT -O get_dvb_firmware
 chmod +x get_dvb_firmware
-patch -p1 < 000-pull_firmware_auto.patch
+install_patch "patches/" "all"
 
 ./get_dvb_firmware
 mkdir -p files/lib/firmware
