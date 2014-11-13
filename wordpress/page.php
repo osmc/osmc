b930f87221f4cbf5bdca256aeee59aabc870eeca
Sam Nazarko <email@samnazarko.co.uk>
2014-11-13 17:52:34 +0000

    [wordpress] Keep the OSMC blog clean: remove ads for a nice minimal design

--- wordpress/page.php
+++ wordpress/page.php
@@ -29,16 +29,7 @@
 
 								<?php comments_template(); ?>
 
-							</article><script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-							<!-- OSMCBlogPageResponsive -->
-							<ins class="adsbygoogle"
-								 style="display:block"
-								 data-ad-client="ca-pub-3888734777526455"
-								 data-ad-slot="1342267196"
-								 data-ad-format="auto"></ins>
-							<script>
-							(adsbygoogle = window.adsbygoogle || []).push({});
-							</script> 
+							</article>
 							<?php // end article ?>
 
 							<?php endwhile; else : ?>
