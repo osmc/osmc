40c3fd904fffdd1ec444a33e3c8a50c715745f6b
Sam Nazarko <email@samnazarko.co.uk>
2014-11-16 23:36:08 +0000

    [scripts] [common] use command -v; not which

--- scripts/common.sh
+++ scripts/common.sh
@@ -126,9 +126,9 @@ function install_patch()
 
 function pull_source()
 {
-	if ! which unzip >/dev/null 2>&1; then echo -e "Installing unzip" && update_sources && verify_action && install_package "unzip" && verify_action; fi
-	if ! which git >/dev/null 2>&1; then echo -e "Installing Git" && update_sources && verify_action && install_package "git" && verify_action; fi
-	if ! which svn >/dev/null 2>&1; then echo -e "Installing Subversion" && update_sources && verify_action && install_package "subversion" && verify_action; fi
+	if ! command -v unzip >/dev/null 2>&1; then echo -e "Installing unzip" && update_sources && verify_action && install_package "unzip" && verify_action; fi
+	if ! command -v git >/dev/null 2>&1; then echo -e "Installing Git" && update_sources && verify_action && install_package "git" && verify_action; fi
+	if ! command -v svn >/dev/null 2>&1; then echo -e "Installing Subversion" && update_sources && verify_action && install_package "subversion" && verify_action; fi
 	if [ -d ${2} ]
 	then 
 		if [ "$2" != "." ]
