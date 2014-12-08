9a8646c18c8c6b7b08308c4022ca38c01aac05e7
Sam Nazarko <email@samnazarko.co.uk>
2014-12-08 04:06:56 +0000

    [package] [rbp-armmem] build in native environment

--- package/rbp-armmem-osmc/build.sh
+++ package/rbp-armmem-osmc/build.sh
@@ -5,16 +5,22 @@
 
 . ../common.sh
 
-echo -e "Building package rbp-armmem"
-out=$(pwd)/files
-make clean
-pull_source "https://github.com/bavison/arm-mem" "$(pwd)/src"
-cd src
-make
-if [ $? != 0 ]; then echo "Error occured during build" && exit 1; fi
-strip_libs
-mkdir -p $out/usr/lib
-cp -ar libarmmem.so $out/usr/lib
-cp -ar libarmmem.a $out/usr/lib
-cd ../
-dpkg -b files/ rbp-armmem-osmc.deb
+# Build in native environment
+build_in_env "${1}" $(pwd) "rbp-armmem-osmc"
+if [ $? == 0 ]
+then
+	echo -e "Building package rbp-armmem"
+	out=$(pwd)/files
+	make clean
+	pull_source "https://github.com/bavison/arm-mem" "$(pwd)/src"
+	cd src
+	make
+	if [ $? != 0 ]; then echo "Error occured during build" && exit 1; fi
+	strip_libs
+	mkdir -p $out/usr/lib
+	cp -ar libarmmem.so $out/usr/lib
+	cp -ar libarmmem.a $out/usr/lib
+	cd ../
+	dpkg -b files/ rbp-armmem-osmc.deb
+fi
+teardown_env "${1}"
