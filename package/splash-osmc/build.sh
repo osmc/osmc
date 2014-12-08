5a30b4fe71687b0953a425f81e998cca8fab40ad
Sam Nazarko <email@samnazarko.co.uk>
2014-12-08 15:58:44 +0000

    [package] [splash-osmc] pull source outside of qemu chroot

--- package/splash-osmc/build.sh
+++ package/splash-osmc/build.sh
@@ -6,6 +6,7 @@
 . ../common.sh
 
 # Build in native environment
+pull_source "https://github.com/samnazarko/plymouth-lite" "$(pwd)/src"
 build_in_env "${1}" $(pwd) "splash-osmc"
 if [ $? == 0 ]
 then
@@ -15,17 +16,15 @@ then
 	sed '/Package/d' -i files/DEBIAN/control
 	test "$1" == gen && echo "Package: splash-osmc" >> files/DEBIAN/control
 	test "$1" == rbp && echo "Package: rbp-splash-osmc" >> files/DEBIAN/control
-	echo "Depends: fbset" >> files/DEBIAN/control
 	handle_dep "libpng12-dev"
-	pull_source "https://github.com/samnazarko/plymouth-lite" "$(pwd)/src"
-	cd src
+	pushd src
 	$BUILD
 	if [ $? != 0 ]; then echo "Error occured during build" && exit 1; fi
 	mkdir -p ${out}/usr/bin
 	cp -ar ply-image ${out}/usr/bin
 	cp -ar splash.png ${out}/usr
 	cp -ar splash_sad.png ${out}/usr
-	cd ../
+	popd
 	fix_arch_ctl "files/DEBIAN/control"
 	dpkg -b files splash-osmc.deb
 	fi
