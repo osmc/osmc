5ef90680fd5d2074d10e9b34f5a5301db483fab2
Sam Nazarko <email@samnazarko.co.uk>
2014-12-08 16:00:10 +0000

    [package] [rbp-armmem-osmc] pull source outside of qemu chroot

--- package/rbp-armmem-osmc/build.sh
+++ package/rbp-armmem-osmc/build.sh
@@ -4,23 +4,22 @@
 #!/bin/bash
 
 . ../common.sh
-
 # Build in native environment
+pull_source "https://github.com/bavison/arm-mem" "$(pwd)/src"
 build_in_env "${1}" $(pwd) "rbp-armmem-osmc"
 if [ $? == 0 ]
 then
 	echo -e "Building package rbp-armmem"
 	out=$(pwd)/files
 	make clean
-	pull_source "https://github.com/bavison/arm-mem" "$(pwd)/src"
-	cd src
+	pushd src
 	make
 	if [ $? != 0 ]; then echo "Error occured during build" && exit 1; fi
 	strip_libs
 	mkdir -p $out/usr/lib
 	cp -ar libarmmem.so $out/usr/lib
 	cp -ar libarmmem.a $out/usr/lib
-	cd ../
+	popd
 	dpkg -b files/ rbp-armmem-osmc.deb
 fi
 teardown_env "${1}"
