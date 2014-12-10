7a4a6ba9e9dfd07db81764a47f45ca1bcbd1756b
Sam Nazarko <email@samnazarko.co.uk>
2014-12-10 06:32:59 +0000

    [package] [kernel-osmc] use build in native env for kernel

--- package/kernel-osmc/build.sh
+++ package/kernel-osmc/build.sh
@@ -4,32 +4,36 @@
 #!/bin/bash
 
 . ../common.sh
-
-echo -e "Building Linux kernel"
-make clean
-sed '/Package/d' -i files/DEBIAN/control
-sed '/Depends/d' -i files/DEBIAN/control
-handle_dep "kernel-package"
-handle_dep "liblz4-tool"
-echo -e "Downloading kernel sources"
-echo "maintainer := Sam G Nazarko
-email := email@samnazarko.co.uk
-priority := High" >/etc/kernel-pkg.conf
-JOBS=$(if [ ! -f /proc/cpuinfo ]; then mount -t proc proc /proc; fi; cat /proc/cpuinfo | grep processor | wc -l && umount /proc/ >/dev/null 2>&1)
-test $1 == rbp && VERSION="3.17.4" && REVISION="1" && ARCH = "arm"
+test $1 == rbp && VERSION="3.17.4" && REV="1"
 if [ -z $VERSION ]; then echo "Don't have a defined kernel version for this target!" && exit 1; fi
-echo -e "Downloading kernel sources"
-pull_source "https://www.kernel.org/pub/linux/kernel/v3.x/linux-${VERSION}.tar.xz" "${pwd}/src/"
+pull_source "https://www.kernel.org/pub/linux/kernel/v3.x/linux-${VERSION}.tar.xz" "$(pwd)/src/"
 if [ $? != 0 ]; then echo -e "Error downloading" && exit 1; fi
-pushd src
-install_patch "../patches" "all"
-test "$1" == "rbp" && install_patch "../patches" "rbp"
-make-kpkg --stem $1 kernel_image --append-to-version -osmc --jobs $JOBS --revision $REV --arch $ARCH
-if [ $? != 0 ]; then echo "Building kernel image package failed" && exit 1; fi
-make-kpkg --stem $1 kernel_headers --append-to-version -osmc --jobs $JOBS --revision $REV
-if [ $? != 0 ]; then echo "Building kernel headers package failed" && exit 1; fi
-popd
-echo "Package: kernel-${1}-osmc" >> files/DEBIAN/control && echo "Depends: $1-image-${VERSION}-osmc" >> files-dev/DEBIAN/control
-fix_arch_ctl "files/DEBIAN/control"
-dpkg -b files/ kernel-{$1}-osmc.deb
-echo -e "Build complete"
+# Build in native environment
+build_in_env "${1}" $(pwd) "kernel-osmc"
+if [ $? == 0 ]
+then
+	echo -e "Building Linux kernel"
+	make clean
+	sed '/Package/d' -i files/DEBIAN/control
+	sed '/Depends/d' -i files/DEBIAN/control
+	update_sources
+	handle_dep "kernel-package"
+	handle_dep "liblz4-tool"
+	echo "maintainer := Sam G Nazarko
+	email := email@samnazarko.co.uk
+	priority := High" >/etc/kernel-pkg.conf
+	JOBS=$(if [ ! -f /proc/cpuinfo ]; then mount -t proc proc /proc; fi; cat /proc/cpuinfo | grep processor | wc -l && umount /proc/ >/dev/null 2>&1)
+	pushd src/linux-*
+	install_patch "../../patches" "all"
+	test "$1" == "rbp" && install_patch "../../patches" "rbp"
+	make-kpkg --stem $1 kernel_image --append-to-version -osmc --jobs $JOBS --revision $REV
+	if [ $? != 0 ]; then echo "Building kernel image package failed" && exit 1; fi
+	make-kpkg --stem $1 kernel_headers --append-to-version -osmc --jobs $JOBS --revision $REV
+	if [ $? != 0 ]; then echo "Building kernel headers package failed" && exit 1; fi
+	popd
+	echo "Package: kernel-${1}-osmc" >> files/DEBIAN/control && echo "Depends: $1-image-${VERSION}-osmc" >> files-dev/DEBIAN/control
+	fix_arch_ctl "files/DEBIAN/control"
+	dpkg -b files/ kernel-{$1}-osmc.deb
+	echo -e "Build complete"
+fi
+teardown_env "${1}"
