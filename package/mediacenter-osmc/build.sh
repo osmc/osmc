d8a4916c4234c812c4df2cc13cb177d472398d31
Sam Nazarko <email@samnazarko.co.uk>
2014-12-11 00:05:29 +0000

    [package] [mediacenter-osmc] build in native environment

--- package/mediacenter-osmc/build.sh
+++ package/mediacenter-osmc/build.sh
@@ -5,171 +5,173 @@
 
 . ../common.sh
 
-echo -e "Building package Kodi"
-out=$(pwd)/files
-make clean
-handle_dep "autopoint"
-handle_dep "automake"
-handle_dep "bison"
-handle_dep "make"
-handle_dep "curl"
-handle_dep "cvs"
-handle_dep "default-jre"
-handle_dep "fp-compiler"
-handle_dep "gawk"
-handle_dep "gdc"
-handle_dep "gettext"
-handle_dep "gperf"
-handle_dep "libasound2-dev"
-handle_dep "libass-dev"
-handle_dep "libboost-dev"
-handle_dep "libboost-thread-dev"
-handle_dep "libz2-dev"
-handle_dep "libcap-dev"
-handle_dep "libcdio-dev"
-handle_dep "libcurl3"
-handle_dep "libcurl4-gnutls-dev"
-handle_dep "libdbus-1-dev"
-handle_dep "libenca-dev"
-handle_dep "libflac-dev"
-handle_dep "libfontconfig-dev"
-handle_dep "libfreetype6-dev"
-handle_dep "libfribidi-dev"
-handle_dep "libglew-dev"
-handle_dep "libiso9660-dev"
-handle_dep "libjasper-dev"
-handle_dep "libjpeg-dev"
-handle_dep "liblzo2-dev"
-handle_dep "libmad0-dev"
-handle_dep "libmicrohttpd-dev"
-handle_dep "libmodplug-dev"
-handle_dep "libmpeg2-4-dev"
-handle_dep "libmpeg3-dev"
-handle_dep "libmysqlclient-dev"
-handle_dep "libogg-dev"
-handle_dep "libpcre3-dev"
-handle_dep "libplist-dev"
-handle_dep "libpng-dev"
-handle_dep "libpulse-dev"
-handle_dep "libsdl-dev"
-handle_dep "libsdl-gfx1.2-dev"
-handle_dep "libsdl-image1.2-dev"
-handle_dep "libsdl-mixer1.2-dev"
-handle_dep "libsmbclient-dev"
-handle_dep "libsqlite3-dev"
-handle_dep "libssh-dev"
-handle_dep "libssl-dev"
-handle_dep "libtiff-dev"
-handle_dep "libtinyxml-dev"
-handle_dep "libtool"
-handle_dep "libudev-dev"
-handle_dep "libusb-dev"
-handle_dep "libvorbisenc2"
-handle_dep "libxml2-dev"
-handle_dep "libxmu-dev"
-handle_dep "libxslt1-dev"
-handle_dep "libxt-dev"
-handle_dep "libyajl-dev"
-handle_dep "nasm"
-handle_dep "pmount"
-handle_dep "python-dev"
-handle_dep "python-imaging"
-handle_dep "python-sqlite"
-handle_dep "swig"
-handle_dep "unzip"
-handle_dep "yasm"
-handle_dep "zip"
-handle_dep "zlib1g-dev"
-handle_dep "libbluray-dev"
-handle_dep "libtag1-dev"
-handle_dep "libsamplerate-dev"
-handle_dep "libmp3lame-dev"
-handle_dep "libltdl-dev"
-handle_dep "cmake"
-if [ "$1" == "rbp" ]
+pull_source "https://github.com/xbmc/xbmc" "kodi/"
+if [ $? != 0 ]; then echo -e "Error fetching Kodi source" && exit 1; fi
+pull_source "https://github.com/opdenkamp/xbmc-pvr-addons" "kodi-pvr/"
+if [ $? != 0 ]; then echo -e "Error fetching Kodi PVR source" && exit 1; fi
+# Build in native environment
+build_in_env "${1}" $(pwd) "mediacenter-osmc"
+if [ $? == 0 ]
 then
-	handle_dep "rbp-libcec-dev-osmc"
-	handle_dep "rbp-libshairplay-dev-osmc"
-	handle_dep "rbp-librtmp-dev-osmc"
-	handle_dep "rbp-libnfs-dev-osmc"
-	handle_dep "rbp-libafpclient-dev-osmc"
+	echo -e "Building package Kodi"
+	out=$(pwd)/files
+	make clean
+	handle_dep "autopoint"
+	handle_dep "automake"
+	handle_dep "bison"
+	handle_dep "make"
+	handle_dep "curl"
+	handle_dep "cvs"
+	handle_dep "default-jre"
+	handle_dep "fp-compiler"
+	handle_dep "gawk"
+	handle_dep "gdc"
+	handle_dep "gettext"
+	handle_dep "gperf"
+	handle_dep "libasound2-dev"
+	handle_dep "libass-dev"
+	handle_dep "libboost-dev"
+	handle_dep "libboost-thread-dev"
+	handle_dep "libz2-dev"
+	handle_dep "libcap-dev"
+	handle_dep "libcdio-dev"
+	handle_dep "libcurl3"
+	handle_dep "libcurl4-gnutls-dev"
+	handle_dep "libdbus-1-dev"
+	handle_dep "libenca-dev"
+	handle_dep "libflac-dev"
+	handle_dep "libfontconfig-dev"
+	handle_dep "libfreetype6-dev"
+	handle_dep "libfribidi-dev"
+	handle_dep "libglew-dev"
+	handle_dep "libiso9660-dev"
+	handle_dep "libjasper-dev"
+	handle_dep "libjpeg-dev"
+	handle_dep "liblzo2-dev"
+	handle_dep "libmad0-dev"
+	handle_dep "libmicrohttpd-dev"
+	handle_dep "libmodplug-dev"
+	handle_dep "libmpeg2-4-dev"
+	handle_dep "libmpeg3-dev"
+	handle_dep "libmysqlclient-dev"
+	handle_dep "libogg-dev"
+	handle_dep "libpcre3-dev"
+	handle_dep "libplist-dev"
+	handle_dep "libpng-dev"
+	handle_dep "libpulse-dev"
+	handle_dep "libsdl-dev"
+	handle_dep "libsdl-gfx1.2-dev"
+	handle_dep "libsdl-image1.2-dev"
+	handle_dep "libsdl-mixer1.2-dev"
+	handle_dep "libsmbclient-dev"
+	handle_dep "libsqlite3-dev"
+	handle_dep "libssh-dev"
+	handle_dep "libssl-dev"
+	handle_dep "libtiff-dev"
+	handle_dep "libtinyxml-dev"
+	handle_dep "libtool"
+	handle_dep "libudev-dev"
+	handle_dep "libusb-dev"
+	handle_dep "libvorbisenc2"
+	handle_dep "libxml2-dev"
+	handle_dep "libxmu-dev"
+	handle_dep "libxslt1-dev"
+	handle_dep "libxt-dev"
+	handle_dep "libyajl-dev"
+	handle_dep "nasm"
+	handle_dep "pmount"
+	handle_dep "python-dev"
+	handle_dep "python-imaging"
+	handle_dep "python-sqlite"
+	handle_dep "swig"
+	handle_dep "unzip"
+	handle_dep "yasm"
+	handle_dep "zip"
+	handle_dep "zlib1g-dev"
+	handle_dep "libbluray-dev"
+	handle_dep "libtag1-dev"
+	handle_dep "libsamplerate-dev"
+	handle_dep "libmp3lame-dev"
+	handle_dep "libltdl-dev"
+	handle_dep "cmake"
+	if [ "$1" == "rbp" ]
+	then
+		handle_dep "rbp-libcec-dev-osmc"
+		handle_dep "rbp-libshairplay-dev-osmc"
+		handle_dep "rbp-librtmp-dev-osmc"
+		handle_dep "rbp-libnfs-dev-osmc"
+		handle_dep "rbp-libafpclient-dev-osmc"
+	fi
+	sed '/Package/d' -i files/DEBIAN/control
+	sed /'Depends/d' -i files/DEBIAN/control
+	test "$1" == atv && echo "Package: atv-mediacenter-osmc" >> files/DEBIAN/control
+	test "$1" == rbp && echo "Package: rbp-mediacenter-osmc" >> files/DEBIAN/control
+	pushd kodi/
+	install_patch "../patches" "all"
+	test "$1" == atv && install_patch "../patches" "atv"
+	test "$1" == rbp && install_patch "../patches" "rbp" && install_patch "../patches" "lpr"
+	./bootstrap
+	# Apple TV configuration
+	test "$1" == atv && \
+	CXXFLAGS="-I/usr/include/afpfs-ng" ./configure \
+		--prefix=/usr \
+		--disable-vtbdecoder \ 
+		--disable-vaapi \ 
+		--disable-vdpau \
+		--disable-pulse \ 
+		--disable-projectm 
+	# Raspberry Pi Configuration
+	test "$1" == rbp && \ 
+	LIBRARY_PATH+=/opt/vc/lib \ 
+	CXXFLAGS="-I/usr/include/afpfs-ng -I/opt/vc/include -I/opt/vc/include/interface -I/opt/vc/include/interface/vcos/pthreads -I/opt/vc/include/interface/vmcs_host/linux -L/opt/vc/lib" \ 
+	./configure \
+		--prefix=/usr \
+		--enable-gles \
+		--disable-sdl \ 
+		--disable-x11 \ 
+		--disable-xrandr \ 
+		--disable-openmax \
+		--enable-optical-drive \ 
+		--enable-libbluray \ 
+		--enable-dvdcss \
+		--disable-joystick \ 
+		--disable-debug \
+		--disable-crystalhd \ 
+		--disable-vtbdecoder \ 
+		--disable-vaapi \ 
+		--disable-vdpau \
+		--disable-pulse \ 
+		--disable-projectm \ 
+		--with-platform=raspberry-pi \
+		--enable-optimizations \
+		--enable-libcec \
+		--enable-player=omxplayer
+	if [ $? != 0 ]; then echo -e "Configure failed!" && exit 1; fi
+	$BUILD
+	if [ $? != 0 ]; then echo -e "Build failed!" && exit 1; fi
+	make install DESTDIR=${out}
+	popd
+	pushd kodi-pvr
+	make clean >/dev/null 2>&1
+	./bootstrap
+	./configure --prefix=/usr --enable-addons-with-dependencies
+	if [ $? != 0 ]; then echo -e "Configure failed!" && exit 1; fi
+	$BUILD
+	if [ $? != 0 ]; then echo -e "Build failed!" && exit 1; fi
+	make install DESTDIR=${out}
+	cd ../
+	rm -rf ${out}/usr/share/xbmc/addons/service.kodi.versioncheck
+	strip ${out}/usr/lib/kodi/kodi.bin
+	strip ${out}/usr/lib/kodi/addons/*/*.so
+	strip ${out}/usr/lib/kodi/addons/pvr.*/*.pvr
+	COMMON_DEPENDS="niceprioritypolicy-osmc, mediacenter-send-osmc, libssh-4, libavahi-client3, python, libsmbclient, libtiff5, libjpeg8, libsqlite3-0, libflac8, libtinyxml2.6.2, libogg0, libmad0, libmicrohttp10, libjasper1, libyajl2, libmysqlclient18, libasound2, libxml2, libxslt1.1, libpng12-0, libsamplerate0, libtag1-vanilla, libfribidi0, libzlk02-2, libcdio13, libpcrecpp0, libfreetype6, libvorbisenc2, libcurl3-gnutls, upower"
+	X86_DEPENDS="libcec-osmc, libshairplay-osmc, libnfs-osmc, libafpclient-osmc, librtmp-osmc, splash-osmc"
+	test "$1" == atv && echo "Depends: ${COMMON_DEPENDS}, ${X86_DEPENDS}, libpulse0, libxrandr2, libsdl-image1.2, libglew1.10, libglu1-mesa, libcrystalhd3, firmware-crystalhd" >> files/DEBIAN/control
+	test "$1" == rbp && echo "Depends: ${COMMON_DEPENDS}, rbp-libcec-osmc, rbp-libafpclient-osmc, rbp-librtmp-osmc, rbp-userland-osmc, rbp-armmem-osmc, rbp-splash-osmc" >> files/DEBIAN/control
+	chmod +x ${out}/lib/systemd/system/mediacenter.service
+	cp ${1}-watchdog ${out}/usr/bin/mediacenter
+	chmod +x ${out}/usr/bin/mediacenter
+	fix_arch_ctl "files/DEBIAN/control"
+	dpkg -b files/ mediacenter-osmc.deb
 fi
-sed '/Package/d' -i files/DEBIAN/control
-sed /'Depends/d' -i files/DEBIAN/control
-test "$1" == atv && echo "Package: atv-mediacenter-osmc" >> files/DEBIAN/control
-test "$1" == rbp && echo "Package: rbp-mediacenter-osmc" >> files/DEBIAN/control
-KODI_SRC="https://github.com/xbmc/xbmc"
-KODI_BRANCH="master"
-echo Downloading sources
-git clone "${KODI_SRC}" -b "${KODI_BRANCH}" Kodi/
-if [ $? != 0 ]; then echo -e "Checkout failed" && exit 1; fi
-cd Kodi/
-install_patch "../patches" "all"
-test "$1" == atv && install_patch "../patches" "atv"
-test "$1" == rbp && install_patch "../patches" "rbp" && install_patch "../patches" "lpr"
-./bootstrap
-# Apple TV configuration
-test "$1" == atv && \
-CXXFLAGS="-I/usr/include/afpfs-ng" ./configure \
-	--prefix=/usr \
-	--disable-vtbdecoder \ 
- 	--disable-vaapi \ 
- 	--disable-vdpau \
- 	--disable-pulse \ 
- 	--disable-projectm 
-# Raspberry Pi Configuration
-test "$1" == rbp && \ 
-LIBRARY_PATH+=/opt/vc/lib \ 
-CXXFLAGS="-I/usr/include/afpfs-ng -I/opt/vc/include -I/opt/vc/include/interface -I/opt/vc/include/interface/vcos/pthreads -I/opt/vc/include/interface/vmcs_host/linux -L/opt/vc/lib" \ 
-./configure \
-	--prefix=/usr \
- 	--enable-gles \
- 	--disable-sdl \ 
- 	--disable-x11 \ 
- 	--disable-xrandr \ 
- 	--disable-openmax \
-	--enable-optical-drive \ 
-	--enable-libbluray \ 
-	--enable-dvdcss \
-	--disable-joystick \ 
-	--disable-debug \
- 	--disable-crystalhd \ 
- 	--disable-vtbdecoder \ 
- 	--disable-vaapi \ 
- 	--disable-vdpau \
- 	--disable-pulse \ 
- 	--disable-projectm \ 
- 	--with-platform=raspberry-pi \
- 	--enable-optimizations \
- 	--enable-libcec \
- 	--enable-player=omxplayer
-if [ $? != 0 ]; then echo -e "Configure failed!" && exit 1; fi
-$BUILD
-if [ $? != 0 ]; then echo -e "Build failed!" && exit 1; fi
-make install DESTDIR=${out}
-cd ../
-PVR_SRC="https://github.com/opdenkamp/xbmc-pvr-addons"
-PVR_BRANCH="master"
-git clone "${PVR_SRC}" -b "${PVR_BRANCH}" Kodi-PVR
-cd Kodi-PVR
-make clean >/dev/null 2>&1
-./bootstrap
-./configure --prefix=/usr --enable-addons-with-dependencies
-if [ $? != 0 ]; then echo -e "Configure failed!" && exit 1; fi
-$BUILD
-if [ $? != 0 ]; then echo -e "Build failed!" && exit 1; fi
-make install DESTDIR=${out}
-cd ../
-rm -rf ${out}/usr/share/xbmc/addons/service.kodi.versioncheck
-strip ${out}/usr/lib/kodi/kodi.bin
-strip ${out}/usr/lib/kodi/addons/*/*.so
-strip ${out}/usr/lib/kodi/addons/pvr.*/*.pvr
-COMMON_DEPENDS="niceprioritypolicy-osmc, mediacenter-send-osmc, libssh-4, libavahi-client3, python, libsmbclient, libtiff5, libjpeg8, libsqlite3-0, libflac8, libtinyxml2.6.2, libogg0, libmad0, libmicrohttp10, libjasper1, libyajl2, libmysqlclient18, libasound2, libxml2, libxslt1.1, libpng12-0, libsamplerate0, libtag1-vanilla, libfribidi0, libzlk02-2, libcdio13, libpcrecpp0, libfreetype6, libvorbisenc2, libcurl3-gnutls, upower"
-X86_DEPENDS="libcec-osmc, libshairplay-osmc, libnfs-osmc, libafpclient-osmc, librtmp-osmc, splash-osmc"
-test "$1" == atv && echo "Depends: ${COMMON_DEPENDS}, ${X86_DEPENDS}, libpulse0, libxrandr2, libsdl-image1.2, libglew1.10, libglu1-mesa, libcrystalhd3, firmware-crystalhd" >> files/DEBIAN/control
-test "$1" == rbp && echo "Depends: ${COMMON_DEPENDS}, rbp-libcec-osmc, rbp-libafpclient-osmc, rbp-librtmp-osmc, rbp-userland-osmc, rbp-armmem-osmc, rbp-splash-osmc" >> files/DEBIAN/control
-chmod +x ${out}/lib/systemd/system/mediacenter.service
-cp ${1}-watchdog ${out}/usr/bin/mediacenter
-chmod +x ${out}/usr/bin/mediacenter
-fix_arch_ctl "files/DEBIAN/control"
-dpkg -b files/ mediacenter-osmc.deb
+teardown_env "${1}"
