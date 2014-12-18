2266e3423520f009108945b51d1f98b119f8479a
Sam Nazarko <email@samnazarko.co.uk>
2014-12-18 12:42:49 +0000

    [toolchain] [imx6] [vero] use jessie not wheezy repositories!

--- toolchains/vero-toolchain-osmc/build.sh
+++ toolchains/vero-toolchain-osmc/build.sh
@@ -54,9 +54,9 @@ enable_nw_chroot "${DIR}"
 verify_action
 
 # Set up sources.list
-echo "deb http://ftp.debian.org/debian stable main contrib non-free
-deb http://ftp.debian.org/debian/ wheezy-updates main contrib non-free
-deb http://security.debian.org/ wheezy/updates main contrib non-free
+echo "deb http://ftp.debian.org/debian jessie main contrib non-free
+deb http://ftp.debian.org/debian/ jessie-updates main contrib non-free
+deb http://security.debian.org/ jessie/updates main contrib non-free
 deb http://apt.osmc.tv jessie main
 " > ${DIR}/etc/apt/sources.list
 
