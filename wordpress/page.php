f0fe287c9cf44a8650e0b89f0f2ec6cbabeefe60
Sam Nazarko <email@samnazarko.co.uk>
2014-12-21 19:22:59 +0000

    [wordpress] diagnostics

--- wordpress/page.php
+++ wordpress/page.php
@@ -20,6 +20,7 @@
 										if (strpos($url,'wiki/') && strcmp($url, 'help/wiki/') !== 0)
 										{
 										    echo '<a href="http://osmc.tv/wiki">Back to Wiki</a>';
+										    echo $url;
 										}
 
 									?>
