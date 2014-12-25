669bdf374d96538608465d419ff73175b058fdb3
Sam Nazarko <email@samnazarko.co.uk>
2014-12-25 02:05:26 +0000

    [installer] [target] stick to 640*480 for now

--- installer/target/qt_target/qt_target_installer/mainwindow.cpp
+++ installer/target/qt_target/qt_target_installer/mainwindow.cpp
@@ -23,7 +23,8 @@ MainWindow::MainWindow(QWidget *parent) :
 
 {
     ui->setupUi(this);
-    this->setFixedSize(QApplication::desktop()->size());
+    /* this->setFixedSize(QApplication::desktop()->size()); */
+    this->setFixedSize(this->size());
     /* Set up logging */
     logger = new Logger();
     logger->addLine("Starting OSMC installer");
