56e2c877aa2bc2ac586c6377977efbd04b4419ec
Sam Nazarko <email@samnazarko.co.uk>
2014-12-28 23:47:07 +0000

    [package] [mediacenter-skin-osmc] add skeleton for skin

---
+++ package/mediacenter-skin-osmc/build.sh
@@ -0,0 +1,10 @@
+# (c) 2014 Sam Nazarko
+# email@samnazarko.co.uk
+
+#!/bin/bash
+
+. ../common.sh
+
+echo -e "Building package mediacenter-skin-osmc"
+dpkg -b files/ mediacenter-skin-osmc.deb
+teardown_env "${1}"
