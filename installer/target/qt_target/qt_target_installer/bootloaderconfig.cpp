e76e3daec2cb05d14f40c6bd53f46976edab1f21
Sam Nazarko <email@samnazarko.co.uk>
2014-12-28 01:59:25 +0000

    [installer] [target] set Normal overclock settings

--- installer/target/qt_target/qt_target_installer/bootloaderconfig.cpp
+++ installer/target/qt_target/qt_target_installer/bootloaderconfig.cpp
@@ -52,7 +52,7 @@ void BootloaderConfig::configureEnvironment()
         utils->writeToFile(cmdlineFile, cmdlineStringList, false);
         QFile configFile("/mnt/boot/config.txt");
         QStringList configStringList;
-        configStringList << "arm_freq=800\n" << "gpu_mem=128\n" << "hdmi_ignore_cec_init=1\n" << "disable_overscan=1\n" << "start_file=start_x.elf\n" << "fixup_file=fixup_x.dat";
+        configStringList << "arm_freq=850\n" << "core_freq=375\n" << "gpu_mem=128\n" << "hdmi_ignore_cec_init=1\n" << "disable_overscan=1\n" << "start_file=start_x.elf\n" << "fixup_file=fixup_x.dat";
         utils->writeToFile(configFile, configStringList, false);
     }
 }
