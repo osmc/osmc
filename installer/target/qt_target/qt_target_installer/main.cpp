ab09ad7e3b5b988c6b69204588dc29de24018667
srmo <srm@schakalacka.org>
2014-12-28 01:46:07 +0100

    [installer][target] auto-scale fonts and minor changes to positioning. Hail round corners!

--- installer/target/qt_target/qt_target_installer/main.cpp
+++ installer/target/qt_target/qt_target_installer/main.cpp
@@ -9,7 +9,7 @@ int main(int argc, char *argv[])
     QFontDatabase fontDatabase;
     fontDatabase.addApplicationFont(":/assets/resources/SourceSansPro-Regular.ttf");
     MainWindow w;
-    w.showMaximized();
+    w.showFullScreen();
     /* Make sure our app installs only after starting the event loop */
     QTimer::singleShot(0, &w, SLOT(install()));
     return a.exec();
