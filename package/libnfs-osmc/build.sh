87dd7ca2365904f030595438d116cf6b45bf1c5f
Sam Nazarko <email@samnazarko.co.uk>
2014-12-30 05:56:06 +0000

    [package] [libnfs] fix build for armv7

--- package/libnfs-osmc/build.sh
+++ package/libnfs-osmc/build.sh
@@ -24,7 +24,7 @@ then
 	handle_dep "libtool"
 	test "$1" == gen && echo "Package: libnfs-osmc" >> files/DEBIAN/control && echo "Package: libnfs-dev-osmc" >> files-dev/DEBIAN/control && echo "Depends: libnfs-osmc" >> files-dev/DEBIAN/control
 	test "$1" == rbp && echo "Package: rbp-libnfs-osmc" >> files/DEBIAN/control && echo "Package: rbp-libnfs-dev-osmc" >> files-dev/DEBIAN/control && echo "Depends: rbp-libnfs-osmc" >> files-dev/DEBIAN/control
-	test "$1" == rbp && echo "Package: armv7-libnfs-osmc" >> files/DEBIAN/control && echo "Package: armv7-libnfs-dev-osmc" >> files-dev/DEBIAN/control && echo "Depends: armv7-libnfs-osmc" >> files-dev/DEBIAN/control
+	test "$1" == armv7 && echo "Package: armv7-libnfs-osmc" >> files/DEBIAN/control && echo "Package: armv7-libnfs-dev-osmc" >> files-dev/DEBIAN/control && echo "Depends: armv7-libnfs-osmc" >> files-dev/DEBIAN/control
 	pushd src
 	./bootstrap
 	./configure --prefix=/usr
