a927f35cb314a787d62c84ac13098a78b7d48a4b
Sam Nazarko <email@samnazarko.co.uk>
2014-12-30 02:28:38 +0000

    [toolchain] mark tcver with platform type

--- toolchains/common/funcs.sh
+++ toolchains/common/funcs.sh
@@ -11,7 +11,8 @@ function build_deb_package()
 	cp -ar ${1}/DEBIAN ${1}/output
 	mv ${1}/opt ${1}/output
 	# Mark our FS
-	echo ${2} >${1}/output/tcver
+	target=$(echo $t1 | cut -f 1 -d -)
+	echo ${2} >${1}/output/tcver.$target
 	dpkg -b ${1}/output ${2}.deb
 }
 
