48aa4c227cbd691905dac6ec9bc5b9d782362e22
Sam Nazarko <email@samnazarko.co.uk>
2015-01-10 16:14:23 +0000

    [package] [rbp-userland-osmc] use tarball, not git for faster build

--- package/rbp-userland-osmc/build.sh
+++ package/rbp-userland-osmc/build.sh
@@ -9,16 +9,16 @@ echo -e "Building package rbp-userland"
 out=$(pwd)/files
 make clean
 echo Downloading userland
-pull_source "https://github.com/raspberrypi/firmware" "$(pwd)/src"
+pull_source "https://github.com/raspberrypi/firmware/archive/master.tar.gz" "$(pwd)/src"
 if [ $? != 0 ]; then echo -e "Error downloading" && exit 1; fi
 echo Moving files in to place
 mkdir -p files/opt/vc
 mkdir -p files-dev/opt/vc
 mkdir -p files/etc/ld.so.conf.d
 echo "/opt/vc/lib" > files/etc/ld.so.conf.d/rbp.conf
-cp -ar src/hardfp/opt/vc/bin/ files/opt/vc
-cp -ar src/hardfp/opt/vc/lib files/opt/vc
-cp -ar src/hardfp/opt/vc/include files-dev/opt/vc
+cp -ar src/firmware-master/hardfp/opt/vc/bin/ files/opt/vc
+cp -ar src/firmware-master/hardfp/opt/vc/lib files/opt/vc
+cp -ar src/firmware-master/hardfp/opt/vc/include files-dev/opt/vc
 rm -rf src
 dpkg -b files/ rbp-userland-osmc.deb
 dpkg -b files-dev/ rbp-userland-dev-osmc.deb
