8dc473fe50ad03a3b435d2565da8e1c151ab40dd
Sam Nazarko <email@samnazarko.co.uk>
2015-01-11 15:44:33 +0000

    [wordpress] don't make wiki pages take up full width

--- wordpress/404.php
+++ wordpress/404.php
@@ -4,7 +4,7 @@
 
 				<div id="inner-content" class="wrap clearfix">
 
-					<div id="main" class="eightcol first clearfix" role="main">
+					<div id="main" class="eightcol first clearfix pure-u-3-4" role="main">
 							<?php
 							$url = $_SERVER['REQUEST_URI'];
 							if (strpos($url, 'wiki/')) {
