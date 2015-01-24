530c0c92c58d2a558b0baec4fc46a2be4e71e7ba
Sam Nazarko <email@samnazarko.co.uk>
2015-01-24 07:45:41 +0000

    [package] [atv-darwin-cross-osmc] Rename atv-darwin-x-osmc to atv-darwin-cross-osmc. Clean up docs to save some space
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

---
+++ package/atv-darwin-cross-osmc/build.sh
@@ -0,0 +1,8 @@
+# (c) 2014-2015 Sam Nazarko
+# email@samnazarko.co.uk
+
+#!/bin/bash
+
+echo -e "Building package Darwin Cross for AppleTV"
+make clean
+dpkg -b files/ appletv-darwin-cross-osmc.deb
--- package/atv-darwinx-osmc/build.sh
+++
@@ -1,8 +0,0 @@
-# (c) 2014-2015 Sam Nazarko
-# email@samnazarko.co.uk
-
-#!/bin/bash
-
-echo -e "Building package Darwin-X for AppleTV"
-make clean
-dpkg -b files/ appletv-darwinx-osmc.deb
