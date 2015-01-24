22b57aad33ca5dbec1c683dbf71f0af1a86cc981
Sam Nazarko <email@samnazarko.co.uk>
2015-01-24 08:28:52 +0000

    [package] [atv-remote-osmc] keep source locally and build from there
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

--- package/atv-remote-osmc/build.sh
+++ package/atv-remote-osmc/build.sh
@@ -5,15 +5,22 @@
 
 . ../common.sh
 
-echo -e "Building atvclient for AppleTV"
-make clean
-out=$(pwd)/files
-pull_source "https://github.com/samnazarko/atvclient" "$(pwd)/src"
-cd src
-./configure --prefix=/usr
-$BUILD
-make install DESTDIR=${out}
-if [ $? != 0 ]; then echo "Error occured during build" && exit 1; fi
-strip_files "${out}"
-cd ../
-dpkg -b files/ appletv-remote-osmc.deb
+# Build in native environment
+build_in_env "${1}" $(pwd) "atv-remote-osmc"
+if [ $? == 0 ]
+then
+	echo -e "Building atv-remote"
+	out=$(pwd)/files
+	make clean
+	update_sources
+	handle_dep "libusb-dev"
+	pushd src/
+	./configure --prefix=/usr
+	$BUILD
+	make install DESTDIR=${out}
+	if [ $? != 0 ]; then echo "Error occured during build" && exit 1; fi
+	strip_files "${out}"
+	popd
+	dpkg -b files/ atv-remote-osmc.deb
+fi
+teardown_env "${1}"
