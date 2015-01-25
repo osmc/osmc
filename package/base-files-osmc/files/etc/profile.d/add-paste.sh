a3c6d99f144080ccea72701718cee4b8c0d97809
DBMandrake <sbyrnand@gmail.com>
2015-01-25 21:12:05 +0000

    [package] [base-files-osmc] Rewrite paste to remove file size limitation and add filename argument in addition to stdin.

--- package/base-files-osmc/files/etc/profile.d/add-paste.sh
+++ package/base-files-osmc/files/etc/profile.d/add-paste.sh
@@ -1,1 +1,11 @@
-paste() { a=$(cat); curl -X POST -s -d "$a" http://paste.osmc.io/documents | awk -F '"' '{print "http://paste.osmc.io/"$4}'; }
+paste() {
+
+if [ $# -ne 0 ]; then
+	curl -X POST -s -T "$1" http://paste.osmc.io/documents | awk -F '"' '{print "http://paste.osmc.io/"$4}'
+else
+	tmp=$(tempfile)
+	cat > $tmp
+	curl -X POST -s -T "$tmp" http://paste.osmc.io/documents | awk -F '"' '{print "http://paste.osmc.io/"$4}'
+	rm -f $tmp
+fi
+}
