61417de85a696a613b6520bc80183e9d64619234
KODeKarnage <sublime.kev@gmail.com>
2015-01-31 21:46:52 +1000

    [package][mediacenter-addon-osmc] switched to 'is_enabled', added running string to friendly name

--- package/mediacenter-addon-osmc/src/script.module.osmcsetting.services/resources/lib/service_selection_gui.py
+++ package/mediacenter-addon-osmc/src/script.module.osmcsetting.services/resources/lib/service_selection_gui.py
@@ -2,6 +2,7 @@ import os
 import subprocess
 import xbmcgui
 from collections import OrderedDict
+import glob
 
 
 ACTION_PREVIOUS_MENU = 10
@@ -16,6 +17,11 @@ class MaitreD(object):
     def __init__(self, logger):
         self.log = logger
         self.services = {}
+        self.poll_services()
+
+    def poll_services(self):
+        full_paths = glob.glob('/etc/systemd/system/*.wants/*')
+        self.active_services = [os.path.basename(x) for x in full_paths]
 
     def enable_service(self, service_name):
         self.log("MaitreD: Enabling " + service_name)
@@ -34,10 +40,16 @@ class MaitreD(object):
         return True if 'running' in out else False
 
     def is_enabled(self, service_name):
-        self.log("The service is currently enabled")
+
+        if service_name in self.active_services:
+            self.log("%s is currently enabled" % service_name)  
+            return True
+        else:
+            self.log("%s is currently enabled" % service_name)
+            return False
 
     def all_services(self):
-        ''' Returns a dict of service tuples. {s_name: entry, service_name, running} '''
+        ''' Returns a dict of service tuples. {s_name: (entry, service_name, running, enabled)} '''
 
         svcs = OrderedDict()
 
@@ -50,11 +62,20 @@ class MaitreD(object):
                     self.log("MaitreD: Service Friendly Name: " + s_name)
                     self.log("MaitreD: Service Entry Point: " + s_entry)
 
-                    svcs[s_name] = ( s_entry, service_name, self.is_running(service_name) )
+                    enabled = self.is_enabled(service_name)
+                    running = ''
+                    if enabled:
+                        r = self.is_running(service_name)
+                        if r:
+                            running = " (running)"
+                        else:
+                            running = " (stopped)"
+
+                    svcs[s_name] = ( s_entry, service_name, running, enabled )
                                     
 
         # this last part creates a dictionary ordered by the services friendly name
-        self.services = OrderedDict(k: svcs[k] for k sorted(svcs.keys()))
+        self.services = OrderedDict([(k, svcs[k]) for k in sorted(svcs.keys())])
         self.log('MaitreD: service list = %s' % self.services)
         return self.services
 
@@ -114,8 +135,8 @@ class service_selection(xbmcgui.WindowXMLDialog):
 
         for s_name, service_tup in self.service_list.iteritems():
             # populate the list
-            entry, service_name, status = service_tup
-            self.tmp = xbmcgui.ListItem(label=s_name, label2=str(status))
+            entry, service_name, running, enabled = service_tup
+            self.tmp = xbmcgui.ListItem(label=s_name + running, label2=str(enabled))
             self.name_list.addItem(self.tmp)
 
             # highlight the already selection randos
@@ -154,7 +175,7 @@ class service_selection(xbmcgui.WindowXMLDialog):
 
         for x in range(sz):
             line = self.name_list.getListItem(x)
-            processing_list.append((line.getLabel(), line.isSelected()))
+            processing_list.append((line.getLabel().replace(' (running)','').replace(' (stopped)',''), line.isSelected()))
 
         self.garcon.process_services(processing_list)
 
