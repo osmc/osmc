9777f584d3179a25e138d8312964ed84599f923a
Mark Theis Madsen <marktheism@gmail.com>
2015-02-03 19:22:04 +0100

    [wp] align paypal image

--- wordpress/header.php
+++ wordpress/header.php
@@ -45,7 +45,7 @@
 		<link rel="alternate" type="application/rss+xml" title="OSMC Feed" href="/feed?cat=-42" />
 		<?php wp_head(); ?>
         <!-- inject:css -->
-        <link type="text/css" rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/library/style/css/style-cb8013f7.css">
+        <link type="text/css" rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/library/style/css/style-ad978719.css">
         <!-- endinject -->
         
 	</head>
