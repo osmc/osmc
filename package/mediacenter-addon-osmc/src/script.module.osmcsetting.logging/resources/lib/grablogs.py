99b805054540ca4a8cfeb11e2fc1ccd409216264
KODeKarnage <sublime.kev@gmail.com>
2015-02-13 11:11:00 +1100

    [package][mediacenter-addon-osmc] Log Uploader: removed email function and options, added fstab, cmdline, keyboard, sources, advancesettings

--- package/mediacenter-addon-osmc/src/script.module.osmcsetting.logging/resources/lib/grablogs.py
+++ package/mediacenter-addon-osmc/src/script.module.osmcsetting.logging/resources/lib/grablogs.py
@@ -32,24 +32,28 @@ class Main(object):
 
 		self.log_list = []
 
-		grab_all 	= True if __addon__.getSetting('all') == 'true' else False
-		kodi 		= True if __addon__.getSetting('kodi') == 'true' else False
-		config 		= True if __addon__.getSetting('config') == 'true' else False
-		packages 	= True if __addon__.getSetting('packages') == 'true' else False
-		apt 		= True if __addon__.getSetting('apt') == 'true' else False
-		action      = __addon__.getSetting('mode') 
-		self.email       = __addon__.getSetting('email') 
+		grab_all 			= True if __addon__.getSetting('all') == 'true' else False
+		kodi 				= True if __addon__.getSetting('kodi') == 'true' else False
+		config 				= True if __addon__.getSetting('config') == 'true' else False
+		packages 			= True if __addon__.getSetting('packages') == 'true' else False
+		apt 				= True if __addon__.getSetting('apt') == 'true' else False
+		cmdline 			= True if __addon__.getSetting('cmdline') == 'true' else False
+		fstab 				= True if __addon__.getSetting('fstab') == 'true' else False
+		advancedsettings 	= True if __addon__.getSetting('advancedsettings') == 'true' else False
+		sources 			= True if __addon__.getSetting('sources') == 'true' else False
+		keyboard 			= True if __addon__.getSetting('keyboard') == 'true' else False
 
-		log(type(action))
-
-		if self.email == '':
-			action = '0'
 
 		if grab_all:
 
 			self.grab_config()
+			self.grab_cmdline()
 			self.grab_package_list()
 			self.grab_apt_logs()
+			self.grab_fstab()
+			self.grab_advancedsettings()
+			self.grab_sources()
+			self.grab_keyboard()
 			self.grab_kodi_logs()
 
 		else:
@@ -57,17 +61,27 @@ class Main(object):
 				self.grab_kodi_logs()
 			if config:
 				self.grab_config()
+			if cmdline:
+				self.grab_cmdline()
 			if packages:
 				self.grab_package_list()
 			if apt:
 				self.grab_apt_logs()
+			if advancedsettings:
+				self.grab_advancedsettings()
+			if keyboard:
+				self.grab_keyboard()
+			if fstab:
+				self.grab_fstab()
+			if sources:
+				self.grab_sources()
 
 		self.tmp_log_location = '/var/tmp/uploadlog.txt'
 
 		with open(self.tmp_log_location, 'w') as f:
 			f.writelines(self.log_list)
 
-		with os.popen('curl -X POST -s -T "/var/tmp/uploadlog.txt" http://paste.osmc.io/documents') as f:
+		with os.popen('curl -X POST -s -T "%s" http://paste.osmc.io/documents' % self.tmp_log_location) as f:
 			line = f.readline()
 			key = line.replace('{"key":"','').replace('"}','')
 
@@ -75,53 +89,8 @@ class Main(object):
 
 		log(self.url)
 
-		if action == '0':
+		ok = xbmcgui.Dialog().ok(lang(32013), lang(32014) % self.url)
 
-			ok = xbmcgui.Dialog().ok(lang(32013), lang(32014) % self.url)
-
-		elif action == '1':
-
-			self.send_email('url')
-
-			ok = xbmcgui.Dialog().ok(lang(32013), lang(32015))
-
-		elif action == '2':
-
-			self.send_email('log')
-
-			ok = xbmcgui.Dialog().ok(lang(32013), lang(32015))
-
-
-	def send_email(self, item):
-
-		try:
-			recipient = 'subliminal.karnage@gmail.com'
-
-			body = '<table border="1">'
-
-			if item == 'log':
-
-				body += '\n'.join(self.log_list)
-
-			else:
-				body += self.url
-
-			body += '</table>'
-
-			msg = MIMEText(body, 'html')
-			msg['Subject'] = 'OSMC Log Uploader'
-			msg['From'] = 'OSMC'
-			msg['To'] = self.email
-			msg['X-Mailer'] = 'OSMC Log Uploader'
-
-			smtp = smtplib.SMTP('alt4.gmail-smtp-in.l.google.com')
-
-			smtp.sendmail(msg['From'], msg['To'], msg.as_string())
-			smtp.quit()
-		
-		except:
-
-			ok = xbmcgui.Dialog().ok(lang(32013), lang(32014) % self.url)
 
 	def grab_kodi_logs(self):
 
@@ -135,6 +104,7 @@ class Main(object):
 				self.log_list.extend(lines[len(lines)-500:])
 		except:
 			self.log_list.extend(['kodi logs not found'])
+
 
 	def grab_config(self):
 
@@ -168,6 +138,75 @@ class Main(object):
 				self.log_list.extend(f.readlines)
 		except:
 			self.log_list.extend(['apt log not found'])
+
+
+	def grab_cmdline(self):
+
+		self.log_list.extend(['\n====================== Pi Cmdline ======================\n'])
+
+		location = '/boot/cmdline.txt'
+
+		try:
+			with open (location, 'r') as f:
+				self.log_list.extend(f.readlines)
+		except:
+			self.log_list.extend(['cmdline.txt not found'])
+
+
+	def grab_advancedsettings(self):
+
+		self.log_list.extend(['\n====================== Advanced Settings ======================\n'])
+
+		location = '/home/osmc/.kodi/userdata/advancedsettings.xml'
+
+		try:
+			with open (location, 'r') as f:
+				lines = f.readlines
+				self.log_list.extend(f.readlines)
+		except:
+			self.log_list.extend(['advanced settings not found'])
+
+
+	def grab_sources(self):
+
+		self.log_list.extend(['\n====================== sources.xml ======================\n'])
+		
+		location = '/home/osmc/.kodi/userdata/sources.xml'
+
+		try:
+			with open (location, 'r') as f:
+				lines = f.readlines
+				self.log_list.extend(f.readlines)
+		except:
+			self.log_list.extend(['sources.xml not found'])
+
+
+	def grab_fstab(self):
+
+		self.log_list.extend(['\n====================== fstab ======================\n'])
+
+		location = '/etc/fstab'
+
+		try:
+			with open (location, 'r') as f:
+				self.log_list.extend(f.readlines)
+		except:
+			self.log_list.extend(['fstab not found'])
+
+
+	def grab_keyboard(self):
+
+		self.log_list.extend(['\n====================== keyboard.xml ======================\n'])
+
+		location = '/home/osmc/.kodi/userdata/keyboard.xml'
+
+		try:
+			with open (location, 'r') as f:
+				lines = f.readlines
+				self.log_list.extend(f.readlines)
+		except:
+			self.log_list.extend(['keyboard.xml not found'])
+
 
 if __name__ == "__main__":
 	Main()		
