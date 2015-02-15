f631e8b6da8a4d44ed3ec0e868c00dfcbe0200ef
Sam Nazarko <email@samnazarko.co.uk>
2015-02-15 18:36:50 +0000

    [package] [wireless-firmware-osmc] Add support for Broadcom 43xx chipsets
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

--- package/wireless-firmware-osmc/build.sh
+++ package/wireless-firmware-osmc/build.sh
@@ -7,11 +7,11 @@ WD="/tmp/fw-dump"
 
 function install_fw_from_deb()
 {
-	DEB_BASE="http://ftp.debian.org/debian/pool/non-free/f/firmware-nonfree/"
+	DEB_BASE="http://ftp.debian.org/debian/pool/non-free/f/firmware-nonfree"
 	if [ -d $WD ]; then rm -rf $WD > /dev/null 2>&1; fi
 	mkdir -p $WD
 	pushd $WD
-	wget ${DEB_BASE}$1
+	wget ${DEB_BASE}/${1}
 	if [ $? != 0 ]; then echo "Firmware download failed" && exit 1; fi
 	dpkg -x ${1} .
 	popd
@@ -25,10 +25,12 @@ make clean
 RALINK_DEB="firmware-ralink_0.43_all.deb"
 REALTEK_DEB="firmware-realtek_0.43_all.deb"
 ATHEROS_DEB="firmware-atheros_0.43_all.deb"
+BRCM_DEB="firmware-brcm80211_0.43_all.deb"
 
 mkdir -p files/lib/firmware
 install_fw_from_deb ${RALINK_DEB}
 install_fw_from_deb ${REALTEK_DEB}
 install_fw_from_deb ${ATHEROS_DEB}
+install_fw_from_deb ${BRCM_DEB}
 
 dpkg -b files/ wireless-firmware-osmc.deb
