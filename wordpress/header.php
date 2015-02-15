9ed10b51312b4a207af35c09c6564f82d080ea57
Mark Theis Madsen <marktheism@gmail.com>
2015-02-15 19:04:34 +0100

    [wp] disk image line move.

--- wordpress/header.php
+++ wordpress/header.php
@@ -45,7 +45,7 @@
 		<link rel="alternate" type="application/rss+xml" title="OSMC Feed" href="/feed?cat=-42" />
 		<?php wp_head(); ?>
         <!-- inject:css -->
-        <link type="text/css" rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/library/style/css/style-77fd7fe7.css">
+        <link type="text/css" rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/library/style/css/style-f6f041cb.css">
         <!-- endinject -->
         
 	</head>
