3193d9846d06dc28ee1974da2042c681c536f1b8
gerald.barker <gezb@users.noreply.github.com>
2015-02-18 22:42:52 +0000

    [Package] [mediacenter-addon-osmc] make sure we update preseed.cfg file with any network config changes

--- package/mediacenter-addon-osmc/src/script.module.osmcsetting.networking/resources/lib/osmc_network.py
+++ package/mediacenter-addon-osmc/src/script.module.osmcsetting.networking/resources/lib/osmc_network.py
@@ -11,15 +11,18 @@ ETHERNET_PATH = '/net/connman/service/ethernet'
 RUNNING_NETWORK_DETAILS_FILE = '/proc/cmdline'
 # but we want to update here - this gets copied to /proc/ as part og boot
 UPDATE_NETWORK_DETAILS_FILE = '/boot/cmdline'
+PREESEED_TEMP_LOCATION = '/tmp/preseed.tmp'
+PREESEED_LOCATION = '/boot/preseed.cfg'
 
 manager = connman.get_manager_interface()
+
 
 def get_network_settings():
     for entry in manager.GetServices():
         path = entry[0]
         dbus_properties = entry[1]
         if path.startswith(ETHERNET_PATH):
-            props = {'path' : path, 'NFS' : False}
+            props = {'path': path}
             # get IPv4 Data
             ipv4_props = dbus_properties['IPv4']
             props['Method'] = str(ipv4_props['Method'])
@@ -30,7 +33,7 @@ def get_network_settings():
             nameservers = dbus_properties['Nameservers']
             count = 1
             for nameserver in nameservers:
-                props['DNS_'+str(count)] = str(nameserver)
+                props['DNS_' + str(count)] = str(nameserver)
                 count += 1
             # get state
             props['State'] = str(dbus_properties['State'])
@@ -50,7 +53,6 @@ def get_network_settings():
         if cmdline_value.startswith('ip='):
             ip_value = cmdline_value[3:]
 
-    nfs_settings = None
     if nfs_found:
         if ip_value == 'dhcp':
             nfs_settings = get_non_connman_connection_details()
@@ -61,22 +63,22 @@ def get_network_settings():
 
         nfs_settings['Interface'] = 'eth0 (NFS)'
         nfs_settings['State'] = 'online'
+        nfs_settings['path'] = 'nfs'
         if not has_internet_connection():
             nfs_settings['State'] = 'ready'
         return nfs_settings
 
     return None
-        
-        
+
+
 def apply_network_changes(settings_dict):
-    if settings_dict['Method'] in ['manual', 'dhcp']: # non NFS setup
+    if settings_dict['Method'] in ['manual', 'dhcp']:  # non NFS setup
         path = settings_dict['path']
         service = connman.get_service_interface(path)
-        properties = service.GetProperties()
-        ipv4_configuration = { 'Method': make_variant(settings_dict['Method']) }
-        ipv4_configuration['Address'] = make_variant(settings_dict['Address'])
-        ipv4_configuration['Netmask'] = make_variant(settings_dict['Netmask'])
-        ipv4_configuration['Gateway'] = make_variant(settings_dict['Gateway'])
+        ipv4_configuration = {'Method': make_variant(settings_dict['Method']),
+                              'Address': make_variant(settings_dict['Address']),
+                              'Netmask': make_variant(settings_dict['Netmask']),
+                              'Gateway': make_variant(settings_dict['Gateway'])}
         service.SetProperty('IPv4.Configuration', ipv4_configuration)
         time.sleep(2)
         dns = []
@@ -87,6 +89,7 @@ def apply_network_changes(settings_dict):
         # duplicate SetProperty message works around connman dns forwarder bug
         service.SetProperty('Nameservers.Configuration', dbus.Array(dns, signature=dbus.Signature('s')))
         service.SetProperty('Nameservers.Configuration', dbus.Array(dns, signature=dbus.Signature('s')))
+        update_preseed_file(settings_dict)
     elif settings_dict['Method'].startswith('nfs_'):
         ip_value = None
         if settings_dict['Method'] == 'nfs_dhcp':
@@ -95,32 +98,35 @@ def apply_network_changes(settings_dict):
             ip_value = create_cmdline_nfs_manual_string(settings_dict)
         if ip_value:
             update_cmdline_file(UPDATE_NETWORK_DETAILS_FILE, 'ip', ip_value)
+        update_preseed_file(settings_dict)
+
 
 def make_variant(string):
     return dbus.String(string, variant_level=1)
 
 
 def update_cmdline_file(file_path, key, value):
-    key_found = False
     cmdline_file = open(file_path, 'r')
     cmdline_data = cmdline_file.read()
     cmdline_file.close()
     cmdline_values = []
     for cmdline_value in cmdline_data.split(' '):
         if cmdline_value.startswith(key + '='):
-            key_found = True
             cmdline_values.extend([key + '=' + value])
         else:
             cmdline_values.extend([cmdline_value])
-    updated_cmdline = ' '.join(cmdline_values) # join the list with a space
+    updated_cmdline = ' '.join(cmdline_values)  # join the list with a space
     cmdline_file = open(file_path, 'w')
     cmdline_file.write(updated_cmdline)
     cmdline_file.close()
+
 
 '''
   | Address   || gateway   | netmask     |    |    |   | DNS_1 | DNS_2
   192.168.1.20::192.168.1.1:255.255.255.0:osmc:eth0:off:8.8.8.8:8.8.4.4
 '''
+
+
 def split_nfs_static_cmdlline(value):
     connection_details = value.split(':')
     print connection_details
@@ -131,14 +137,16 @@ def split_nfs_static_cmdlline(value):
         nfs_static['DNS_2'] = connection_details[8]
     return nfs_static
 
+
 def create_cmdline_nfs_manual_string(settings_dict):
-    cmd_string = settings_dict['Address'] +'::' + settings_dict['Gateway'] + ':' + settings_dict['Netmask']
+    cmd_string = settings_dict['Address'] + '::' + settings_dict['Gateway'] + ':' + settings_dict['Netmask']
     cmd_string += ':osmc:eth0:off'
     if 'DNS_1' in settings_dict:
         cmd_string = cmd_string + ':' + settings_dict['DNS_1']
     if 'DNS_2' in settings_dict:
         cmd_string = cmd_string + ':' + settings_dict['DNS_2']
     return cmd_string
+
 
 def get_non_connman_connection_details():
     device = 'eth0'
@@ -151,15 +159,15 @@ def get_non_connman_connection_details():
     (name, address, netmask) = data[0]
     device_settings = {'Address': address, 'Netmask': netmask}
     # parse resolve.conf for DNS
-    resolve_conf_file = open('/etc/resolv.conf','r')
+    resolve_conf_file = open('/etc/resolv.conf', 'r')
     resolve_conf_data = resolve_conf_file.read()
     resolve_conf_file.close()
     count = 1
     for line in resolve_conf_data.split('\n'):
         if line.startswith('nameserver'):
             ip = line.replace('nameserver ', '').strip()
-            if len(ip.split('.')) == 4: # ipV4 address
-                device_settings['DNS_'+str(count)] = str(ip)
+            if len(ip.split('.')) == 4:  # ipV4 address
+                device_settings['DNS_' + str(count)] = str(ip)
     # get default gateway
     proc = subprocess.Popen(['/sbin/route', '-n'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     (route_data, stderr) = proc.communicate()
@@ -171,11 +179,43 @@ def get_non_connman_connection_details():
 
     return device_settings
 
+
 def has_internet_connection():
     try:
-        ping_response = subprocess.Popen(['/bin/ping', '-c1', '-w2', '8.8.8.8'], stdout=subprocess.PIPE).stdout.read()
+        subprocess.Popen(['/bin/ping', '-c1', '-w2', '8.8.8.8'], stdout=subprocess.PIPE).stdout.read()
         return True
     except:
         return False
 
+
+def update_preseed_file(settings_dict):
+    # reed the current data in
+    preseed_file = open(PREESEED_LOCATION, 'r')
+    preseed_data = preseed_file.read()
+    preseed_file.close()
+    # re-open file for writing
+    preseed_file = open(PREESEED_TEMP_LOCATION, 'w')
+    # copy everything except the network entries to a temp file
+    for entry in preseed_data.split("\n"):
+        if not entry.startswith('d-i network/') and len(entry.strip()) > 0:
+            # write this entry back to the file as is
+            preseed_file.write(entry + "\n")
+
+    # write the required network options to the temp file
+    if 'eth' in settings_dict['path'] or settings_dict['path'] == 'nfs':
+        preseed_file.write('d-i network/interface string eth\n')
+    if 'dhcp' in settings_dict['Method']:
+        preseed_file.write('d-i network/auto boolean true\n')
+    if 'manual' in settings_dict['Method']:
+        preseed_file.write('d-i network/auto boolean false\n')
+        preseed_file.write('d-i network/ip string ' + settings_dict['Address'] + '\n')
+        preseed_file.write('d-i network/mask string ' + settings_dict['Netmask'] + '\n')
+        preseed_file.write('d-i network/gw string ' + settings_dict['Gateway'] + '\n')
+        if 'DNS_1' in settings_dict:
+            preseed_file.write('d-i network/dns1 string ' + settings_dict['DNS_1'] + '\n')
+        if 'DNS_2' in settings_dict:
+            preseed_file.write('d-i network/dns2 string ' + settings_dict['DNS_2'] + '\n')
+        preseed_file.write('d-i network/gw string ' + settings_dict['Gateway'] + '\n')
+    preseed_file.close()
+    subprocess.call(['sudo', 'mv', PREESEED_TEMP_LOCATION, PREESEED_LOCATION])
 
