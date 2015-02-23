29ce3ca920377a12f0fa6396c1296bae64db5848
Sam Nazarko <email@samnazarko.co.uk>
2015-02-23 02:23:33 +0000

    [wordpress] Use UKFast for CITM operations
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

--- wordpress/citm.php
+++ wordpress/citm.php
@@ -1,6 +1,6 @@
 <?php
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, "http://173.255.206.49/osmc/download/" . $_GET["citm"]);
+curl_setopt($ch, CURLOPT_URL, "http://46.37.189.135/osmc/download/" . $_GET["citm"]);
 curl_setopt($ch, CURLOPT_HEADER, 0);
 curl_setopt($ch, CURLOPT_CONNECTTIMEOUT , 20);
 curl_setopt($ch, CURLOPT_TIMEOUT, 20); //timeout in seconds
