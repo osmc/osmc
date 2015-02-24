4d84509c464c0fd2c36d31a9357fdcd465589729
KODeKarnage <sublime.kev@gmail.com>
2015-02-24 11:58:29 +1100

    [package][mediacenter-addon-osmc] Main: added better handling for main GUI failure to launch

--- package/mediacenter-addon-osmc/src/service.osmc.settings/default.py
+++ package/mediacenter-addon-osmc/src/service.osmc.settings/default.py
@@ -1,16 +1,36 @@
 import socket
 import xbmc
+import xbmcgui
+import xbmcaddon
+
+__addon__        = xbmcaddon.Addon()
+__setting__      = __addon__.getSetting
+DIALOG           = xbmcgui.Dialog()
+
+def lang(id):
+	san = __addon__.getLocalizedString(id).encode( 'utf-8', 'ignore' )
 
 def log(message):
 	xbmc.log(str(message), level=xbmc.LOGDEBUG)
+	return san 
+
 
 log('default started')
 
-address = '/var/tmp/osmc.settings.sockfile'
-sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-sock.connect(address)
-sock.sendall('open')
-sock.close()
+
+try:
+
+	address = '/var/tmp/osmc.settings.sockfile'
+	sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+	sock.connect(address)
+	sock.sendall('open')
+	sock.close()
+
+except:
+
+	log('default failed to open')
+
+	ok = DIALOG.ok(lang(32007), lang(32005), lang(32006))
 
 log('default closing')
 
