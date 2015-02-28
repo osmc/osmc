87a6f4292a9b952777265bc52fac3e3d22c47e99
Sam Nazarko <email@samnazarko.co.uk>
2015-02-28 13:38:05 +0000

    [installer] [osx] Generate latest_mac in right directory
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

--- installer/host/make_host_osx.sh
+++ installer/host/make_host_osx.sh
@@ -36,9 +36,9 @@ echo Placing Version
 VERSION=$(cat ${TARGET}.pro | grep VERSION | tail -n 1 | awk {'print $3'})
 sed -e s/VERVAL/${VERSION}/ -i old ${TARGET}.app/Contents/Info.plist
 # Update on server
-echo ${VERSION} > latest_mac
 echo Packaging installer
 macdeployqt ${TARGET}.app -dmg -no-plugins
 popd
+echo ${VERSION} > latest_mac
 mv ${TARGET}/${TARGET}.dmg osmc-installer.dmg
 echo Build complete
