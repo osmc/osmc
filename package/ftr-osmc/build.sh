dfea299a6eba5b64f627ec66c368104ce511c31b
Sam Nazarko <email@samnazarko.co.uk>
2015-03-01 22:23:35 +0000

    [package] [ftr-osmc] Fix order
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

--- package/ftr-osmc/build.sh
+++ package/ftr-osmc/build.sh
@@ -9,13 +9,13 @@ if [ $? == 0 ]
 then
 	echo -e "Building ftr"
 	make clean
+	sed '/Package/d' -i files/DEBIAN/control
+	echo "Package: ${1}-ftr-osmc" >> files/DEBIAN/control
+	fix_arch_ctl "files/DEBIAN/control"
 	pushd files
 	test "$1" == rbp1 && install_patch "../patches" "rbp1"
 	test "$1" == rbp2 && install_patch "../patches" "rbp1"
 	test "$1" == atv && install_patch "../patches" "atv"
-	sed '/Package/d' -i files/DEBIAN/control
-	echo "Package: ${1}-ftr-osmc" >> files/DEBIAN/control
-	fix_arch_ctl "files/DEBIAN/control"
 	popd
 	dpkg -b files/ ftr-osmc.deb
 fi
