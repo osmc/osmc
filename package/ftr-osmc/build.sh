11b79f28e04cc5559059ec8230be073516819c75
Sam Nazarko <email@samnazarko.co.uk>
2015-03-01 21:54:41 +0000

    [package] [ftr-osmc] Make ftr-osmc device dependent
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

--- package/ftr-osmc/build.sh
+++ package/ftr-osmc/build.sh
@@ -3,6 +3,13 @@
 
 #!/bin/bash
 
+. ../common.sh
+
 echo -e "Building ftr"
 make clean
+pushd files
+test "$1" == rbp1 && install_patch "../patches" "rbp1"
+test "$1" == rbp2 && install_patch "../patches" "rbp1"
+test "$1" == atv && install_patch "../patches" "atv"
+popd
 dpkg -b files/ ftr-osmc.deb
