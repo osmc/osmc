14b60b5a638290bc61481108433b03c14ec11fbe
Wietse van Buitenen <thewiep@gmail.com>
2015-03-07 13:25:38 +0100

    [installer] [noobs] Add version when downloading filesystem

--- installer/noobs/build.sh
+++ installer/noobs/build.sh
@@ -10,9 +10,9 @@ function build_fs_image()
 	echo -e "Downloading latest filesystem for RBP Version ${1}"
 	date=$(date +%Y%m%d)
 	count=150
-	while [ $count -gt 0 ]; do wget --spider -q ${DOWNLOAD_URL}/filesystems/osmc-rbp-filesystem-${date}.tar.xz
+	while [ $count -gt 0 ]; do wget --spider -q ${DOWNLOAD_URL}/filesystems/osmc-rbp${1}-filesystem-${date}.tar.xz
 		   if [ "$?" -eq 0 ]; then
-				wget ${DOWNLOAD_URL}/filesystems/osmc-rbp-filesystem-${date}.tar.xz -O filesystem.tar.xz
+				wget ${DOWNLOAD_URL}/filesystems/osmc-rbp${1}-filesystem-${date}.tar.xz -O filesystem.tar.xz
 				break
 		   fi
 		   date=$(date +%Y%m%d --date "yesterday $date")
