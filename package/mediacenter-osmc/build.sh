c5f1a136d4bd378a2859f464faac87b7506aaf5b
Sam Nazarko <email@samnazarko.co.uk>
2015-03-08 11:05:01 +0000

    [package] [mediacenter-osmc] fix check for pi2
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

--- package/mediacenter-osmc/build.sh
+++ package/mediacenter-osmc/build.sh
@@ -144,7 +144,7 @@ then
 		--disable-pulse \
 		--disable-projectm
 	# Raspberry Pi Configuration
-	if [ "$1" == "rbp" ]; then $PI2="--enable-neon"; fi
+	if [ "$1" == "rbp2" ]; then $PI2="--enable-neon"; fi
 	if [ "$1" == "rbp1" ] || [ "$1" == "rbp2" ]; then
 	LIBRARY_PATH+=/opt/vc/lib && \
 	export CFLAGS="-I/opt/vc/include -I/usr/include/afpfs-ng -I/opt/vc/include/interface -I/opt/vc/include/interface/vcos/pthreads -I/opt/vc/include/interface/vmcs_host/linux" && \
