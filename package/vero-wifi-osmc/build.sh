21c6b0cd62f01be3cce041a819da07c2533d8184
Sam Nazarko <email@samnazarko.co.uk>
2015-03-09 11:34:48 +0000

    [package] [vero-wifi-osmc] Broadcom BCM4330 WiFi implementation. Pull down UART RTS on /dev/mxc3 to upload firmware via patchram. Bring up device with hciconfig after. Hard coded MAC because no way of determining Mac.
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

---
+++ package/vero-wifi-osmc/build.sh
@@ -0,0 +1,23 @@
+# (c) 2014-2015 Sam Nazarko
+# email@samnazarko.co.uk
+
+#!/bin/bash
+
+. ../common.sh
+
+# Build in native environment
+build_in_env "${1}" $(pwd) "vero-wifi-osmc"
+if [ $? == 0 ]
+then
+	echo -e "Building vero-wifi-osmc"
+	out=$(pwd)/files
+	make clean
+	mkdir -p ${out}/usr/bin
+	pushd src
+	gcc brcm_patchram_plus.c -o ${out}/usr/bin/brcm_patchram_plus
+	if [ $? != 0 ]; then echo "Error occured during build" && exit 1; fi
+	strip_files "${out}"
+	popd
+	dpkg -b files/ vero-wifi-osmc.deb
+fi
+teardown_env "${1}"
