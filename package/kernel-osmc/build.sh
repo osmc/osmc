ceda738f443e15f7508647f6995716e64a8b4885
Sam Nazarko <email@samnazarko.co.uk>
2015-03-12 23:49:49 +0000

    [package] [kernel-osmc] More generic pushd src path
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

--- package/kernel-osmc/build.sh
+++ package/kernel-osmc/build.sh
@@ -36,7 +36,7 @@ then
 	email := email@samnazarko.co.uk
 	priority := High" >/etc/kernel-pkg.conf
 	JOBS=$(if [ ! -f /proc/cpuinfo ]; then mount -t proc proc /proc; fi; cat /proc/cpuinfo | grep processor | wc -l && umount /proc/ >/dev/null 2>&1)
-	pushd src/linux-*
+	pushd src/*linux*
 	if [ "$1" == "rbp1" ] || [ "$1" == "rbp2" ]; then install_patch "../../patches" "rbp"; fi
 	install_patch "../../patches" "${1}"
 	make-kpkg --stem $1 kernel_image --append-to-version -${REV}-osmc --jobs $JOBS --revision $REV
