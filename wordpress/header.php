03099e223b751a6b9bb14bca594464e75550a80f
Mark Theis Madsen <marktheis@users.noreply.github.com>
2015-03-23 14:45:55 +0100

    [wp] fix list style + wiki list

--- wordpress/header.php
+++ wordpress/header.php
@@ -45,7 +45,7 @@
 		<link rel="alternate" type="application/rss+xml" title="OSMC Feed" href="/feed?cat=-42" />
 		<?php wp_head(); ?>
         <!-- inject:css -->
-        <link type="text/css" rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/library/style/css/style-3c0064da.css">
+        <link type="text/css" rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/library/style/css/style-a2306622.css">
         <!-- endinject -->
         
 	</head>
