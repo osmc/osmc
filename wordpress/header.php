549d68b89df08d4485bb228c28fca3057c00f8b0
Mark Theis Madsen <marktheis@users.noreply.github.com>
2015-04-05 12:04:26 +0200

    [wp] various style improvements & fixes

--- wordpress/header.php
+++ wordpress/header.php
@@ -45,7 +45,7 @@
 		<link rel="alternate" type="application/rss+xml" title="OSMC Feed" href="/feed?cat=-42" />
 		<?php wp_head(); ?>
         <!-- inject:css -->
-        <link type="text/css" rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/library/style/css/style-882d1e6d.css">
+        <link type="text/css" rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/library/style/css/style-f652e6ac.css">
         <!-- endinject -->
         
 	</head>
