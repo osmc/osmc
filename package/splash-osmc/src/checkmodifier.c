ffd99ce67a37b3fba617b33c7a8fe7cc045f3a7b
DBMandrake <sbyrnand@gmail.com>
2015-04-22 19:35:33 +0100

    [package] [splash-osmc] Merge comments.

--- package/splash-osmc/src/checkmodifier.c
+++ package/splash-osmc/src/checkmodifier.c
@@ -3,20 +3,19 @@
  *
  * (c) 2014-2015 Sam Nazarko
  * email@samnazarko.co.uk
+ *
+ * Return kernel shift_state (depressed modifier keys) bitmask from /dev/console. Requires root access.
+ * 0 = no modifier keys
+ * 1 = Left/Right Shift
+ * 2 = Alt Gr (right ALT)
+ * 4 = Left/Right Ctrl
+ * 8 = Left Alt
  */
 
 #include <sys/ioctl.h>
 #include <asm/ioctls.h>
 #include <stdio.h>
 #include <fcntl.h>
-
-/*      Return kernel shift_state (depressed modifier keys) bitmask from /dev/console. Needs root access.
-        0 = no modifier keys
-        1 = Left/Right Shift
-        2 = Alt Gr (right ALT)
-        4 = Left/Right Ctrl
-        8 = Left Alt
-*/
 
 int main(int argc, char *argv[])
 {
