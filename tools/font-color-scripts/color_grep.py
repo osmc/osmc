0d6a707fad70e17b7f812ebff800b7a7f1a9e6c2
KODeKarnage <sublime.kev@gmail.com>
2015-04-27 15:25:28 +1000

    [package][mediacenter-addon-osmc] tools: added script for finding and replacing fonts

---
+++ tools/font-color-scripts/color_grep.py
@@ -0,0 +1,73 @@
+import os
+
+
+# Unique Entries							Original Color					Replacement
+COLOR_CONVERSIONS = {		
+				'<backgroundcolor>%s</backgroundcolor>'		: {'orig': '0xff18394a',	'repl': 'ff203b4c', 	'count': 0},
+				'<colordiffuse>%s</colordiffuse>'			: {'orig': '55f1f1f1',		'repl': 'ff0b2430', 	'count': 0},
+				'<colordiffuse>%s</colordiffuse>'			: {'orig': 'FFFFFFFF',		'repl': 'ff0b2430', 	'count': 0},
+				'<disabledcolor>%s</disabledcolor>'			: {'orig': 'ff000000',		'repl': 'ff000000', 	'count': 0},
+				'<focusedcolor>%s</focusedcolor>'			: {'orig': 'ffffffff',		'repl': 'ffe0b074', 	'count': 0},
+				'<selectedcolor>%s</selectedcolor>'			: {'orig': 'FF8FF5EE',		'repl': 'fff1f1f1', 	'count': 0},
+				'<selectedcolor>%s</selectedcolor>'			: {'orig': 'ffffa500',		'repl': 'fff1f1f1', 	'count': 0},
+				'<shadowcolor>%s</shadowcolor>'				: {'orig': '44000000',		'repl': '44000000', 	'count': 0},
+				'<shadowcolor>%s</shadowcolor>'				: {'orig': 'ff000000',		'repl': '44000000', 	'count': 0},
+				'<textcolor>%s</textcolor>'					: {'orig': '66ffffff',		'repl': 'ff808080', 	'count': 0},
+				'<textcolor>%s</textcolor>'					: {'orig': '88f0f0f0',		'repl': 'ff808080', 	'count': 0},
+				'<textcolor>%s</textcolor>'					: {'orig': 'ffffffff',		'repl': 'ff808080', 	'count': 0},
+					}
+
+GOFFS_COLORS = 	{
+				'ColorFO'				: 'ffe0b074',
+				'ColorNF'				: 'ff808080',
+				'ColorHeading'			: 'ffd6d6d6',
+				'ColorDetails'			: 'ff707070',
+				'ColorSelected'			: 'fff1f1f1',
+				'ColorDisabled'			: 'ff000000',
+				'ColorInvalid'			: 'ffa10000',
+				'ShadowColor'			: '44000000',
+				'WindowBackgroundColor'	: 'ff0b2430',
+				'DialogBackgroundColor'	: 'ff203b4c',
+				}
+
+
+
+base_folder = 'C:\\Users\\kodonne\\Desktop\\GitHub\\osmc\\package\\mediacenter-addon-osmc\\src'
+
+for root, dirs, files in os.walk(base_folder, topdown=False):
+	if root == 'C:\\Users\\kodonne\\Desktop\\GitHub\\osmc\\package\\mediacenter-addon-osmc\\src\\script.module.elementtree':
+		continue
+
+	for name in files:
+		if name.endswith('.xml'):
+			new_lines = []
+			with open(os.path.join(root, name), 'r') as f:
+				lines = f.readlines()
+				conversion = False
+				for line in lines:
+
+					# ignore the line if it is commented out
+					if line.lstrip().startswith('<!--'):
+						new_line = line
+
+					else:
+
+						for key in COLOR_CONVERSIONS:
+							search_key = key % COLOR_CONVERSIONS[key]['orig']
+							if search_key in line.replace('<!--%s-->' % search_key, ''):
+								COLOR_CONVERSIONS[key]['count'] = COLOR_CONVERSIONS[key]['count'] + 1
+								conversion = True
+								line = line.replace('\n','').replace('\t','')
+								new_line = line.replace(search_key, key % COLOR_CONVERSIONS[key]['orig']) + '<!--former_line%s-->\n' % line
+								break
+						else: # if nobreak
+							new_line = line
+
+					new_lines.append(new_line)
+
+			if conversion:
+				with open(os.path.join(root, name), 'w') as f:		
+					f.writelines(new_lines)
+
+for k, v in COLOR_CONVERSIONS.iteritems():
+	print v['count'], (6 - len(str(v['count'])) ) * ' ', k					
---
+++ tools/font-color-scripts/font_and_color_scripts.py
@@ -0,0 +1,108 @@
+import os
+import collections
+
+
+FONT_CONVERSIONS = 		{
+						'osmc_addon_OLD_Font72' 	: {'repl': 'osmc_addon_720_XLarge', 	'count': 0},
+						'osmc_addon_OLD_Font48' 	: {'repl': 'osmc_addon_XLarge', 		'count': 0},
+						'osmc_addon_OLD_Font42' 	: {'repl': 'osmc_addon_Large', 			'count': 0},
+						'osmc_addon_OLD_Font36' 	: {'repl': 'osmc_addon_Med', 			'count': 0},
+						'osmc_addon_OLD_Font33' 	: {'repl': 'osmc_addon_Small', 			'count': 0},
+						'osmc_addon_OLD_Font30' 	: {'repl': 'osmc_addon_XSmall', 		'count': 0},
+						'osmc_addon_OLD_Font28' 	: {'repl': 'osmc_addon_XSmall', 		'count': 0},
+						'osmc_addon_OLD_Font27' 	: {'repl': 'osmc_addon_XSmall', 		'count': 0},
+						'osmc_addon_OLD_Font25' 	: {'repl': 'osmc_addon_XSmall', 		'count': 0},
+
+						'osmc_addon_Font26' 		: {'repl': 'osmc_addon_XSmall', 		'count': 0},
+						'osmc_addon_font13' 		: {'repl': 'osmc_addon_font13', 		'count': 0},
+
+						'osmc_addon_720_OLD_Font72' : {'repl': 'osmc_addon_720_XLarge', 	'count': 0},
+						'osmc_addon_720_OLD_Font48' : {'repl': 'osmc_addon_720_XLarge', 	'count': 0},
+						'osmc_addon_720_OLD_Font42' : {'repl': 'osmc_addon_720_Large', 		'count': 0},
+						'osmc_addon_720_OLD_Font36' : {'repl': 'osmc_addon_720_Med', 		'count': 0},
+						'osmc_addon_720_OLD_Font33' : {'repl': 'osmc_addon_720_Small', 		'count': 0},
+						'osmc_addon_720_OLD_Font30' : {'repl': 'osmc_addon_720_XSmall', 	'count': 0},
+						'osmc_addon_720_OLD_Font28' : {'repl': 'osmc_addon_720_XSmall', 	'count': 0},
+						'osmc_addon_720_OLD_Font27' : {'repl': 'osmc_addon_720_XSmall', 	'count': 0},
+						'osmc_addon_720_OLD_Font25' : {'repl': 'osmc_addon_720_XSmall', 	'count': 0},
+
+						'osmc_addon_720_Font60' 	: {'repl': 'osmc_addon_Med', 			'count': 0},
+						'osmc_addon_720_Font40' 	: {'repl': 'osmc_addon_XSmall', 		'count': 0},
+						'osmc_addon_720_Font30' 	: {'repl': 'osmc_addon_720_XSmall', 	'count': 0},
+						'osmc_addon_720_Font26' 	: {'repl': 'osmc_addon_720_font13', 	'count': 0},
+						'osmc_addon_720_font13' 	: {'repl': 'osmc_addon_720_font13', 	'count': 0},
+
+						}
+
+
+# test = '<font>osmc_addon_720_OLD_Font72</font>'
+# key = 'osmc_addon_720_OLD_Font72'
+
+# new_line = test[:test.index(key)] + FONT_CONVERSIONS[key]['repl'] + test[test.index(key)+len(key):] + '<!--%s-->' % key
+
+# print new_line
+
+cnt = []
+
+base_folder = 'C:\\Users\\kodonne\\Desktop\\GitHub\\osmc\\package\\mediacenter-addon-osmc\\src'
+
+for root, dirs, files in os.walk(base_folder, topdown=False):
+	if root == 'C:\\Users\\kodonne\\Desktop\\GitHub\\osmc\\package\\mediacenter-addon-osmc\\src\\script.module.elementtree':
+		continue
+	for name in files:
+
+		if name.endswith('.xml'):
+			new_lines = []
+			with open(os.path.join(root, name), 'r') as f:
+				lines = f.readlines()
+				conversion = False
+				for line in lines:
+
+					# ignore the line if it is commented out
+					if line.lstrip().startswith('<!--'):
+						new_line = line
+
+					else:
+
+						for key in FONT_CONVERSIONS:
+							if key in line.replace('<!--%s-->' % key, ''):
+								FONT_CONVERSIONS[key]['count'] = FONT_CONVERSIONS[key]['count'] + 1
+								conversion = True
+								line = line.replace('\n','').replace('\t','')
+								new_line = line[:line.index(key)] + FONT_CONVERSIONS[key]['repl'] + line[line.index(key)+len(key):] + '<!--former_line%s-->\n' % line
+								break
+						else: # if nobreak
+							new_line = line
+
+					new_lines.append(new_line)
+
+			if conversion:
+				with open(os.path.join(root, name), 'w') as f:		
+					f.writelines(new_lines)
+
+for k, v in FONT_CONVERSIONS.iteritems():
+	print v['count'], (6 - len(str(v['count'])) ) * ' ', k
+
+
+# cnt.sort()
+
+# r = ['<!--', '-->','<font>','</font>']
+
+# coll = collections.Counter(cnt)
+# print 'start'
+# for x,v in coll.iteritems():
+# 	for rep in r:
+# 		x = x.replace(rep, '')
+# 	print x.lstrip()
+
+
+
+
+
+
+
+
+
+
+
+
