37c69a8e191dd58190ccaaafbf1c7cce9b22dd31
Mark Theis Madsen <marktheism@gmail.com>
2015-05-04 14:02:28 +0200

    [wp] rm old images

--- wordpress/header.php
+++ wordpress/header.php
@@ -6,10 +6,10 @@
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<title><?php bloginfo('name'); ?><?php wp_title(); ?></title>
-		<meta name="description" content="<?php echo get_bloginfo ( 'description' );  ?>" />
+		<meta name="description" content="<?php echo get_bloginfo ('description');  ?>" />
 		<meta property=og:title content="<?php bloginfo('name'); ?><?php wp_title(); ?>" />
+		<meta property=og:description content="<?php echo get_bloginfo ('description');  ?>" />
 		<meta property=og:image content="<?php echo get_template_directory_uri(); ?>/library/images/osmc_meta.png" />
-		<meta property=og:description content="<?php echo get_bloginfo ( 'description' );  ?>" />
 		<meta name="HandheldFriendly" content="True">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 		
