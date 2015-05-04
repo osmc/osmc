574a7d6805fff4a340510a239c1fd826519c95b9
Mark Theis Madsen <marktheis@users.noreply.github.com>
2015-05-04 03:30:37 +0200

    [wp] New front page.

--- wordpress/footer.php
+++ wordpress/footer.php
@@ -44,9 +44,7 @@
 
 		</div> <?php // end #container ?>
 
-		<?php // all js scripts are loaded in library/bones.php ?>
 		<?php wp_footer(); ?>
-
 	</body>
 
 </html> <?php // end page. what a ride! ?>
--- wordpress/library/bones.php
+++ wordpress/library/bones.php
@@ -131,7 +131,7 @@ function bones_scripts_and_styles() {
 		}
 
 		//adding scripts file in the footer
-		wp_register_script( 'bones-js', get_stylesheet_directory_uri() . '/library/js/scripts.min.js', array( 'jquery' ), '', true );
+		wp_register_script( 'bones-js', get_stylesheet_directory_uri() . '/library/js/scripts_v2.min.js', array( 'jquery' ), '', true );
 
 		// enqueue styles and scripts
 		wp_enqueue_script( 'bones-modernizr' );
