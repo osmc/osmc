07ed9ca589068a5afa5ae2983ee93956e0a7a745
Sam Nazarko <email@samnazarko.co.uk>
2015-05-05 00:52:40 +0100

    [package] [cron-app-osmc] Add Cron to OSMC App Store
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

---
+++ package/cron-app-osmc/build.sh
@@ -0,0 +1,10 @@
+# (c) 2014-2015 Sam Nazarko
+# email@samnazarko.co.uk
+
+#!/bin/bash
+
+. ../common.sh
+
+echo -e "Building package cron-app-osmc"
+publish_applications_any $(pwd) "cron-app-osmc"
+dpkg -b files/ cron-app-osmc.deb
