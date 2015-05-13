ab765fb531a353a1ce92a6fcd2b719bbf897e270
Sam Nazarko <email@samnazarko.co.uk>
2015-05-13 20:59:16 +0100

    [package] [qt-bsp-osmc] Add initial OSMC SDK support for Pi1 and Pi2
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

---
+++ package/qt-bsp-osmc/build.sh
@@ -0,0 +1,54 @@
+# (c) 2014-2015 Sam Nazarko
+# email@samnazarko.co.uk
+
+#!/bin/bash
+
+. ../common.sh
+
+pull_source "http://download.qt-project.org/official_releases/qt/5.4/5.4.1/single/qt-everywhere-opensource-src-5.4.1.tar.gz" "$(pwd)/src"
+if [ $? != 0 ]; then echo -e "Error downloading" && exit 1; fi
+# Build in native environment
+build_in_env "${1}" $(pwd) "qt-bsp-osmc"
+build_return=$?
+if [ $build_return == 99 ]
+then
+	echo -e "Building Qt BSP for OSMC"
+	out=$(pwd)/files
+	make clean
+	sed '/Package/d' -i files/DEBIAN/control
+	update_sources
+	handle_dep "libudev-dev"
+	handle_dep "libdbus-1-dev"
+	handle_dep "lixcb-1-dev"
+	handle_dep "libssl-dev"
+	handle_dep "libasound2-dev"
+	handle_dep "libfreetype6-dev"
+	handle_dep "libpng12-dev"
+	handle_dep "libjpeg62-turbo-dev"
+	handle_dep "zlib1g-dev"
+	echo "Package: ${1}-qt-bsp-osmc" >> files/DEBIAN/control && echo "Package: ${1}-qt-bsp-dev-osmc" >> files-dev/DEBIAN/control && echo "Depends: ${1}-qt-bsp-osmc" >> files-dev/DEBIAN/control
+	if [ "$1" == "rbp1" ] || [ "$1" == "rbp2" ]; then handle_dep "rbp-userland-dev-osmc"; fi
+	if [ "$1" == "vero" ]; then handle_dep "vero-userland-dev-osmc"; fi
+	pushd src/qt-everywhere-opensource*
+	if [ "$1" == "rbp1" ] || [ "$1" == "rbp2" ]; then install_patch "../../patches" "rbp"; fi
+	install_patch "../../patches" "$1"
+	if [ "$1" == "rbp1" ] || [ "$1" == "rbp2" ]; then PLATFORM="-no-directfb -no-linuxfb -eglfs -no-xcb -device linux-rasp-pi-g++ -device-option CROSS_COMPILE=/usr/bin/ -sysroot /"; fi
+        if [ "$1" == "vero" ]; then PLATFORM="-no-directfb -no-linuxfb -eglfs -no-xcb -device linux-g++ -device-option CROSS_COMPILE=/usr/bin/ -sysroot /"; fi
+	./configure --prefix=/usr -release -opensource -confirm-license -c++11 -no-largefile -no-qml-debug -system-zlib \
+	-no-journald -system-libpng -system-freetype -system-libjpeg -openssl -no-pulseaudio -alsa -no-sql-sqlite \
+	-no-sql-db2 -no-sql-ibase -no-sql-mysql -no-sql-oci -no-sql-odbc -no-sql-psql -no-sql-sqlite -no-sql-sqlite2 -no-sql-tds -nomake examples -reduce-exports $PLATFORM
+	$BUILD
+	make install DESTDIR=${out}
+	if [ $? != 0 ]; then echo "Error occured during build" && exit 1; fi
+	strip_files "${out}"
+	popd
+	mkdir -p files-dev/usr
+	mv files/usr/include  files-dev/usr/
+	fix_arch_ctl "files/DEBIAN/control"
+	fix_arch_ctl "files-dev/DEBIAN/control"
+	dpkg -b files/ qt-bsp-osmc.deb
+	dpkg -b files-dev qt-bsp-dev-osmc.deb
+	build_return=$?
+fi
+teardown_env "${1}"
+exit $build_return
