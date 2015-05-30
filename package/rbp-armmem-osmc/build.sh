2cd38b3d91a1ae37b75f7df97602a9e3a67999b2
Sam Nazarko <email@samnazarko.co.uk>
2015-05-30 10:46:02 +0100

    [package] [rbp-armmem-osmc] Always use HEAD for rbp-armmem-osmc regardless of target; use -a7 suffix for RBP2; bump version
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

--- package/rbp-armmem-osmc/build.sh
+++ package/rbp-armmem-osmc/build.sh
@@ -5,15 +5,14 @@
 
 . ../common.sh
 # Build in native environment
-if [ $1 == "rbp1" ]; then pull_source "https://github.com/bavison/arm-mem/archive/cd2c8f9202137c79f7afb77ecb87e713a0800d3c.zip" "$(pwd)/src"; fi
-if [ $1 == "rbp2" ]; then pull_source "https://github.com/bavison/arm-mem/archive/master.zip" "$(pwd)/src"; fi
+pull_source "https://github.com/bavison/arm-mem/archive/master.zip" "$(pwd)/src"
 build_in_env "${1}" $(pwd) "rbp-armmem-osmc"
 build_return=$?
 if [ $build_return == 99 ]
 then
 	echo -e "Building package rbp-armmem"
 	out=$(pwd)/files
-    sed '/Package/d' -i files/DEBIAN/control
+	sed '/Package/d' -i files/DEBIAN/control
 	echo "Package: ${1}-armmem-osmc" >> files/DEBIAN/control
 	make clean
 	pushd src/arm-mem-*
@@ -21,8 +20,16 @@ then
 	if [ $? != 0 ]; then echo "Error occured during build" && exit 1; fi
 	strip_libs
 	mkdir -p $out/usr/lib
-	cp -ar libarmmem.so $out/usr/lib
-	cp -ar libarmmem.a $out/usr/lib
+	if [ "$1" == "rbp1" ]
+	then
+	    cp -ar libarmmem.so $out/usr/lib
+	    cp -ar libarmmem.a $out/usr/lib
+	fi
+	if [ "$1" == "rbp2" ]
+	then
+	    cp -ar libarmmem-a7.so $out/usr/lib
+	    cp -ar libarmmem-a7.a $out/usr/lib
+	fi
 	popd
 	fix_arch_ctl "files/DEBIAN/control"
 	dpkg -b files/ rbp-armmem-osmc.deb
