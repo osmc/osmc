14cb28de2669c0181f093e791d621e1acb1a677f
Sam Nazarko <email@samnazarko.co.uk>
2015-05-31 23:17:26 +0100

    [package] [libplatform-osmc] Add libplatform library: used for CEC and binary addons
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

---
+++ package/libplatform-osmc/build.sh
@@ -0,0 +1,42 @@
+# (c) 2014-2015 Sam Nazarko
+# email@samnazarko.co.uk
+
+#!/bin/bash
+
+. ../common.sh
+
+pull_source "https://github.com/Pulse-Eight/platform/archive/1.0.9.tar.gz" "$(pwd)/src"
+if [ $? != 0 ]; then echo -e "Error downloading" && exit 1; fi
+# Build in native environment
+build_in_env "${1}" $(pwd) "libplatform-osmc"
+build_return=$?
+if [ $build_return == 99 ]
+then
+	echo -e "Building libplatform"
+	out=$(pwd)/files
+	if [ -d files/usr ]; then rm -rf files/usr; fi
+	if [ -d files-dev/usr ]; then rm -rf files-dev/usr; fi
+	sed '/Package/d' -i files/DEBIAN/control
+	sed '/Package/d' -i files-dev/DEBIAN/control
+	sed '/Depends/d' -i files-dev/DEBIAN/control
+	update_sources
+	handle_dep "autoconf"
+	handle_dep "libtool"
+	echo "Package: ${1}-libplatform-osmc" >> files/DEBIAN/control && echo "Package: ${1}-libplatform-dev-osmc" >> files-dev/DEBIAN/control && echo "Depends: ${1}-libplatform-osmc" >> files-dev/DEBIAN/control
+	pushd src/platform-*
+	cmake -DCMAKE_INSTALL_PREFIX:PATH=/usr .
+	$BUILD
+	make install DESTDIR=${out}
+	if [ $? != 0 ]; then echo "Error occured during build" && exit 1; fi
+	strip_files "${out}"
+	popd
+	mkdir -p files-dev/usr
+	mv files/usr/include  files-dev/usr/
+	fix_arch_ctl "files/DEBIAN/control"
+	fix_arch_ctl "files-dev/DEBIAN/control"
+	dpkg -b files libplatform-osmc.deb
+	dpkg -b files-dev libplatform-dev-osmc.deb
+	build_return=$?
+fi
+teardown_env "${1}"
+exit $build_return
