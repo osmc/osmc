cbb4393b64dae11512080a476efb190b1f84d08a
Sam Nazarko <email@samnazarko.co.uk>
2015-06-08 14:30:00 +0100

    [package] [kernel-osmc] RBP: add additional overlays
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

--- package/kernel-osmc/build.sh
+++ package/kernel-osmc/build.sh
@@ -4,8 +4,8 @@
 #!/bin/bash
 
 . ../common.sh
-test $1 == rbp1 && VERSION="3.18.13" && REV="1"
-test $1 == rbp2 && VERSION="3.18.13" && REV="1"
+test $1 == rbp1 && VERSION="3.18.13" && REV="2"
+test $1 == rbp2 && VERSION="3.18.13" && REV="2"
 test $1 == vero && VERSION="3.14.37" && REV="15"
 test $1 == atv && VERSION="4.0.2" && REV="2"
 if [ $1 == "rbp1" ] || [ $1 == "rbp2" ] || [ $1 == "atv" ]
@@ -76,7 +76,7 @@ then
 	if [ "$1" == "rbp1" ] || [ "$1" == "rbp2" ]
 	then
 		mv arch/arm/boot/dts/*.dtb ../../files-image/boot/dtb-${VERSION}-${REV}-osmc/
-		overlays=( "hifiberry-dac-overlay" "hifiberry-dacplus-overlay" "hifiberry-digi-overlay" "iqaudio-dac-overlay" "iqaudio-dacplus-overlay" "rpi-dac-overlay" "lirc-rpi-overlay" "w1-gpio-overlay" "w1-gpio-pullup-overlay" "hy28a-overlay" "hy28b-overlay" "piscreen-overlay" "rpi-display-overlay" "spi-bcm2835-overlay" )
+		overlays=( "hifiberry-dac-overlay" "hifiberry-dacplus-overlay" "hifiberry-digi-overlay" "iqaudio-dac-overlay" "iqaudio-dacplus-overlay" "rpi-dac-overlay" "lirc-rpi-overlay" "w1-gpio-overlay" "w1-gpio-pullup-overlay" "hy28a-overlay" "hy28b-overlay" "piscreen-overlay" "rpi-display-overlay" "spi-bcm2835-overlay" "sdhost-overlay" "rpi-proto-overlay" "i2c-rtc-overlay" "i2s-mmap-overlay" "pps-gpio-overlay" )
 		pushd arch/arm/boot/dts
 		for dtb in ${overlays[@]}
 		do
