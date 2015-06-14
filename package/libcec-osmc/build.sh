5a43e8e4d2d5ba6a48871328fc00153aaf249ba8
Sam Nazarko <email@samnazarko.co.uk>
2015-06-15 00:21:30 +0100

    [package] [libcec-osmc] Specify DHAVE_RPI_API instead of explicitly defining it in a patch; add i.MX6 support for libCEC 3.0.
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

--- package/libcec-osmc/build.sh
+++ package/libcec-osmc/build.sh
@@ -32,9 +32,9 @@ then
 	echo "Package: ${1}-libcec-osmc" >> files/DEBIAN/control && echo "Package: ${1}-libcec-dev-osmc" >> files-dev/DEBIAN/control && echo "Depends: ${1}-libcec-osmc" >> files-dev/DEBIAN/control
 	pushd src/libcec*
 	install_patch "../../patches" "all"
-	if [ "$1" == "rbp1" ] || [ "$1" == "rbp2" ]; then export LIBRARY_PATH+=/opt/vc/lib && install_patch "../../patches" "rbp"; fi
-	if [ "$1" == "vero" ]; then install_patch "../../patches" "vero"; fi
-	cmake -DCMAKE_INSTALL_PREFIX:PATH=/usr -DBUILD_SHARED_LIBS=1 .
+	if [ "$1" == "rbp1" ] || [ "$1" == "rbp2" ]; then export LIBRARY_PATH+=/opt/vc/lib && install_patch "../../patches" "rbp" && PLATFORM="-DHAVE_RPI_API=1"; fi
+	if [ "$1" == "vero" ]; then install_patch "../../patches" "vero" && PLATFORM="-DHAVE_IMX_API=1"; fi
+	cmake -DCMAKE_INSTALL_PREFIX:PATH=/usr -DBUILD_SHARED_LIBS=1 $PLATFORM .
 	$BUILD
 	make install DESTDIR=${out}
 	if [ $? != 0 ]; then echo "Error occured during build" && exit 1; fi
