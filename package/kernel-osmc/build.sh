9aeee1d588c3c14fbf71ce0a1369f52e6a26e5b0
Sam Nazarko <email@samnazarko.co.uk>
2015-06-15 06:14:05 +0100

    [package] [kernel-osmc] Vero: added shared CEC driver; interlace display output fixes and 3D rework
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

--- package/kernel-osmc/build.sh
+++ package/kernel-osmc/build.sh
@@ -6,7 +6,7 @@
 . ../common.sh
 test $1 == rbp1 && VERSION="3.18.14" && REV="4"
 test $1 == rbp2 && VERSION="3.18.14" && REV="4"
-test $1 == vero && VERSION="3.14.37" && REV="15"
+test $1 == vero && VERSION="3.14.37" && REV="16"
 test $1 == atv && VERSION="4.0.2" && REV="2"
 if [ $1 == "rbp1" ] || [ $1 == "rbp2" ] || [ $1 == "atv" ]
 then
