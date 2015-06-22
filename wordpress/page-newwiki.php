fe80447638c38573bba63d3a037de274989c00e5
Mark Theis Madsen <marktheis@users.noreply.github.com>
2015-06-22 02:42:42 +0200

    [wp] new wiki template

---
+++ wordpress/page-newwiki.php
@@ -0,0 +1,38 @@
+<?php get_header(); ?>
+
+  <div class="row clearfix main">
+    <div class="container">
+      <div class="column three-fourths">
+        
+        <h1 class="page-title" itemprop="headline"><!-- TITLE HERE!!!?! --></h1>
+        
+        <div class="byline vcard">
+          <p><a href="http://osmc.tv/help/wiki">Back to Wiki</a></p>
+          <div class="cat">
+            <p>
+              <!-- LINK TO PAGE ON GITHUB!!!?! -->
+
+              <!-- EXAMPLE HERE !!!?!
+
+              <a href="https://github.com/samnazarko/osmc/blob/master/wiki/pages/kodi-osmc" target="_blank">Edit this page</a> 
+
+              -->
+            </p>
+          </div>
+        </div>
+        
+        <section class="entry-content clearfix" itemprop="articleBody">
+           
+          <!--- THE CONTENT HERE DUDE !!!?! -->
+          <p>Some neat content <3 </p>
+
+        </section> <?php // end article section ?>
+        
+      </div> <?php // end #main ?>
+      
+      <?php get_sidebar(); ?>
+      
+    </div> <?php // end #inner-content ?>
+  </div> <?php // end #content ?>
+
+<?php get_footer(); ?>
