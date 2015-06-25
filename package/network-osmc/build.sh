71f3413ce913e2a75e138dc3a71899a0cba5f77b
DBMandrake <sbyrnand@gmail.com>
2015-06-25 19:26:48 +0100

    [package] [network-osmc] Bump version and update dependency version to connman build which actually includes --noresolvmodify.

--- package/network-osmc/build.sh
+++ package/network-osmc/build.sh
@@ -13,7 +13,7 @@ then
 	sed '/Package/d' -i files/DEBIAN/control
 	sed '/Depends/d' -i files/DEBIAN/control
 	echo "Package: ${1}-network-osmc" >> files/DEBIAN/control
-	echo "Depends: ${1}-connman-osmc (>= 1.2.9-1), bluez, wget, iptables, wireless-firmware-osmc, net-tools, ntp, wpasupplicant, libnss-mdns, nfs-common, cifs-utils, ca-certificates, curl, python-dbus, python-gobject, ifupdown" >> files/DEBIAN/control
+	echo "Depends: ${1}-connman-osmc (>= 1.2.9-3), bluez, wget, iptables, wireless-firmware-osmc, net-tools, ntp, wpasupplicant, libnss-mdns, nfs-common, cifs-utils, ca-certificates, curl, python-dbus, python-gobject, ifupdown" >> files/DEBIAN/control
 	fix_arch_ctl "files/DEBIAN/control"
 	dpkg_build files/ ${1}-network-osmc.deb
 	build_return=$?
