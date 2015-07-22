f2fd81d00c3ccf53f4b0c61558d99cb78fd83a61
Sam Nazarko <email@samnazarko.co.uk>
2015-07-22 22:52:35 +0100

    [package] [rbp-bootloader-osmc] Various improvements
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

--- package/rbp-bootloader-osmc/build.sh
+++ package/rbp-bootloader-osmc/build.sh
@@ -9,7 +9,7 @@ echo -e "Building package rbp-bootloader-osmc"
 
 BOOT="files/boot"
 FWFILES=( "LICENCE.broadcom" "start_x.elf" "fixup_x.dat" "bootcode.bin" )
-REV="cf20cb2554fef392da5d355029bcb06b841f2b2e"
+REV="369d99df38de4c33b648ac864dd42e185461b477"
 
 make clean
 
