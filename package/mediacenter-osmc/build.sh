bd7e26b4535b02a29b3b4de1731c14aefb902e14
Sam Nazarko <email@samnazarko.co.uk>
2015-07-30 03:05:38 +0100

    [package] [mediacenter-osmc] RBP: update backports
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

--- package/mediacenter-osmc/build.sh
+++ package/mediacenter-osmc/build.sh
@@ -6,7 +6,7 @@
 . ../common.sh
 if [ "$1" == "rbp1" ] || [ "$1" == "rbp2" ] || [ "$1" == "vero" ]
 then
-pull_source "https://github.com/xbmc/xbmc/archive/15.0-Isengard.tar.gz" "$(pwd)/src"
+pull_source "https://github.com/xbmc/xbmc/archive/5ad943831acf3de1ea072463f82628d4e1337e37.tar.gz" "$(pwd)/src"
 API_VERSION="15"
 else
 pull_source "https://github.com/xbmc/xbmc/archive/master.tar.gz" "$(pwd)/kodi"
