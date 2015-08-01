8143ac63b1a6e01cefed7d43b71b8c85b873e789
Sam Nazarko <email@samnazarko.co.uk>
2015-08-01 13:39:33 +0100

    [package] [mediacenter-osmc] Bump to 349abe4587673b75a71650b13b88ebe34f4a3880. Add patch to fix crash on language change
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

--- package/mediacenter-osmc/build.sh
+++ package/mediacenter-osmc/build.sh
@@ -6,7 +6,7 @@
 . ../common.sh
 if [ "$1" == "rbp1" ] || [ "$1" == "rbp2" ] || [ "$1" == "vero" ]
 then
-pull_source "https://github.com/xbmc/xbmc/archive/5ad943831acf3de1ea072463f82628d4e1337e37.tar.gz" "$(pwd)/src"
+pull_source "https://github.com/xbmc/xbmc/archive/349abe4587673b75a71650b13b88ebe34f4a3880.tar.gz" "$(pwd)/src"
 API_VERSION="15"
 else
 pull_source "https://github.com/xbmc/xbmc/archive/master.tar.gz" "$(pwd)/kodi"
