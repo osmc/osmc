4e2be7c69fddd146b593e3f2b8d6521aaa01b999
Sam Nazarko <email@samnazarko.co.uk>
2015-08-01 16:35:25 +0100

    [package] [kodi-platform-osmc] Fix the build. Use Kodi build indirectly
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

--- package/kodi-platform-osmc/build.sh
+++ package/kodi-platform-osmc/build.sh
@@ -21,9 +21,25 @@ then
 	sed '/Depends/d' -i files-dev/DEBIAN/control
 	update_sources
 	handle_dep "cmake"
+	handle_dep "wget"
+	handle_dep "libtinyxml-dev"
+        if [ "$1" == "rbp2" ] || [ "$1" == "vero" ]; then handle_dep "armv7-libplatform-dev-osmc"; fi
+        if [ "$1" == "rbp1" ]; then handle_dep "armv6l-libplatform-dev-osmc"; fi
+        if [ "$1" == "atv" ]; then handle_dep "i386-libplatform-dev-osmc"; fi
+	# Hack # Hack # Hack
+	# This is an unconventional handle_dep
+	# We don't have a dev package for Kodi, but we need some bits from it.
+	# We don't want all the dependencies it drags in.
+	DEP=$(apt-get install -qq --print-uris ${1}-mediacenter-osmc | grep ${1}-mediacenter-osmc | perl -lne "/'(.*?)'/;print \$1" | tail -n 1)
+	wget ${DEP} -O $(pwd)/src/mediacenter-osmc.deb # Can't pull_bin in chroot
+	if [ $? != 0 ]; then echo "Downloading Kodi package failed"; fi
+	dpkg -x $(pwd)/src/mediacenter-osmc.deb $(pwd)/src/kodi-platform-*
 	echo "Package: ${1}-kodi-platform-osmc" >> files/DEBIAN/control && echo "Package: ${1}-kodi-platform-dev-osmc" >> files-dev/DEBIAN/control && echo "Depends: ${1}-kodi-platform-osmc" >> files-dev/DEBIAN/control
 	pushd src/kodi-platform-*
-	cmake -DCMAKE_INSTALL_PREFIX:PATH=/usr .
+	# Fix stupid defaults
+	sed s:/usr/include/kodi:$(pwd)/usr/include/kodi: -i $(pwd)/usr/lib/kodi/kodi-config.cmake
+	sed s:/usr/lib/kodi:$(pwd)/usr/lib/kodi: -i $(pwd)/usr/lib/kodi/kodi-config.cmake
+	cmake -DCMAKE_INSTALL_PREFIX:PATH=/usr -Dkodi_DIR=$(pwd)/usr/lib/kodi/ .
 	$BUILD
 	make install DESTDIR=${out}
 	if [ $? != 0 ]; then echo "Error occured during build" && exit 1; fi
