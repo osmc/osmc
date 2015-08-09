412eb52b1d10fcc650c071f966130c392f355346
Sam Nazarko <email@samnazarko.co.uk>
2015-08-09 17:33:05 +0100

    [wordpress] Rip wordpress.
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

--- wordpress/citm.php
+++
@@ -1,9 +0,0 @@
-<?php
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, "http://46.37.189.135/osmc/download/" . $_GET["citm"]);
-curl_setopt($ch, CURLOPT_HEADER, 0);
-curl_setopt($ch, CURLOPT_CONNECTTIMEOUT , 20);
-curl_setopt($ch, CURLOPT_TIMEOUT, 20); //timeout in seconds
-curl_exec($ch);
-curl_close($ch);
-?>
