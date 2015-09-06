2556010e902974851e84229bc16470afdb32dfc8
Sam Nazarko <email@samnazarko.co.uk>
2015-09-06 01:12:32 +0100

    [installer] [target] Vendor preseed was only applying for RBP2. Apply it for RBP1 as well.
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

--- installer/target/qt_target/qt_target_installer/bootloaderconfig.cpp
+++ installer/target/qt_target/qt_target_installer/bootloaderconfig.cpp
@@ -69,12 +69,12 @@ void BootloaderConfig::configureEnvironment()
         if (utils->getOSMCDev() == "rbp2")
         {
             configStringList << "gpu_mem_1024=256\n" << "hdmi_ignore_cec_init=1\n" << "disable_overscan=1\n" << "start_x=1\n" << "disable_splash=1\n";
-            if (preseed->getBoolValue("vendor/dtoverlay"))
-                configStringList << "dtoverlay=" << preseed->getStringValue("vendor/dtoverlayparam") << "\n";
-            else
-                configStringList << "dtoverlay=lirc-rpi:gpio_out_pin=17,gpio_in_pin=18\n";
             cmdlineStringList << "osmcdev=rbp2";
         }
+        if (preseed->getBoolValue("vendor/dtoverlay"))
+            configStringList << "dtoverlay=" << preseed->getStringValue("vendor/dtoverlayparam") << "\n";
+        else
+            configStringList << "dtoverlay=lirc-rpi:gpio_out_pin=17,gpio_in_pin=18\n";
         utils->writeToFile(configFile, configStringList, false);
         utils->writeToFile(cmdlineFile, cmdlineStringList, false);
         configFile.close();
