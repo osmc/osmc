f351c3baee6249979fe3f5f45f2c76eea2c25aa2
Sam Nazarko <email@samnazarko.co.uk>
2015-09-14 20:46:06 +0100

    [package] [rbp-userland-osmc] Used a specific revision of userland, which should always match bootloader.
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

--- package/rbp-userland-osmc/build.sh
+++ package/rbp-userland-osmc/build.sh
@@ -5,11 +5,13 @@
 
 . ../common.sh
 
+REV="bbf5495c0424fe38d795111007e93f8763b88bcc"
+
 echo -e "Building package rbp-userland"
 out=$(pwd)/files
 make clean
 echo Downloading userland
-pull_source "https://github.com/raspberrypi/firmware/archive/master.tar.gz" "$(pwd)/src"
+pull_source "https://github.com/raspberrypi/firmware/archive/${REV}.tar.gz" "$(pwd)/src"
 if [ $? != 0 ]; then echo -e "Error downloading" && exit 1; fi
 echo Moving files in to place
 mkdir -p files/opt/vc
@@ -17,10 +19,10 @@ mkdir -p files-dev/opt/vc
 mkdir -p files-src/opt/vc
 mkdir -p files/etc/ld.so.conf.d
 echo "/opt/vc/lib" > files/etc/ld.so.conf.d/rbp.conf
-cp -ar src/firmware-master/hardfp/opt/vc/bin/ files/opt/vc
-cp -ar src/firmware-master/hardfp/opt/vc/lib files/opt/vc
-cp -ar src/firmware-master/hardfp/opt/vc/include files-dev/opt/vc
-cp -ar src/firmware-master/hardfp/opt/vc/src files-src/opt/vc
+cp -ar src/firmware-${REV}/hardfp/opt/vc/bin/ files/opt/vc
+cp -ar src/firmware-${REV}/hardfp/opt/vc/lib files/opt/vc
+cp -ar src/firmware-${REV}/hardfp/opt/vc/include files-dev/opt/vc
+cp -ar src/firmware-${REV}/hardfp/opt/vc/src files-src/opt/vc
 rm -rf src
 dpkg_build files/ rbp-userland-osmc.deb
 dpkg_build files-dev/ rbp-userland-dev-osmc.deb
