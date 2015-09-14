f61cfe4d2f39d7bbdbd3ad80d6656692fc76018c
Sam Nazarko <email@samnazarko.co.uk>
2015-09-14 20:40:19 +0100

    [package] [rbp-userland-osmc] Add a source package. Fixes #219
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

--- package/rbp-userland-osmc/build.sh
+++ package/rbp-userland-osmc/build.sh
@@ -14,11 +14,14 @@ if [ $? != 0 ]; then echo -e "Error downloading" && exit 1; fi
 echo Moving files in to place
 mkdir -p files/opt/vc
 mkdir -p files-dev/opt/vc
+mkdir -p files-src/opt/vc
 mkdir -p files/etc/ld.so.conf.d
 echo "/opt/vc/lib" > files/etc/ld.so.conf.d/rbp.conf
 cp -ar src/firmware-master/hardfp/opt/vc/bin/ files/opt/vc
 cp -ar src/firmware-master/hardfp/opt/vc/lib files/opt/vc
 cp -ar src/firmware-master/hardfp/opt/vc/include files-dev/opt/vc
+cp -ar src/firmware-master/hardfp/opt/vc/src files-src/opt/vc
 rm -rf src
 dpkg_build files/ rbp-userland-osmc.deb
 dpkg_build files-dev/ rbp-userland-dev-osmc.deb
+dpkg_build files-src/ rbp-userland-src-osmc.deb
