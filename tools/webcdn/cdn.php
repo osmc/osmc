3e2d1e5d8bf3f020f539d571223a87c71696db57
Sam Nazarko <email@samnazarko.co.uk>
2015-09-22 17:19:31 +0100

    [webcdn] Use HTTP-X-Forwarded-For due to 'inconsistencies' with mod_rpaf
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

--- tools/webcdn/cdn.php
+++ tools/webcdn/cdn.php
@@ -17,7 +17,7 @@ $FALLBACK = "http://46.37.189.135";
 
 
 // Remote IP as provided by WebClient to Server
-$IP_tocheck=$_SERVER['REMOTE_ADDR'];
+$IP_tocheck=$_SERVER['HTTP_X_FORWARDED_FOR'];
 
 // Checking validity of IP
 // Will remove reserved IP's -> local loopback
