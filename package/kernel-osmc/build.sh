2dcb9f5d6d63d9390286b1d44710d7f228271ce6
Sam Nazarko <email@samnazarko.co.uk>
2015-09-24 20:48:26 +0100

    [package] [kernel-osmc] Vero: add ondemand cpu governor
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

--- package/kernel-osmc/build.sh
+++ package/kernel-osmc/build.sh
@@ -6,7 +6,7 @@
 . ../common.sh
 test $1 == rbp1 && VERSION="4.2.1" && REV="1"
 test $1 == rbp2 && VERSION="4.2.1" && REV="1"
-test $1 == vero && VERSION="4.1.7" && REV="1"
+test $1 == vero && VERSION="4.1.7" && REV="2"
 test $1 == atv && VERSION="4.0.2" && REV="2"
 if [ $1 == "rbp1" ] || [ $1 == "rbp2" ] || [ $1 == "atv" ]
 then
