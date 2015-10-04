aab68fbeefe6ca5f73953fe0d1a79c0925414efc
Sam Nazarko <email@samnazarko.co.uk>
2015-10-04 17:45:28 +0100

    [package] [kernel-osmc] Fix typo..
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

--- package/kernel-osmc/build.sh
+++ package/kernel-osmc/build.sh
@@ -8,7 +8,7 @@ test $1 == rbp1 && VERSION="4.2.1" && REV="1"
 test $1 == rbp2 && VERSION="4.2.1" && REV="1"
 test $1 == vero && VERSION="4.1.7" && REV="4"
 test $1 == atv && VERSION="4.0.2" && REV="2"
-test $1 == pc && VERSION"4.0.3" && REV="1"
+test $1 == pc && VERSION="4.0.3" && REV="1"
 if [ $1 == "rbp1" ] || [ $1 == "rbp2" ] || [ $1 == "atv" ] || [ $1 == "pc" ]
 then
 	if [ -z $VERSION ]; then echo "Don't have a defined kernel version for this target!" && exit 1; fi
