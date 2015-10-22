e27606346c03d1d11d6447249eb2432d8eafa441
Sam Nazarko <email@samnazarko.co.uk>
2015-10-22 13:24:48 +0100

    [package] [rbp-bootloader-osmc] Bump version
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

--- package/rbp-bootloader-osmc/build.sh
+++ package/rbp-bootloader-osmc/build.sh
@@ -9,7 +9,7 @@ echo -e "Building package rbp-bootloader-osmc"
 
 BOOT="files/boot"
 FWFILES=( "LICENCE.broadcom" "start_x.elf" "fixup_x.dat" "bootcode.bin" )
-REV="ba7a8fb709adab287495f4e836b1cd3e5c9db409"
+REV="f7108cfdc715f79c5fd051fb4c3ae6f4b1b01f23"
 
 make clean
 
