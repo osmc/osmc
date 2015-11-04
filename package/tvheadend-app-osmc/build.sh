56fa9d9f87019abba2ef2dd8043770840362d2bb
Sam Nazarko <email@samnazarko.co.uk>
2015-11-04 13:30:12 +0000

    [package] [tvheadend-app-osmc] Update version to 4.0.7
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

--- package/tvheadend-app-osmc/build.sh
+++ package/tvheadend-app-osmc/build.sh
@@ -4,7 +4,7 @@
 #!/bin/bash
 
 . ../common.sh
-VERSION="v4.0.5"
+VERSION="v4.0.7"
 pull_source "https://github.com/tvheadend/tvheadend/archive/${VERSION}.tar.gz" "$(pwd)/src"
 if [ $? != 0 ]; then echo -e "Error downloading" && exit 1; fi
 # Build in native environment
