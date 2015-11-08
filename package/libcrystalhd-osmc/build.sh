c6af13ad8f4e0b039ff777721725d2d969d95aa5
Sam Nazarko <email@samnazarko.co.uk>
2015-11-08 02:03:34 +0000

    [package] [libcrystalhd-osmc] Tighten devel dependency
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

--- package/libcrystalhd-osmc/build.sh
+++ package/libcrystalhd-osmc/build.sh
@@ -19,8 +19,13 @@ then
 	sed '/Package/d' -i files/DEBIAN/control
 	sed '/Package/d' -i files-dev/DEBIAN/control
 	sed '/Depends/d' -i files-dev/DEBIAN/control
+        sed '/Version/d' -i files-dev/DEBIAN/control
+        VERSION_DEV=$(grep Version ${out}/DEBIAN/control)
+        VERSION_NUM=$(echo $VERSION_DEV | awk {'print $2'})
+        echo $VERSION_DEV >> files-dev/DEBIAN/control
+        echo "Depends: ${1}-libcrystalhd-osmc (=${VERSION_NUM})" >> files-dev/DEBIAN/control
 	update_sources
-	echo "Package: ${1}-libcrystalhd-osmc" >> files/DEBIAN/control && echo "Package: ${1}-libcrystalhd-dev-osmc" >> files-dev/DEBIAN/control && echo "Depends: ${1}-libcrystalhd-osmc" >> files-dev/DEBIAN/control
+	echo "Package: ${1}-libcrystalhd-osmc" >> files/DEBIAN/control && echo "Package: ${1}-libcrystalhd-dev-osmc" >> files-dev/DEBIAN/control
 	pushd src/crystalhd*/linux_lib/libcrystalhd
 	$BUILD
 	make install DESTDIR=${out}
