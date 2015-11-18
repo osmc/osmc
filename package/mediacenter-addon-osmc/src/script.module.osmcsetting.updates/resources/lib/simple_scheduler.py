c66a853de6f333523e1189f821b0415226a84284
KODeKarnage <sublime.kev@gmail.com>
2015-11-18 22:19:26 +1100

    [package][mediacenter-addon-osmc] Updates: fixed bug with update scheduler that may cause updates to skip a month. Added simple test function to module.

--- package/mediacenter-addon-osmc/src/script.module.osmcsetting.updates/resources/lib/simple_scheduler.py
+++ package/mediacenter-addon-osmc/src/script.module.osmcsetting.updates/resources/lib/simple_scheduler.py
@@ -5,7 +5,7 @@ import random
 
 class SimpleScheduler(object):
 
-	def __init__(self, setting_dict):
+	def __init__(self, setting_dict, right_now=None):
 
 		self.frequency 		= setting_dict.get('check_freq', 1)				# how often the action occurs (Never, Daily, Weekly, Monthly)
 		self.specific_time	= setting_dict.get('check_time', 0)				# whether the action should take place at a specific or random time (boolean)
@@ -16,14 +16,15 @@ class SimpleScheduler(object):
 		self.trigger_time 	= datetime.datetime.now().replace(year=2222)	# the time of the next update check
 		self.leeway			= 15											# the number of minutes past the action time that the action can still take place
 
-		self.set_trigger()
+		if right_now is None:
+			right_now = datetime.datetime.now()
+
+		self.set_trigger(right_now)
 
 
-	def set_trigger(self):
+	def set_trigger(self, right_now):
 
 		# use right_nows year and month
-
-		right_now = datetime.datetime.now()
 
 		if self.frequency == 1:
 
@@ -54,7 +55,11 @@ class SimpleScheduler(object):
 			# specified hour and minute (using defaults if not provided)
 
 			# End of this current month plus or minus the number of days the user has chosen in settings
-			trigger_day = right_now.replace(month = right_now.month % 12 + 1, day = 1) + datetime.timedelta(days=self.daynum-1)
+
+			month = max([1, (right_now.month + 1) % 13])
+			year  = right_now.year if month != 1 else right_now.year + 1
+
+			trigger_day = right_now.replace(year=year, month=month, day = 1) + datetime.timedelta(days=self.daynum-1)
 			#             today, with day replaced by 1 and adding 1 to the month  	plus the number of days the user has chosen
 			#																		minus an extra day to rebase it to month-end
 
@@ -66,6 +71,8 @@ class SimpleScheduler(object):
 		while self.trigger_time < right_now:
 
 			self.step_trigger()
+
+		
 
 
 	def set_trigger_time(self, trigger_time):
@@ -84,8 +91,6 @@ class SimpleScheduler(object):
 	def step_trigger(self):
 		''' Progress the trigger time from its current position to its next position '''
 
-		right_now = datetime.datetime.now()
-
 		if self.frequency == 1:
 
 			# jump one say ahead from the current trigger date
@@ -100,15 +105,22 @@ class SimpleScheduler(object):
 
 			if self.daynum > 0:
 				# if the daynum is 1 to 16 then just add one month to the existing month and set the day to be the users chosen date
-				self.trigger_time = self.trigger_time.replace(month = (self.trigger_time.month % 12) + 1, day = self.daynum)
+				month = (self.trigger_time.month % 12) + 1
+				year  = self.trigger_time.year + 1 if month == 1 else self.trigger_time.year
+
+				self.trigger_time = self.trigger_time.replace(year=year, month = month, day = self.daynum)
 
 			else:
 				# if the daynum is negative, that is, the user wants the update to run a certain number of days BEFORE month-end,
 				# then jump to the first day of the month two months ahead of the current one, and then move back one day to get
 				# next months month-end date, then subtract the number of days the user has chosen
-				new_month = (((self.trigger_time.month % 12) + 1 ) % 12 ) + 1
+				month = (((self.trigger_time.month % 12) + 1) % 12) + 1
+				if month < 3:
+					year  = self.trigger_time.year + 1
+				else:
+					year = self.trigger_time.year
 
-				self.trigger_time = self.trigger_time.replace(month = new_month, day = 1) + datetime.timedelta(days=self.daynum-1)
+				self.trigger_time = self.trigger_time.replace(year=year, month = month, day = 1) + datetime.timedelta(days=self.daynum-1)
 
 
 	def check_trigger(self):
@@ -126,4 +138,21 @@ class SimpleScheduler(object):
 		else:
 
 			return False
+
+def test(settings=None):
+
+	if settings is not None:
+		x = settings
+	else:
+		x = {'check_freq':3, 'check_time':0, 'check_weekday':1, 'check_day': 5, 'check_hour':22, 'check_minute':00}
+
+	right_now = datetime.datetime.now()
+
+	for z in range(3700):
+		right_now += datetime.timedelta(days=1)
+
+		print z
+		print right_now
+		s = SimpleScheduler(x, right_now)
+		print  '%s\n' % s.trigger_time
 
