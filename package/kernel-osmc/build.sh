da633c4dec8dec2723ca577f3b1fc0c5d6fe9047
Sam Nazarko <email@samnazarko.co.uk>
2015-11-19 21:53:43 +0000

    [pacage] [kernel-osmc] Add EMBED_INITRAMFS flag
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

--- package/kernel-osmc/build.sh
+++ package/kernel-osmc/build.sh
@@ -4,11 +4,11 @@
 #!/bin/bash
 
 . ../common.sh
-test $1 == rbp1 && VERSION="4.3.0" && REV="5"
-test $1 == rbp2 && VERSION="4.3.0" && REV="5"
-test $1 == vero && VERSION="4.1.12" && REV="4"
-test $1 == atv && VERSION="4.2.3" && REV="6"
-test $1 == pc && VERSION="4.2.3" && REV="1"
+test $1 == rbp1 && VERSION="4.3.0" && REV="5" && EMBED_INITRAMFS="1"
+test $1 == rbp2 && VERSION="4.3.0" && REV="5" && EMBED_INITRAMFS="1"
+test $1 == vero && VERSION="4.1.12" && REV="4" && EMBED_INITRAMFS="1"
+test $1 == atv && VERSION="4.2.3" && REV="6" && EMBED_INITRAMFS="0"
+test $1 == pc && VERSION="4.2.3" && REV="1" && EMBED_INITRAMFS="1"
 if [ $1 == "rbp1" ] || [ $1 == "rbp2" ] || [ $1 == "atv" ] || [ $1 == "pc" ]
 then
 	if [ -z $VERSION ]; then echo "Don't have a defined kernel version for this target!" && exit 1; fi
