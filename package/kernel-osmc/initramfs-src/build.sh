d1e9b425c700281f67f8a3e79dc8b71e0bfa4070
Sam Nazarko <email@samnazarko.co.uk>
2015-11-26 05:49:38 +0000

    [package] [kernel-osmc] initramfs: add NFS support; fix sleeps
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

--- package/kernel-osmc/initramfs-src/build.sh
+++ package/kernel-osmc/initramfs-src/build.sh
@@ -64,10 +64,12 @@ mkdir -p target/tmp
 mkdir -p target/var
 mkdir -p target/etc
 mkdir -p target/dev
+mkdir -p target/usr/share/udhcpc
 install -m 0755 e2fsprogs/e2fsprogs-${E2FSPROGS_VERSION}/e2fsck/e2fsck target/bin/e2fsck
 install -m 0755 busybox/busybox-${BUSYBOX_VERSION}/busybox target/bin/busybox
 install -m 0755 init target/init
 install -m 0755 init.d/${2} init-device
+cp -ar udhcpc.script target/usr/share/udhcpc/default.script
 ln -s target/bin/e2fsck target/bin/fsck.ext4
 ln -s target/bin/e2fsck target/bin/fsck.ext3
 ln -s target/bin/e2fsck target/bin/fsck.ext2
