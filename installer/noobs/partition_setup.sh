ddb27f72b27a5375b26858b946d5981d7c1b2d78
Sam Nazarko <email@samnazarko.co.uk>
2015-11-28 18:08:23 +0000

    [installer] [noobs] Pi Zero: handle the (unlikely) case someone tries to install OSMC via NOOBS on a Pi Zero
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

--- installer/noobs/partition_setup.sh
+++ installer/noobs/partition_setup.sh
@@ -2,6 +2,20 @@
 
 # Let OSIRIS see what we are doing
 set -x
+
+is_pi_zero()
+{
+    revision=$(cat /proc/cmdline | awk -v RS=" " -F= '/boardrev/ { print $2 }')
+    if [ "$(( $revision >> 23 & 1 ))" -eq 1 ]
+    then
+        if [ "$(( $revision >> 4 & 0x7F ))" -eq 9 ]
+        then
+            return 1 # This is a Pi Zero
+    fi
+    fi
+    return 0
+}
+
 # Are we Pi1 or Pi2
 grep -q ARMv7 /proc/cpuinfo
 if [ $? -eq 0 ]
@@ -17,6 +31,14 @@ mkdir -p /tmp/mount
 # Fix the cmdline.txt
 mount $part1 /tmp/mount
 echo "root=$part2 osmcdev=$dev rootfstype=ext4 rootwait quiet" > /tmp/mount/cmdline.txt
+# Check for Pi Zero
+is_pi_zero
+pi_zero=$?
+if [ "$pi_zero" -eq 0 ]
+then
+    echo "arm_freq=850" >> /tmp/mount/config.txt
+    echo "core_freq=375" >> /tmp/mount/config.txt
+fi
 umount /tmp/mount
 # Wait
 sync
