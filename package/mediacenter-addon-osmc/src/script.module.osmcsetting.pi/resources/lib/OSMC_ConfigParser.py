7c3f0b82bd5a574362b3f17f5d832759fe955fc4
KODeKarnage <sublime.kev@gmail.com>
2015-12-23 20:03:52 +1100

    [package][mediacenter-addon-osmc] Pi config: removed debug printing

--- package/mediacenter-addon-osmc/src/script.module.osmcsetting.pi/resources/lib/OSMC_ConfigParser.py
+++ package/mediacenter-addon-osmc/src/script.module.osmcsetting.pi/resources/lib/OSMC_ConfigParser.py
@@ -56,7 +56,7 @@ def read_and_sanitise_file(file_loc='C:\\temp\\config.txt'):
 
             line = line.strip()
 
-            print '>>> ' + line
+            #print '>>> ' + line
 
             for original, alias in aliases.iteritems():
                 line = line.replace(original, alias)
@@ -64,7 +64,7 @@ def read_and_sanitise_file(file_loc='C:\\temp\\config.txt'):
             # pass through all commented lines and newlines
             if line.startswith('#') or line == '\n' or not line:
                 newlines.append(line)
-                print 'pass through\n'
+                #print 'pass through\n'
                 continue
 
             # add dtparams to overlay_storage, this ensures only dtparams that occur right after the dtoverlay are counted as parameters for the dtoverlay
@@ -72,19 +72,19 @@ def read_and_sanitise_file(file_loc='C:\\temp\\config.txt'):
             if overlay_storage and 'dtparam' in line:
                 line = line.replace(' ','')
                 overlay_storage.append(line[line.index('=')+1:].strip())
-                print 'append param\n'
+                #print 'append param\n'
                 continue
 
             # do this if there is an orphaned dtparam line, that is, if a dtparam is found without any starting overlay block in overlay_storage
             elif 'dtparam' in line:
                 line = line.replace(' ' ,'')
                 newlines.append(line.replace('dtparam=', 'dtparam|__|'))   # |__| is the marker declaring this param as an orphan
-                print 'dtparam orphan added'
+                #print 'dtparam orphan added'
                 continue
 
             # if the line is not a new dtparam and there are items in storage, then roll those items up together and appened to newlines
             elif overlay_storage:
-                print 'combining overlay block'
+                #print 'combining overlay block'
                 overlay_root = overlay_storage.pop(0)
                 if overlay_storage:
                     overlay_root = overlay_root + '='
@@ -95,8 +95,8 @@ def read_and_sanitise_file(file_loc='C:\\temp\\config.txt'):
 
                 newlines.append(overlay_oneline)
                 overlay_storage = []
-                print overlay_oneline
-                print 'combined params\n'
+                #print overlay_oneline
+                #print 'combined params\n'
 
                 # once complete, continue on with processing the current line
 
@@ -107,25 +107,25 @@ def read_and_sanitise_file(file_loc='C:\\temp\\config.txt'):
                 if ':' in line:
                     line = line.strip().replace('dtoverlay=', 'dtoverlay_||_').replace(':','=')
                     newlines.append(line)
-                    print 'adding condensed dtoverlay to newlines'
+                    #print 'adding condensed dtoverlay to newlines'
                 else:
 
                     overlay_storage.append(line.strip().replace('dtoverlay=', 'dtoverlay_||_'))     # _||_ this is merely a place holder to indicate dtoverlays
-                    print 'starting overlay block\n'
+                    #print 'starting overlay block\n'
                 continue
 
             # if there is no equals sign, then remove any spaces from the string, and add an equals sign, THIS MUST BE REVERSED WHEN WRITING BACK
             if line and '=' not in line:
                 newlines.append(line.replace(' ', '_SPACE_')+'=NO_EQUALS_SIGN')
-                print 'no equals sign\n'
+                #print 'no equals sign\n'
                 continue
 
             newlines.append(line)
-            print 'added: %s\n' % line
+            #print 'added: %s\n' % line
 
         # take care of any orphaned dtoverlay blocks, this will only occur if the overlay block items are at the end of the file
         if overlay_storage:
-            print 'handling ophaned overlay blocks: %s' % overlay_storage
+            #print 'handling ophaned overlay blocks: %s' % overlay_storage
 
             overlay_root = overlay_storage.pop(0)
 
@@ -138,8 +138,8 @@ def read_and_sanitise_file(file_loc='C:\\temp\\config.txt'):
             newlines.append(overlay_oneline)
             overlay_storage = []
 
-            print overlay_oneline
-            print 'combined params\n'
+            #print overlay_oneline
+            #print 'combined params\n'
 
 
     # handle duplicates by commenting out the earlier entries, rPi uses the LAST entry, so we should be consistent with that
@@ -176,7 +176,7 @@ def write_to_config_file(config_stringIO, export_location='C:\\temp\\temp.txt'):
     with open(export_location, 'w') as f:
         config_stringIO.seek(0)
         for line in config_stringIO.readlines():
-            print line
+            #print line
             # this identifies dtoverlays
             if '_||_' in line:
 
@@ -260,8 +260,8 @@ def apply_changes_to_configtxt(changes, file_loc='C:\\temp\\config.txt'):
     blotter = StringIO.StringIO()
     config_dict.write(blotter)
 
-    print ''
-    print config_dict
+    #print ''
+    #print config_dict
 
     # temporary location for the config.txt
     if os.path.isfile('/var/tmp/config.txt'):
@@ -340,6 +340,7 @@ def test():
 
 
 if __name__ == "__main__":
+
     print test()
 
 
