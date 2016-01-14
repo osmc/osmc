b74a2967e2456c6de211087c2f1557ac7b758f2b
Sam Nazarko <email@samnazarko.co.uk>
2016-01-14 00:11:22 +0000

    [package] [mediacenter-osmc] build.sh: ensure patches for vero2 are applied
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

--- package/mediacenter-osmc/build.sh
+++ package/mediacenter-osmc/build.sh
@@ -191,6 +191,7 @@ then
 	fi
 	if [ "$1" == "rbp1" ] || [ "$1" == "rbp2" ] || [ "$1" == "vero" ]; then install_patch "../../patches" "arm"; fi
 	test "$1" == vero && install_patch "../../patches" "vero"
+	test "$1" == vero2 && install_patch "../../patches" "vero2"
 	./bootstrap
 	# Apple TV configuration
 	test "$1" == atv && \
