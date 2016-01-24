a1afc60511a07b5db6cf9a3135ae563456f28743
Sam Nazarko <email@samnazarko.co.uk>
2016-01-24 20:09:07 +0000

    [package] [libplatform-osmc] Use old libplatform for now
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

--- package/libplatform-osmc/build.sh
+++ package/libplatform-osmc/build.sh
@@ -5,7 +5,7 @@
 
 . ../common.sh
 
-pull_source "https://github.com/Pulse-Eight/platform/archive/p8-platform-2.0.1.tar.gz" "$(pwd)/src"
+pull_source "https://github.com/Pulse-Eight/platform/archive/1.0.10.tar.gz" "$(pwd)/src" #tbc: make p8-platform
 if [ $? != 0 ]; then echo -e "Error downloading" && exit 1; fi
 # Build in native environment
 build_in_env "${1}" $(pwd) "libplatform-osmc"
