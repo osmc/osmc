c50573bef1611b525ee358a2e00b7a67f045a9f2
Leonard de Ruijter <alderuijter@gmail.com>
2016-03-17 20:23:53 +0100

    [package][mediacenter-screenreader-osmc] Update to 1.0.6b2

--- package/mediacenter-screenreader-osmc/build.sh
+++ package/mediacenter-screenreader-osmc/build.sh
@@ -1,4 +1,4 @@
-# (c) 2014-2015 Sam Nazarko
+# (c) 2014-2016 Sam Nazarko
 # email@samnazarko.co.uk
 
 #!/bin/bash
@@ -6,7 +6,7 @@
 . ../common.sh
 
 make clean
-pull_source "https://raw.githubusercontent.com/ruuk/repository/master/beta/service.xbmc.tts/service.xbmc.tts-1.0.6b1.zip" "$(pwd)/files/usr/share/kodi/addons"
+pull_source "https://raw.githubusercontent.com/ruuk/repository/master/beta/service.xbmc.tts/service.xbmc.tts-1.0.6b2.zip" "$(pwd)/files/usr/share/kodi/addons"
 if [ $? != 0 ]; then echo -e "Error downloading" && exit 1; fi
 echo -e "Building package mediacenter-screenreader-osmc"
 dpkg_build files/ mediacenter-screenreader-osmc.deb
