d28a41407dccb0b2996bc592c9fa96afc4403eec
KODeKarnage <sublime.kev@gmail.com>
2016-05-04 16:11:17 +1000

    [package][mediacenter-addon-osmc] Backups: added in new fstab mount insertion functions

--- package/mediacenter-addon-osmc/src/script.module.osmcsetting.updates/resources/lib/OSMC_Backups.py
+++ package/mediacenter-addon-osmc/src/script.module.osmcsetting.updates/resources/lib/OSMC_Backups.py
@@ -435,10 +435,6 @@ class osmc_backup(object):
 
 				ok = DIALOG.ok('Backup failed', 'Backup failed to copy the tar file.')
 
-                                try:
-                                        xbmcvfs.delete(local_tarball_name)
-                                except:
-                                        log('Cannot delete temp file at %s' % local_tarball_name)
 
 				return 'failed'
 
@@ -678,11 +674,12 @@ class osmc_backup(object):
 
 							elif member.name.endswith('userdata/fstab') and self.restoring_fstab:
 
-								member.name = os.path.basename(member.name)
+								# restore temp version back to userdata
+								t.extract(member, restore_location)
 
-								t.extract(member, '/tmp/')
+								fstab_loc = os.path.join(restore_location, os.path.basename(member.name))
 
-								self.copy_fstab_to_etc('/tmp/fstab')
+								self.restore_fstab(fstab_loc)
 
 							else:
 
@@ -830,6 +827,87 @@ class osmc_backup(object):
 		# exportlibrary(video,true,thumbs,overwrite,actorthumbs)
 		# The video library is exported to multiple files with the given options.
 		# Here thumbs, overwrite and actorthumbs are boolean values (true or false).
+
+
+	def uniquify(self, list_with_duplicates):
+		'''Takes a list with duplicates and removes the duplicated lines, 
+		could just use list(set(list_with_duplicates)) but that screws with
+		the ordering of the list.'''
+
+		seen = []
+		list_without_duplicates = []
+
+		for item in list_with_duplicates:
+
+			# if the line is pure whitespace, then retain it, and move on
+			if not item.strip(' \n\t\r'):
+
+				list_without_duplicates.append(item)
+				continue
+
+			# if we've already added this line, then move to the next one    
+			if item in seen:
+				continue
+
+			seen.append(item)
+			list_without_duplicates.append(item)
+
+		return list_without_duplicates
+
+
+	def restore_fstab(self, location_of_backedup_fstab):
+		''' Restores the mounts in the fstab file  '''
+
+		# run the backup file through millhams function to get a list of valid entries
+		unused_mnts = backup_mnts.copy()
+		new_lines   = []
+
+		# read the current fstab file, process it line by line
+		with open('/etc/fstab', 'r') as current_fstab:
+			
+			# process each line in the current fstab files
+			for line in current_fstab.readlines():
+
+				# determine if the line contains a valid mount
+
+				# if not, or if the type of mount is no on this permitted list, then 
+				# add the line as-is to the new version, and then go to the next line
+				if not tup or tup.fs_vfstype not in ['nfs','cifs','bind']:
+					new_lines.append(line)
+					continue
+
+				# if there is a valid mount, then check that local mount point against those
+				# found in the backup file we are restoring
+				for mnt in backup_mnts:
+
+					# if the local mount point is the same, then add the existing line to the new file,
+					# put the backup mnt into new file as a comment, then remove the mnt from the
+					# list of unused backup mnts
+					if mnt.fs_file == tup.fs_file:
+						new_lines.append(line)
+						new_lines.append('#' + mnt.unparsed)
+						unused_mnts.remove(mnt)
+						break
+
+				else:   # no break
+
+					# if there is no match, then just add the line as-is to the new file
+					new_lines.append(line)
+
+			# add on all the unused mnts from the backup up file at then end
+			new_lines.extend(unused_mnts)
+
+		if new_lines:
+
+			# create the new, replacement fstab file
+			# we have to unquify the list first, as the proceedure above could result in multiple 
+			# commented out lines appearing in the restored fstab
+			# it's just easier to remove them here than avoid adding them in the first place
+			with open('/tmp/fstab', 'w') as f:
+				f.writelines(uniquify(new_lines))
+
+			# finally, copy the temp fstab over the live fstab
+			res = subprocess.call(["sudo", "mv", '/tmp/fstab', '/etc/fstab' ])
 
 
 
