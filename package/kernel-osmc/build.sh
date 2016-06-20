0e6afa9c2dc6f7549d6e74f6047c9f124fa7f2b5
Sam Nazarko <email@samnazarko.co.uk>
2016-06-20 17:18:01 +0100

    [package] [kernel-osmc] RBP: round again..
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

--- package/kernel-osmc/build.sh
+++ package/kernel-osmc/build.sh
@@ -10,8 +10,8 @@ INITRAMFS_EMBED=2
 INITRAMFS_NOBUILD=4
 
 . ../common.sh
-test $1 == rbp1 && VERSION="4.4.13" && REV="4" && FLAGS_INITRAMFS=$(($INITRAMFS_BUILD + $INITRAMFS_EMBED)) && IMG_TYPE="zImage"
-test $1 == rbp2 && VERSION="4.4.13" && REV="4" && FLAGS_INITRAMFS=$(($INITRAMFS_BUILD + $INITRAMFS_EMBED)) && IMG_TYPE="zImage"
+test $1 == rbp1 && VERSION="4.4.13" && REV="5" && FLAGS_INITRAMFS=$(($INITRAMFS_BUILD + $INITRAMFS_EMBED)) && IMG_TYPE="zImage"
+test $1 == rbp2 && VERSION="4.4.13" && REV="5" && FLAGS_INITRAMFS=$(($INITRAMFS_BUILD + $INITRAMFS_EMBED)) && IMG_TYPE="zImage"
 test $1 == vero && VERSION="4.4.0" && REV="2" && FLAGS_INITRAMFS=$(($INITRAMFS_BUILD + $INITRAMFS_EMBED)) && IMG_TYPE="zImage"
 test $1 == vero2 && VERSION="3.10.101" && REV="16" && FLAGS_INITRAMFS=$(($INITRAMFS_BUILD)) && IMG_TYPE="uImage"
 test $1 == atv && VERSION="4.2.3" && REV="12" && FLAGS_INITRAMFS=$(($INITRAMFS_NOBUILD)) && IMG_TYPE="zImage"
@@ -120,7 +120,7 @@ then
 		$BUILD dtbs
 		mv arch/arm/boot/dts/*.dtb ../../files-image/boot/dtb-${VERSION}-${REV}-osmc/
 		rename -v 's/\.dtbo$/\-overlay.dtb/' arch/arm/boot/dts/overlays/*.dtbo
-		mv arch/arm/boot/dts/overlays/*.dtbo ../../files-image/boot/dtb-${VERSION}-${REV}-osmc/overlays
+		mv arch/arm/boot/dts/overlays/*.dtb ../../files-image/boot/dtb-${VERSION}-${REV}-osmc/overlays
 		mv arch/arm/boot/dts/overlays/README ../../files-image/boot/dtb-${VERSION}-${REV}-osmc/overlays
 	fi
 	if [ "$1" == "vero" ]
