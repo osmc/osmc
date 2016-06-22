3434443af051df704e0b2c7d0d3199e52d7439ce
Sam Nazarko <email@samnazarko.co.uk>
2016-06-22 04:32:26 +0100

    [package] [kernel-osmc] ATV: fix 8192cu patch
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

--- package/kernel-osmc/build.sh
+++ package/kernel-osmc/build.sh
@@ -14,7 +14,7 @@ test $1 == rbp1 && VERSION="4.4.13" && REV="5" && FLAGS_INITRAMFS=$(($INITRAMFS_
 test $1 == rbp2 && VERSION="4.4.13" && REV="5" && FLAGS_INITRAMFS=$(($INITRAMFS_BUILD + $INITRAMFS_EMBED)) && IMG_TYPE="zImage"
 test $1 == vero && VERSION="4.4.0" && REV="2" && FLAGS_INITRAMFS=$(($INITRAMFS_BUILD + $INITRAMFS_EMBED)) && IMG_TYPE="zImage"
 test $1 == vero2 && VERSION="3.10.101" && REV="16" && FLAGS_INITRAMFS=$(($INITRAMFS_BUILD)) && IMG_TYPE="uImage"
-test $1 == atv && VERSION="4.2.3" && REV="12" && FLAGS_INITRAMFS=$(($INITRAMFS_NOBUILD)) && IMG_TYPE="zImage"
+test $1 == atv && VERSION="4.2.3" && REV="13" && FLAGS_INITRAMFS=$(($INITRAMFS_NOBUILD)) && IMG_TYPE="zImage"
 test $1 == pc && VERSION="4.2.3" && REV="5" && FLAGS_INITRAMFS=$(($INITRAMFS_BUILD + $INITRAMFS_EMBED)) && IMG_TYPE="zImage"
 if [ $1 == "rbp1" ] || [ $1 == "rbp2" ] || [ $1 == "atv" ] || [ $1 == "pc" ]
 then
