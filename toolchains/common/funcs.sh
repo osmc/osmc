466ee5cc28d59033a4d0cbae2f5c36d854e3ecdc
Sam Nazarko <email@samnazarko.co.uk>
2016-08-17 07:30:41 +0100

    [toolchain] Fixed missing toolchain path parameter when creating /root/.ccache directory. This needs to exist for buildsystem to mount persistent ccache overlay
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

--- toolchains/common/funcs.sh
+++ toolchains/common/funcs.sh
@@ -26,7 +26,7 @@ function patchfs()
 function configure_ccache()
 {
 	chroot ${1} apt-get -y install --no-install-recommends ccache
-	mkdir -p /root/.ccache
+	mkdir -p ${1}/root/.ccache
 }
 
 CHROOT_PKGS="build-essential nano sudo libeatmydata1"
