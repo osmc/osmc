944814df09e8b1640dc5bd4c16f2d06fefdf5d05
Sam Nazarko <email@samnazarko.co.uk>
2016-10-06 13:02:35 +0100

    [buildsystem] Ensure that we have the tools to extract bzip files
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

--- scripts/common.sh
+++ scripts/common.sh
@@ -161,11 +161,12 @@ function pull_source()
 	chrootval=$?
 	if [ $chrootval == 2 ] || [ $chrootval == 0 ]; then return; fi # Prevent recursive loop
 	if ! command -v unzip >/dev/null 2>&1; then update_sources && verify_action && install_package "unzip" && verify_action; fi
+	if ! command -v bunzip2 >/dev/null 2>&1; then update_sources && verify_action && install_package "bzip2" && verify_action; fi
 	if ! command -v git >/dev/null 2>&1; then update_sources && verify_action && install_package "git" && verify_action; fi
 	if ! command -v svn >/dev/null 2>&1; then update_sources && verify_action && install_package "subversion" && verify_action; fi
 	if ! command -v wget >/dev/null 2>&1; then update_sources && verify_action && install_package "wget" && verify_action; fi
 	if [ -d ${2} ]
-	then 
+	then
 		if [ "$2" != "." ]
 		then
 			echo "Cleaning old source" && rm -rf ${2}; fi
