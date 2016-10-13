13e2acd546486d5017455cdda3fad0fc1846d091
Sam Nazarko <email@samnazarko.co.uk>
2016-10-13 20:55:37 +0100

    [package] [mediacenter-skin-osmc] Krypton: Bump version. If older version, delete addon_data/script.skinshortcuts/skin.osmc.properties to prevent upgrade issues. Fix chown on no longer needed dependencies. Pull the new skin remotely and deprecate the old one.
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

--- package/mediacenter-skin-osmc/build.sh
+++ package/mediacenter-skin-osmc/build.sh
@@ -5,5 +5,12 @@
 
 . ../common.sh
 
+REV="8888558e458f0d267f1751b2fb2ebeb58c7f2275"
 echo -e "Building package mediacenter-skin-osmc"
+echo -e "Downloading skin"
+pull_source "https://github.com/osmc/skin.osmc/archive/${REV}.tar.gz" "$(pwd)/src"
+if [ $? != 0 ]; then echo -e "Error downloading" && exit 1; fi
+echo -e "Moving files in to place"
+mkdir -p files/usr/share/kodi/addons
+cp -ar src/skin.osmc-${REV}/ files/usr/share/kodi/addons/skin.osmc
 dpkg_build files/ mediacenter-skin-osmc.deb
