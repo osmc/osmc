d6a3966b4039722c8624a44ab90df21bbae1faa1
Sam Nazarko <email@samnazarko.co.uk>
2017-01-27 15:52:33 +0000

    [package] [mediacenter-osmc] Bump to Krypton RC4
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

--- package/mediacenter-osmc/build.sh
+++ package/mediacenter-osmc/build.sh
@@ -6,7 +6,7 @@
 . ../common.sh
 if [ "$1" == "rbp1" ] || [ "$1" == "rbp2" ] || [ "$1" == "vero" ] || [ "$1" == "atv" ] || [ "$1" == "pc" ] || [ "$1" == "vero2" ]
 then
-pull_source "https://github.com/xbmc/xbmc/archive/4d93228e19afa928d08327da6d414e999d7273d6.tar.gz" "$(pwd)/src"
+pull_source "https://github.com/xbmc/xbmc/archive/e80803a74474e964731631a37314bddb83e8b335.tar.gz" "$(pwd)/src"
 API_VERSION="17"
 else
 pull_source "https://github.com/xbmc/xbmc/archive/master.tar.gz" "$(pwd)/kodi"
