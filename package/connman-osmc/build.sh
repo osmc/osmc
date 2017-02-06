b5b7c11cd9812ebe04025618ca024d956d001831
ste-fan <s.t.e-f.a.n@web.de>
2017-02-06 12:03:22 +0100

    [package] [connman-osmc] Upgrade to 1.33
    Delete obsolete (already incorporated) patches and update remaining patches.

--- package/connman-osmc/build.sh
+++ package/connman-osmc/build.sh
@@ -4,7 +4,7 @@
 #!/bin/bash
 
 . ../common.sh
-VERSION="1.30"
+VERSION="1.33"
 pull_source "https://www.kernel.org/pub/linux/network/connman/connman-${VERSION}.tar.gz" "$(pwd)/src"
 if [ $? != 0 ]; then echo -e "Error fetching connman source" && exit 1; fi
 # Build in native environment
