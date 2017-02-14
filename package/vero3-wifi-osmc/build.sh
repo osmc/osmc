7f1cd272c598497fc2bd5ce24e6121f3d869f01a
Sam Nazarko <email@samnazarko.co.uk>
2017-02-14 16:57:20 +0000

    [package] [vero3-wifi-osmc] No longer need to build in toolchain, clean up patchram
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

--- package/vero3-wifi-osmc/build.sh
+++ package/vero3-wifi-osmc/build.sh
@@ -5,22 +5,7 @@
 
 . ../common.sh
 
-# Build in native environment
-build_in_env "${1}" $(pwd) "vero3-wifi-osmc"
-build_return=$?
-if [ $build_return == 99 ]
-then
-	echo -e "Building vero3-wifi-osmc"
-	out=$(pwd)/files
-	make clean
-	mkdir -p ${out}/usr/bin
-	pushd src
-	gcc brcm_patchram_plus.c -o ${out}/usr/bin/brcm_patchram_plus
-	if [ $? != 0 ]; then echo "Error occured during build" && exit 1; fi
-	strip_files "${out}"
-	popd
-	dpkg_build files/ vero3-wifi-osmc.deb
-	build_return=$?
-fi
-teardown_env "${1}"
-exit $build_return
+echo -e "Building vero3-wifi-osmc"
+out=$(pwd)/files
+make clean
+dpkg_build files/ vero3-wifi-osmc.deb
