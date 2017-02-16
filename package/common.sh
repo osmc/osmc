f30a2cf9db2fdaf32c4a80f623761367790bba24
Sam Nazarko <email@samnazarko.co.uk>
2017-02-16 18:16:32 +0000

    [buildsystem] Add vero3_64 build target: this will be used for the kernel
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

--- package/common.sh
+++ package/common.sh
@@ -140,6 +140,7 @@ function build_in_env()
 	test $DEP == vero1 && DEP="armv7"
 	test $DEP == vero2 && DEP="armv7"
 	test $DEP == vero3 && DEP="armv7"
+	test $DEP == vero3_64 && DEP="aarch64"
 	test $DEP == rbp1 && DEP="armv6l"
 	test $DEP == atv && DEP="i386"
 	test $DEP == pc && DEP="amd64"
