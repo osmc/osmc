36af2d59887a21349b9c40b08630a3751a0bf4ee
DBMandrake <sbyrnand@gmail.com>
2017-03-07 15:06:56 +0000

    [package] [mediacenter-addon-osmc] Silence the output of systemctl so it doesn't spam the system journal.

--- package/mediacenter-addon-osmc/src/script.module.osmcsetting.networking/resources/lib/osmc_systemd.py
+++ package/mediacenter-addon-osmc/src/script.module.osmcsetting.networking/resources/lib/osmc_systemd.py
@@ -1,6 +1,6 @@
 import subprocess
 import time
-
+import os
 
 def is_service_running(service_name):
     enabled = is_service_enabled(service_name)
@@ -18,25 +18,33 @@ def toggle_service(service_name, enable):
 
 
 def is_service_enabled(service_name):
-    process = subprocess.call(['/bin/systemctl', 'is-enabled', service_name])
+    fnull = open(os.devnull, 'w')
+    process = subprocess.call(['/bin/systemctl', 'is-enabled', service_name], stderr=fnull, stdout=fnull)
+    fnull.close()
     if process == 0:
         return True
     return False
 
 
 def is_service_active(service_name):
-    process = subprocess.call(['/bin/systemctl', 'is-active', service_name])
+    fnull = open(os.devnull, 'w')
+    process = subprocess.call(['/bin/systemctl', 'is-active', service_name], stderr=fnull, stdout=fnull)
+    fnull.close()
     if process == 0:
         return True
     return False
 
 
 def update_service(service_name, service_status):
-    subprocess.call(['sudo', '/bin/systemctl', service_status, service_name])
+    fnull = open(os.devnull, 'w')
+    subprocess.call(['sudo', '/bin/systemctl', service_status, service_name], stderr=fnull, stdout=fnull)
+    fnull.close()
     time.sleep(1)
 
 def is_service_running(service_name):
-    process = subprocess.call(['/bin/systemctl', 'status', service_name])
+    fnull = open(os.devnull, 'w')
+    process = subprocess.call(['/bin/systemctl', 'status', service_name], stderr=fnull, stdout=fnull)
+    fnull.close()
     if process == 0:
         return True
     return False
