e9ac1151d45f7a43369115f1f3bfee5651d3a84d
Sam Nazarko <email@samnazarko.co.uk>
2017-03-27 20:45:30 +0100

    [package] [rapidjson-osmc] Fix include path
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

--- package/rapidjson-osmc/build.sh
+++ package/rapidjson-osmc/build.sh
@@ -8,5 +8,5 @@
 pull_source "https://github.com/miloyip/rapidjson/archive/v1.1.0.tar.gz" "$(pwd)/src"
 if [ $? != 0 ]; then echo -e "Error downloading" && exit 1; fi
 mkdir -p files-dev/usr/include
-cp -ar src/*rapidjson*/include files-dev/usr/include
+cp -ar src/*rapidjson*/include files-dev/usr/
 dpkg_build files-dev rapidjson-dev-osmc.deb
