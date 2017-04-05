c172728e9a410ea37789bb61de28c090bdfe9a59
Sam Nazarko <email@samnazarko.co.uk>
2017-04-05 02:46:40 +0100

    [package] [connman-osmc] After 3a507 (upstream), ConnMan uses tmpfiles.d to handle resolv.conf with system protection. This isn't appropriate for OSMC. Temporarily remove the tmpfiles.d entry after building and submit a patch to upstream to disable this in ./configure later.
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

--- package/connman-osmc/build.sh
+++ package/connman-osmc/build.sh
@@ -37,6 +37,7 @@ then
 	cp -ar src/connman-dbus-osmc.conf ${out}/etc/dbus-1/system.d/connman-dbus.conf
 	cp -ar plugins/polkit.policy ${out}/usr/share/polkit-1/actions/net.connman.policy
 	cp -ar client/connmanctl ${out}/usr/sbin/connmanctl
+	rm -rf ${out}/usr/lib/tmpfiles.d
 	popd
 	strip_files "${out}"
 	fix_arch_ctl "files/DEBIAN/control"
