b0027e59bff8343afbb881d2f7f7c2d63baa15a9
Sam Nazarko <email@samnazarko.co.uk>
2017-04-07 16:04:00 +0100

    [package] [libcec-osmc] Bump to 8563411d1b449edab6edb52b7beba053c1106ec0
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

--- package/libcec-osmc/build.sh
+++ package/libcec-osmc/build.sh
@@ -5,7 +5,7 @@
 
 . ../common.sh
 
-pull_source "https://github.com/Pulse-Eight/libcec/archive/0a97062dd4b196ceeb003ec41841c7a7edc36dd1.tar.gz" "$(pwd)/src"
+pull_source "https://github.com/Pulse-Eight/libcec/archive/8563411d1b449edab6edb52b7beba053c1106ec0.tar.gz" "$(pwd)/src"
 if [ $? != 0 ]; then echo -e "Error downloading" && exit 1; fi
 # Build in native environment
 build_in_env "${1}" $(pwd) "libcec-osmc"
