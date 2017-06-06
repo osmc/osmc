c5ec069cfcbb9361548df9b6a63e177327654ea0
Sam Nazarko <email@samnazarko.co.uk>
2017-06-06 18:15:35 +0100

    [package] [kernel-osmc] Vero 4K: bump version
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

--- package/kernel-osmc/build.sh
+++ package/kernel-osmc/build.sh
@@ -16,7 +16,7 @@ test $1 == vero && VERSION="4.4.0" && REV="12" && FLAGS_INITRAMFS=$(($INITRAMFS_
 test $1 == vero2 && VERSION="3.10.105" && REV="1" && FLAGS_INITRAMFS=$(($INITRAMFS_BUILD)) && IMG_TYPE="uImage"
 test $1 == atv && VERSION="4.2.3" && REV="24" && FLAGS_INITRAMFS=$(($INITRAMFS_NOBUILD)) && IMG_TYPE="zImage"
 test $1 == pc && VERSION="4.2.3" && REV="12" && FLAGS_INITRAMFS=$(($INITRAMFS_BUILD + $INITRAMFS_EMBED)) && IMG_TYPE="zImage"
-test $1 == vero364 && VERSION="3.14.29" && REV="18" && FLAGS_INITRAMFS=$(($INITRAMFS_BUILD)) && IMG_TYPE="zImage"
+test $1 == vero364 && VERSION="3.14.29" && REV="19" && FLAGS_INITRAMFS=$(($INITRAMFS_BUILD)) && IMG_TYPE="zImage"
 if [ $1 == "rbp1" ] || [ $1 == "rbp2" ] || [ $1 == "atv" ] || [ $1 == "pc" ]
 then
 	if [ -z $VERSION ]; then echo "Don't have a defined kernel version for this target!" && exit 1; fi
@@ -263,7 +263,6 @@ then
                 cp -a "${kernel_path}/include/media/videobuf-res.h" "linux/include/media/"
                 echo "obj-m += videobuf-res.o" >> "linux/drivers/media/v4l2-core/Makefile"
 		cp "../drivers/media/rc/meson-ir.c" "linux/drivers/media/rc/meson-ir.c"
-		sed -i 's,allowed_protos,allowed_protocols,g' "linux/drivers/media/rc/meson-ir.c"
 		echo "obj-m += meson-ir.o" >> "linux/drivers/media/rc/Makefile"
                 $BUILD VER=${VERSION} SRCDIR=$(pwd)/../
                 popd
