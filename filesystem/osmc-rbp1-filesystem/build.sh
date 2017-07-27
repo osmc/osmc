c34ae5a07c9869f9f00f8b8103876040062912ce
Sam Nazarko <email@samnazarko.co.uk>
2017-07-27 17:04:48 +0100

    [filesystems] Set sources.list from $RLS
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

--- filesystem/osmc-rbp1-filesystem/build.sh
+++ filesystem/osmc-rbp1-filesystem/build.sh
@@ -51,8 +51,8 @@ enable_nw_chroot "${DIR}"
 verify_action
 
 # Set up sources.list
-echo "deb http://mirrordirector.raspbian.org/raspbian jessie main contrib non-free
-deb http://apt.osmc.tv jessie main
+echo "deb http://mirrordirector.raspbian.org/raspbian $RLS main contrib non-free
+deb http://apt.osmc.tv $RLS main
 " > ${DIR}/etc/apt/sources.list
 
 # Performing chroot operation
