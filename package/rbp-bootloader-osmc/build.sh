8777a44a2c571a1c7162138f5de6c326f3a63db6
Sam Nazarko <email@samnazarko.co.uk>
2017-07-27 22:32:42 +0100

    [package] [rbp-bootloader-osmc] Bump to 9b85acd3fff8e1a57ceca9c57b56f82763051d2e
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

--- package/rbp-bootloader-osmc/build.sh
+++ package/rbp-bootloader-osmc/build.sh
@@ -9,7 +9,7 @@ echo -e "Building package rbp-bootloader-osmc"
 
 BOOT="files/boot"
 FWFILES=( "LICENCE.broadcom" "start_x.elf" "fixup_x.dat" "bootcode.bin" )
-REV="0f315f88ac91f9be93544bfd757f8d55ca4cf099"
+REV="9b85acd3fff8e1a57ceca9c57b56f82763051d2e"
 
 make clean
 
