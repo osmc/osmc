69b2ed99a1bcaa1c483f14d728e6b527106a59b9
Sam Nazarko <email@samnazarko.co.uk>
2017-09-02 19:15:54 +0100

    [package] [diskmount-osmc] Bring in the newly generated udisks/udisks-glue packages
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

--- package/diskmount-osmc/build.sh
+++ package/diskmount-osmc/build.sh
@@ -21,6 +21,8 @@ then
     sed '/Package/d' -i files/DEBIAN/control
     echo "Package: ${1}-diskmount-osmc" >> files/DEBIAN/control
     make clean
+#    sed '/Depends/d' -i files/DEBIAN/control # mind predepends
+    echo "Depends: ${1}-udisks-osmc, ${1}-udisks-glue-osmc, hdparm, dosfstools, exfat-utils, exfat-fuse, hfsutils, hfsprogs, ntfs-3g, policykit-1, eject" >> files/DEBIAN/control
     fix_arch_ctl "files/DEBIAN/control"
     dpkg_build files/ ${1}-diskmount-osmc.deb
     build_return=$?
