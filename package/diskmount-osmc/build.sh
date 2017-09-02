e75b52db2209f968544463c74e827af9db5a72ef
Sam Nazarko <email@samnazarko.co.uk>
2017-09-02 18:34:31 +0100

    [package] [diskmount-osmc] Make architecture specific
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

--- package/diskmount-osmc/build.sh
+++ package/diskmount-osmc/build.sh
@@ -5,6 +5,26 @@
 
 . ../common.sh
 
+if [ "$1" == "trans" ]
+then
+    echo -e "Building transitional package"
+    dpkg_build files-trans ${1}-diskmount-osmc.deb
+    exit 0
+fi
+
 echo -e "Building disk mounting package"
-make clean
-dpkg_build files/ diskmount-osmc.deb
+# Build in native environment
+build_in_env "${1}" $(pwd) "diskmount-osmc"
+build_return=$?
+if [ $build_return == 99 ]
+then
+    sed '/Package/d' -i files/DEBIAN/control
+    echo "Package: ${1}-diskmount-osmc" >> files/DEBIAN/control
+    make clean
+    fix_arch_ctl "files/DEBIAN/control"
+    dpkg_build files/ ${1}-diskmount-osmc.deb
+    build_return=$?
+fi
+teardown_env "${1}"
+exit $build_return
+
