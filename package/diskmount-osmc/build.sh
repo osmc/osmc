7f24f532f740ef9592e0283697891a8e315f465b
DBMandrake <sbyrnand@gmail.com>
2017-09-14 09:22:12 +0100

    [package] [diskmount-osmc] Rewrite package transition to arch specific package and reinstate custom cdrom udev rule.

--- package/diskmount-osmc/build.sh
+++ package/diskmount-osmc/build.sh
@@ -18,10 +18,10 @@ build_in_env "${1}" $(pwd) "diskmount-osmc"
 build_return=$?
 if [ $build_return == 99 ]
 then
-    sed '/Package/d' -i files/DEBIAN/control
+    sed '/^Package/d' -i files/DEBIAN/control
     echo "Package: ${1}-diskmount-osmc" >> files/DEBIAN/control
     make clean
-#    sed '/Depends/d' -i files/DEBIAN/control # mind predepends
+    sed '/^Depends/d' -i files/DEBIAN/control
     echo "Depends: ${1}-udisks-osmc, ${1}-udisks-glue-osmc, hdparm, dosfstools, exfat-utils, exfat-fuse, hfsutils, hfsprogs, ntfs-3g, policykit-1, eject" >> files/DEBIAN/control
     fix_arch_ctl "files/DEBIAN/control"
     dpkg_build files/ ${1}-diskmount-osmc.deb
