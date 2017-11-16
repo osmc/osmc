855390d6294968e0e1a7a1a320a3bfeeef54b8af
Sam Nazarko <email@samnazarko.co.uk>
2017-11-16 16:49:21 +0000

    [package] [kernel-package-osmc] Ensure helper files installed to target
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

--- package/kernel-package-osmc/build.sh
+++ package/kernel-package-osmc/build.sh
@@ -13,4 +13,7 @@ make clean
 echo Downloading source
 pull_source "https://github.com/osmc/kernel-package-tool-osmc/archive/${REV}.tar.gz" "$(pwd)/src"
 if [ $? != 0 ]; then echo -e "Error downloading" && exit 1; fi
+pushd src/kernel-package-tool-osmc-${REV}
+make install DESTDIR=${out}
+popd
 dpkg_build files/ kernel-package-osmc.deb
