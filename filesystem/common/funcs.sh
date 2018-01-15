d8365cda73bf0f3269fee9385890fb203da5b479
Sam Nazarko <email@samnazarko.co.uk>
2018-01-15 18:05:51 +0000

    [filesystem] Add support for creating rc.local template
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

--- filesystem/common/funcs.sh
+++ filesystem/common/funcs.sh
@@ -97,6 +97,26 @@ function enable_legacy_elf()
 	chroot ${1} ln -s /lib/arm-linux-gnueabihf/ld-linux.so.3 /lib/ld-linux.so.3
 }
 
+function create_rc_local()
+{
+	cat <<EOF >${1}/etc/rc.local
+#!/bin/sh -e
+#
+# rc.local
+#
+# This script is executed at the end of each multiuser runlevel.
+# Make sure that the script will "exit 0" on success or any other
+# value on error.
+#
+# In order to enable or disable this script just change the execution
+# bits.
+#
+# By default this script does nothing.
+
+exit 0
+EOF
+}
+
 export -f setup_osmc_user
 export -f setup_hostname
 export -f setup_hosts
@@ -107,3 +127,4 @@ export -f create_base_fstab
 export -f conf_tty
 export -f setup_busybox_links
 export -f enable_legacy_elf
+export -f create_rc_local
