2ee425c479dbe0557a6d0983cc3a31bc6ffb37a9
Sam Nazarko <email@samnazarko.co.uk>
2018-04-19 13:10:03 +0100

    [package] [base-files-osmc] Warn the user if they are logging in via SSH using the default password
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

---
+++ package/base-files-osmc/files/etc/profile.d/105-check-ssh-password.sh
@@ -0,0 +1,6 @@
+if systemctl is-active ssh -q && sudo grep -q "osmc:\$1\$P.ZH6EFu\$L08/1ZYI6FdHu3aw0us.u0:17569:0:99999:7:::" /etc/shadow && [ ! -f /home/osmc/.nosshwarn ]
+then
+    echo "Warning: you are using SSH with the default OSMC credentials. This is a security risk."
+    echo "To change your password, type passwd"
+    echo "To disable this warning, type touch /home/osmc/.nosshwarn"
+fi
