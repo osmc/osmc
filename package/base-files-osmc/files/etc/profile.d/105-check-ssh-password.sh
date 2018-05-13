201002333bed75928ee99f6c2265ac1888085254
Sam Nazarko <email@samnazarko.co.uk>
2018-05-13 18:36:07 +0100

    [package] [base-files-osmc] SSH password check should return not exit
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

--- package/base-files-osmc/files/etc/profile.d/105-check-ssh-password.sh
+++ package/base-files-osmc/files/etc/profile.d/105-check-ssh-password.sh
@@ -1,5 +1,5 @@
 user=$(whoami)
-if [ "$user" != "osmc" ]; then exit 0; fi
+if [ "$user" != "osmc" ]; then return 0; fi
 if systemctl is-active ssh -q && sudo grep -q "osmc:\$1\$P.ZH6EFu\$L08/1ZYI6FdHu3aw0us.u0:17569:0:99999:7:::" /etc/shadow && [ ! -f /home/osmc/.nosshwarn ]
 then
     echo "Warning: you are using SSH with the default OSMC credentials. This is a security risk."
