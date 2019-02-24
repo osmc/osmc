5c7df825c427bc0206ebe6668698a5dc97e01912
Sam Nazarko <email@samnazarko.co.uk>
2019-02-24 16:02:32 +0000

    [package] [mediacenter-osmc] Bump to v18.1 final
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

--- package/mediacenter-osmc/build.sh
+++ package/mediacenter-osmc/build.sh
@@ -6,7 +6,7 @@
 . ../common.sh
 if [ "$1" == "rbp1" ] || [ "$1" == "rbp2" ] || [ "$1" == "pc" ] || [ "$1" == "vero2" ] || [ "$1" == "vero3" ]
 then
-pull_source "https://github.com/xbmc/xbmc/archive/d81c34c4651a4dfbc9ab0e893fec40f92b5e31e4.tar.gz" "$(pwd)/src"
+pull_source "https://github.com/xbmc/xbmc/archive/8cfdc895f3678fc56aef221711b58854cd761378.tar.gz" "$(pwd)/src"
 API_VERSION="18"
 else
 pull_source "https://github.com/xbmc/xbmc/archive/master.tar.gz" "$(pwd)/kodi"
