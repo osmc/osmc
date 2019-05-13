1c49ce85217b245e23b03958211a363f5ddaa7f1
Sam Nazarko <email@samnazarko.co.uk>
2019-05-13 08:53:31 +0100

    [package] [mediacenter-osmc] Bump Leia to top of tree
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

--- package/mediacenter-osmc/build.sh
+++ package/mediacenter-osmc/build.sh
@@ -6,7 +6,7 @@
 . ../common.sh
 if [ "$1" == "rbp1" ] || [ "$1" == "rbp2" ] || [ "$1" == "pc" ] || [ "$1" == "vero2" ] || [ "$1" == "vero3" ]
 then
-pull_source "https://github.com/xbmc/xbmc/archive/f2643566d0fe86534aa6732cc5c95ede220705c5.tar.gz" "$(pwd)/src"
+pull_source "https://github.com/xbmc/xbmc/archive/5a47c2c342dbfca174bade38a57794f8b4c7a993.tar.gz" "$(pwd)/src"
 API_VERSION="18"
 else
 pull_source "https://github.com/xbmc/xbmc/archive/master.tar.gz" "$(pwd)/kodi"
