5a41c22137d80f63454b6362b2271ca382e2c872
Ch1llb0/skin.osmc <36623079+Ch1llb0@users.noreply.github.com>
2019-08-05 19:52:20 +0200

    [package] [mediacenter-skin-osmc] Bump to 7ad741a0cc37070269779ca56e1f15939e3eaa23

--- package/mediacenter-skin-osmc/build.sh
+++ package/mediacenter-skin-osmc/build.sh
@@ -7,7 +7,7 @@
 
 make clean
 
-REV="7d8db2c5080b63c559a36f281b0f04c962289dab"
+REV="7ad741a0cc37070269779ca56e1f15939e3eaa23"
 echo -e "Building package mediacenter-skin-osmc"
 echo -e "Downloading skin"
 pull_source "https://github.com/osmc/skin.osmc/archive/${REV}.tar.gz" "$(pwd)/src"
