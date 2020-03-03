e2a7e255aa80be1dd220fbdaf9d80d437bb96e65
Sam Nazarko <email@samnazarko.co.uk>
2020-03-03 02:45:14 +0000

    [package] [mediacenter-osmc] Bump to 18.6 final
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

--- package/mediacenter-osmc/build.sh
+++ package/mediacenter-osmc/build.sh
@@ -6,7 +6,7 @@
 . ../common.sh
 if [ "$1" == "rbp1" ] || [ "$1" == "rbp2" ] || [ "$1" == "pc" ] || [ "$1" == "vero2" ] || [ "$1" == "vero3" ]
 then
-pull_source "https://github.com/xbmc/xbmc/archive/18fe69cc9d2dc5a6d94615ebb0586f0df2bded05.tar.gz" "$(pwd)/src"
+pull_source "https://github.com/xbmc/xbmc/archive/8e967df9218279618bcbfa8a898d8f80f7b4e449.tar.gz" "$(pwd)/src"
 API_VERSION="18"
 else
 pull_source "https://github.com/xbmc/xbmc/archive/master.tar.gz" "$(pwd)/kodi"
