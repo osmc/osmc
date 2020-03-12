f849d8cbae15325521d77b16ca378fc5ae6fe247
Graham Horner <graham@hornercs.co.uk>
2020-03-12 14:23:36 +0000

    [package][bluez-alsa-osmc] Initial commit

---
+++ package/bluez-alsa-osmc/build.sh
@@ -0,0 +1,11 @@
+# (c) 2014-2020 Sam Nazarko
+# email@samnazarko.co.uk
+
+#!/bin/bash
+
+. ../common.sh
+
+echo -e "Building package bluez-alsa-app-osmc"
+
+publish_applications_any $(pwd) "bluez-alsa-app-osmc"
+dpkg_build files/ bluez-alsa-app-osmc.deb
