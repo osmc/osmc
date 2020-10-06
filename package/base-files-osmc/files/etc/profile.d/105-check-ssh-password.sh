913b39a269dcde9c81f202675f5a4f5301e80dd6
JimKnopf0 <36783347+JimKnopf0@users.noreply.github.com>
2020-10-06 06:45:34 +0200

    cosmetic change of warning to better fit standard putty 80x24 window
    eliminate unwanted line break in standard putty window

--- package/base-files-osmc/files/etc/profile.d/105-check-ssh-password.sh
+++ package/base-files-osmc/files/etc/profile.d/105-check-ssh-password.sh
@@ -5,10 +5,11 @@ pwd_hash_method=$(echo $shadow_entry|cut -d '$' -f 2)
 pwd_hash_salt=$(echo $shadow_entry|cut -d '$' -f 3)
 pwd_hash_rest=$(echo $shadow_entry|cut -d '$' -f 4)
 pwd_hash=$(echo $pwd_hash_rest|cut -d ':' -f 1)
-openssl_hash=$(echo $(openssl passwd -$pwd_hash_method -salt $pwd_hash_salt osmc)|cut -d '$' -f 4)
+openssl_hash=$(echo $(openssl passwd -$pwd_hash_method -salt $pwd_hash_salt $user)|cut -d '$' -f 4)
 if systemctl is-active ssh -q && [ "$pwd_hash" = "$openssl_hash" ] && [ ! -f /home/osmc/.nosshwarn ]
 then
-    echo "Warning: you are using SSH with the default OSMC credentials. This is a security risk!"
+    echo "Warning: You are using SSH with the default OSMC credentials."
+    echo "This is a security risk!"
     echo "To change your password, type passwd"
     echo "To disable this warning, type touch /home/osmc/.nosshwarn"
 fi
