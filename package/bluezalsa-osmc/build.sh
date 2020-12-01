64705e29e7c21bf22022686f00f8286c000f8141
Sam Nazarko <email@samnazarko.co.uk>
2020-12-01 00:11:24 +0000

    [package] [bluez-alsa-app-osmc] Rename to bluezalsa-osmc as we plan to distribute this on the system by default, and there is no advantage to toggling this service on/off unlike something like SSH.
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

---
+++ package/bluezalsa-osmc/build.sh
@@ -0,0 +1,53 @@
+# (c) 2014-2020 Sam Nazarko
+# email@samnazarko.co.uk
+
+#!/bin/bash
+
+. ../common.sh
+VERSION="2.1.0-osmc"
+pull_source "https://github.com/Arkq/bluez-alsa/archive/49ad348808a15485aa7cb2df0a4d13654cc0cee3.tar.gz" "$(pwd)/src"
+if [ $? != 0 ]; then echo -e "Error fetching bluez-alsa source" && exit 1; fi
+# Build in native environment
+build_in_env "${1}" $(pwd) "bluez-alsa-app-osmc"
+build_return=$?
+if [ $build_return == 99 ]
+then
+	echo -e "Building package bluezalsa-osmc"
+	out=$(pwd)/files
+	make clean
+	update_sources
+	handle_dep "libasound2-dev"
+	handle_dep "libbluetooth-dev"
+	handle_dep "libdbus-1-dev"
+	handle_dep "libsbc-dev"
+	handle_dep "libglib2.0-dev"
+	handle_dep "libfdk-aac-dev"
+	handle_dep "automake"
+	handle_dep "libtool"
+	handle_dep "pkg-config"
+	sed '/Package/d' -i files/DEBIAN/control
+	echo "Package: ${1}-bluezalsa-osmc" >> files/DEBIAN/control
+	pushd src
+	SOURCE=$(ls)
+	pushd $SOURCE
+	install_patch "../../patches" "all"
+	# aclocal
+	autoreconf --install
+	mkdir build && pushd build
+	../configure --enable-aac --with-alsaplugindir=/usr/lib/arm-linux-gnueabihf/alsa-lib/ --with-alsaconfdir=/usr/share/alsa/alsa.conf.d
+	if [ $? != 0 ]; then echo -e "Configure failed!" && umount /proc/ > /dev/null 2>&1 && exit 1; fi
+	$BUILD
+	if [ $? != 0 ]; then echo -e "Build failed!" && exit 1; fi
+	make install DESTDIR=${out}
+	#mkdir -p ${out}/etc/dbus-1/system.d
+	#mkdir -p ${out}/usr/share/polkit-1/actions
+	#cp -ar src/connman-dbus-osmc.conf ${out}/etc/dbus-1/system.d/connman-dbus.conf
+	#cp -ar plugins/polkit.policy ${out}/usr/share/polkit-1/actions/net.connman.policy
+	popd; popd; popd
+	strip_files "${out}"
+	fix_arch_ctl "files/DEBIAN/control"
+	dpkg_build files/ ${1}-bluezalsa-osmc.deb
+	build_return=$?
+fi
+teardown_env "${1}"
+exit $build_return
