83b5d4e457bd122cac17330ccfa1c703a9188e2b
Sam Nazarko <email@samnazarko.co.uk>
2021-01-18 23:49:40 +0000

    [package] [kernel-package-osmc] Deprecate kernel-package which was used to package kernels for Debian. We will package directly instead.
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

--- package/kernel-osmc/build.sh
+++ package/kernel-osmc/build.sh
@@ -10,12 +10,24 @@ INITRAMFS_BUILD=1
 INITRAMFS_EMBED=2
 INITRAMFS_NOBUILD=4
 
+sign_module () {
+
+# Generic function to sign kernel module with key
+
+if [ "$SIGN_KERNEL" -eq 0 ]; then
+    echo "* Ignoring request to sign kernel module $1 as device does not request it"
+    return
+else
+    echo "* Signing kernel module $1 as device requests it"
+    scripts/sign-file sha256 certs/signing_key.pem certs/signing_key.x509 $1
+fi
+}
+
 . ../common.sh
-test $1 == rbp2 && VERSION="5.10.3" && REV="1" && FLAGS_INITRAMFS=$(($INITRAMFS_BUILD + $INITRAMFS_EMBED)) && IMG_TYPE="zImage"
-test $1 == rbp464 && VERSION="5.10.3" && REV="1" && FLAGS_INITRAMFS=$(($INITRAMFS_BUILD + $INITRAMFS_EMBED)) && IMG_TYPE="zImage"
-test $1 == vero2 && VERSION="3.10.105" && REV="13" && FLAGS_INITRAMFS=$(($INITRAMFS_BUILD)) && IMG_TYPE="uImage"
-test $1 == pc && VERSION="4.2.3" && REV="16" && FLAGS_INITRAMFS=$(($INITRAMFS_BUILD + $INITRAMFS_EMBED)) && IMG_TYPE="zImage"
-test $1 == vero364 && VERSION="4.9.113" && REV="30" && FLAGS_INITRAMFS=$(($INITRAMFS_BUILD)) && IMG_TYPE="zImage"
+test $1 == rbp2 && VERSION="5.10.3" && REV="1" && FLAGS_INITRAMFS=$(($INITRAMFS_BUILD + $INITRAMFS_EMBED)) && IMG_TYPE="zImage" && SIGN_KERNEL=0
+test $1 == rbp464 && VERSION="5.10.3" && REV="1" && FLAGS_INITRAMFS=$(($INITRAMFS_BUILD + $INITRAMFS_EMBED)) && IMG_TYPE="zImage" && SIGN_KERNEL=0
+test $1 == pc && VERSION="4.2.3" && REV="16" && FLAGS_INITRAMFS=$(($INITRAMFS_BUILD + $INITRAMFS_EMBED)) && IMG_TYPE="zImage" && SIGN_KERNEL=0
+test $1 == vero364 && VERSION="4.9.113" && REV="30" && FLAGS_INITRAMFS=$(($INITRAMFS_BUILD)) && IMG_TYPE="zImage" && SIGN_KERNEL=0
 if [ $1 == "rbp2" ] || [ $1 == "rbp464" ] || [ $1 == "pc" ]
 then
 	if [ -z $VERSION ]; then echo "Don't have a defined kernel version for this target!" && exit 1; fi
@@ -28,7 +40,6 @@ then
 	fi
 	SOURCE_LINUX="https://www.kernel.org/pub/linux/kernel/v${MAJOR}.x/linux-${DL_VERSION}.tar.xz"
 fi
-if [ $1 == "vero2" ]; then SOURCE_LINUX="https://github.com/osmc/vero2-linux/archive/master.tar.gz"; fi
 if [ $1 == "vero364" ]; then SOURCE_LINUX="https://github.com/osmc/vero3-linux/archive/osmc-openlinux-4.9.tar.gz"; fi
 pull_source "${SOURCE_LINUX}" "$(pwd)/src"
 # We need to download busybox and e2fsprogs here because we run initramfs build within chroot and can't pull_source in a chroot
@@ -37,7 +48,7 @@ then
 	. initramfs-src/VERSIONS
 	pull_source "https://busybox.net/downloads/busybox-${BUSYBOX_VERSION}.tar.bz2" "$(pwd)/initramfs-src/busybox"
 	pull_source "https://www.kernel.org/pub/linux/kernel/people/tytso/e2fsprogs/v${E2FSPROGS_VERSION}/e2fsprogs-${E2FSPROGS_VERSION}.tar.gz" "$(pwd)/initramfs-src/e2fsprogs"
-        if [ "$1" == "vero2" ] || [ "$1" == "vero364" ]
+        if [ "$1" == "vero364" ]
 	then
 	    pull_source "https://mirrors.kernel.org/sourceware/lvm2/LVM2.${LVM_VERSION}.tgz" "$(pwd)/initramfs-src/lvm2"
 	fi
@@ -54,8 +65,15 @@ then
 	make clean
 	sed '/Package/d' -i files/DEBIAN/control
 	sed '/Depends/d' -i files/DEBIAN/control
+	sed '/Package/d' -i files-image/DEBIAN/control
+	sed '/Version/d' -i files-image/DEBIAN/control
+        sed '/Package/d' -i files-headers/DEBIAN/control
+        sed '/Version/d' -i files-headers/DEBIAN/control
+        sed '/Package/d' -i files-debug/DEBIAN/control
+        sed '/Version/d' -i files-debug/DEBIAN/control
+        sed '/Package/d' -i files-source/DEBIAN/control
+        sed '/Version/d' -i files-source/DEBIAN/control
 	update_sources
-	handle_dep "kernel-package-osmc"
 	handle_dep "libssl-dev"
 	handle_dep "liblz4-tool"
 	handle_dep "cpio"
@@ -63,15 +81,32 @@ then
 	handle_dep "flex"
 	handle_dep "rsync"
 	handle_dep "openssl"
-        if [ "$1" == "vero2" ]  || [ "$1" == "vero364" ]
+        if [ "$1" == "vero364" ]
         then
 	    handle_dep "python"
+	    handle_dep "xxd"
         fi
-	if [ "$1" == "vero2" ]; then handle_dep "u-boot-tools"; fi
-	export KPKG_MAINTAINER="Sam G Nazarko"
-	export KPKG_EMAIL="email@samnazarko.co.uk"
+	if [ "$SIGN_KERNEL" -eq 1 ]
+	then
+		SIG_FILE_AES="/etc/osmc/kernelaes"
+		SIG_FILE_AESIV="/etc/osmc/kernelaesiv"
+		SIG_FILE_KERNELKEY="/etc/osmc/kernelkey.pem"
+		if [ ! -f $SIG_FILE_AES ] || [ ! -f $SIG_FILES_AESIV ] || [ ! -f $SIG_FILE_KERNELKEY ]; then echo "Missing files needed for encrypting kernel image" && exit 1; fi
+	fi
 	JOBS=$(if [ ! -f /proc/cpuinfo ]; then mount -t proc proc /proc; fi; cat /proc/cpuinfo | grep processor | wc -l && umount /proc/ >/dev/null 2>&1)
 	pushd src/*linux*
+        # Add out of tree modules that lack a proper Kconfig and Makefile
+        # Fix CPU architecture
+        ARCH=$(arch)
+        echo $ARCH | grep -q arm
+        if [ $? == 0 ]
+            then
+                ARCH=$(echo $ARCH | tr -d v7l | tr -d v6)
+        fi
+        if [ $ARCH == "i686" ]; then ARCH="i386"; fi
+        if [ "$1" == "vero364" ]; then ARCH=arm64; fi
+        if [ "$1" == "rbp464" ]; then ARCH=arm64; fi
+        export ARCH
 	if [ "$1" == "rbp2" ] || [ "$1" == "rbp464" ]
 	then
 		install_patch "../../patches" "rbp"
@@ -85,30 +120,13 @@ then
                 ./scripts/config --set-val CONFIG_KASAN n
                 ./scripts/config --set-val CONFIG_KCOV n
         fi
+        if [ "$SIGN_KERNEL" -eq 1 ] # This functionality is only supported on Vero for now. Needs fragment in place for support and decision on whether final keysig should be ephem or not depending on chain of trust
+        then
+		scripts/kconfig/merge_config.sh .config arch/${ARCH}/configs/enforce-kernel-module-signature.fragment
+        fi
 	# Set up DTC
 	$BUILD scripts
 	DTC=$(pwd)"/scripts/dtc/dtc"
-	# Conver DTD to DTB
-	if [ "$1" == "vero2" ]
-	then
-		$BUILD meson8b_vero2.dtd
-		$BUILD meson8b_vero2.dtb
-	fi
-	if [ "$1" == "vero364" ] || [ "$1" == "rbp464" ]
-	then
-		export kimage=vmlinuz
-		export target=Image.gz
-		export NEED_DIRECT_GZIP_IMAGE=YES
-		export kimagesrc=arch/arm64/boot/Image
-		export kimagedest=$(pwd)/vmlinuz
-		export kelfimagedest=$(pwd)/vmlinux
-		export KERNEL_ARCH=arm64
-	fi
-        if [ "$1" == "vero364" ]
-	then
-		$BUILD vero3_2g_16g.dtb || $BUILD vero3_2g_16g.dtb
-		$BUILD vero3plus_2g_16g.dtb || $BUILD vero3plus_2g_16g.dtb
-	fi
 	# Initramfs time
 	if ((($FLAGS_INITRAMFS & $INITRAMFS_NOBUILD) != $INITRAMFS_NOBUILD))
 	then
@@ -128,65 +146,76 @@ then
 			popd
 		fi
 	fi
-	if [ "$IMG_TYPE" == "zImage" ] || [ -z "$IMG_TYPE" ]; then make-kpkg --stem $1 kernel_image --append-to-version -${REV}-osmc --jobs $JOBS --revision $REV; fi
-	if [ "$IMG_TYPE" == "uImage" ]; then make-kpkg --uimage --stem $1 kernel_image --append-to-version -${REV}-osmc --jobs $JOBS --revision $REV; fi
-	if [ $? != 0 ]; then echo "Building kernel image package failed" && exit 1; fi
-	make-kpkg --stem $1 kernel_headers --append-to-version -${REV}-osmc --jobs $JOBS --revision $REV
-	if [ $? != 0 ]; then echo "Building kernel headers package failed" && exit 1; fi
-	make-kpkg --stem $1 kernel_source --append-to-version -${REV}-osmc --jobs $JOBS --revision $REV
-	if [ $? != 0 ]; then echo "Building kernel source package failed" && exit 1; fi
+	# Set versioning information for packaging
+	sed "s/EXTRAVERSION =/EXTRAVERSION = -${REV}-osmc/" -i Makefile
+	# Create contents of source package
+	mkdir -p ../../files-source/usr/src/${1}-source-${VERSION}-${REV}-osmc/
+	cp -ar * ../../files-source/usr/src/${1}-source-${VERSION}-${REV}-osmc/
+	# Build the kernel
+	$BUILD
+        if [ $? != 0 ]; then echo "Building kernel image failed" && exit 1; fi
+	# Build kernel modules
+        $BUILD modules_install INSTALL_MOD_STRIP=1 INSTALL_MOD_PATH=../../files-image/
+	# Hack: we don't need in-tree FW. Instead of patching the Makefile, rip it.
+	rm -rf ../../files-image/lib/firmware/
+        if [ $? != 0 ]; then echo "Building kernel modules failed" && exit 1; fi
+	mkdir -p ../../files-image/boot
+	# Copy config in to boot (files-image)
+	cp .config ../../files-image/boot/config-${VERSION}-${REV}-osmc
+	# Copy system map to boot (files-image)
+        cp System.map ../../files-image/boot/System.map-${VERSION}-${REV}-osmc
+	# Debug kernel
+	mkdir -p ../../files-debug/var/osmc
+	cp vmlinux ../../files-debug/var/osmc/${1}-debug-${VERSION}-${REV}-osmc
+	# Install headers in to headers package
+        $BUILD headers_install INSTALL_HDR_PATH=../../files-headers/usr/src/${1}-headers-${VERSION}-${REV}-osmc/
+	if [ $? != 0 ]; then echo "Building kernel packages failed" && exit 1; fi
 	# Make modules directory
 	mkdir -p ../../files-image/lib/modules/${VERSION}-${REV}-osmc/kernel/drivers
 	if [ "$1" == "rbp2" ] || [ "$1" == "rbp464" ]; then mkdir -p ../../files-image/boot/dtb-${VERSION}-${REV}-osmc/overlays; fi
-	if [ "$1" == "vero2" ]; then mkdir -p ../../files-image/boot; fi
         if [ "$1" == "rbp2" ] || [ "$1" == "rbp464" ]
         then
                 $BUILD dtbs
-                mv arch/arm/boot/dts/*.dtb ../../files-image/boot/dtb-${VERSION}-${REV}-osmc/
-                mv arch/arm64/boot/dts/broadcom/*.dtb ../../files-image/boot/dtb-${VERSION}-${REV}-osmc/
-                mv arch/arm*/boot/dts/overlays/*.dtbo ../../files-image/boot/dtb-${VERSION}-${REV}-osmc/overlays
-                mv arch/arm/boot/dts/overlays/README ../../files-image/boot/dtb-${VERSION}-${REV}-osmc/overlays
+                mv arch/arm/boot/dts/*.dtb ../../files-image/boot/dtb-${VERSION}-${REV}-osmc/ || true
+                mv arch/arm64/boot/dts/broadcom/*.dtb ../../files-image/boot/dtb-${VERSION}-${REV}-osmc/ || true
+                mv arch/arm*/boot/dts/overlays/*.dtbo ../../files-image/boot/dtb-${VERSION}-${REV}-osmc/overlays || true
+                mv arch/arm/boot/dts/overlays/README ../../files-image/boot/dtb-${VERSION}-${REV}-osmc/overlays || true
         fi
-	if [ "$1" == "vero" ]
-	then
-		make imx6dl-vero.dtb
-		mv arch/arm/boot/dts/*.dtb ../../files-image/boot/dtb-${VERSION}-${REV}-osmc/
-	fi
-	if [ "$1" == "vero2" ]
-	then
-		# Special packaging for Android
-                ./scripts/mkbootimg --kernel arch/arm/boot/uImage --ramdisk ../../initramfs-src/initrd.img.gz --second arch/arm/boot/dts/amlogic/meson8b_vero2.dtb --output ../../files-image/boot/kernel-${VERSION}-${REV}-osmc.img
-		if [ $? != 0 ]; then echo "Building Android image for Vero 2 failed" && exit 1; fi
-	fi
 	if [ "$1" == "vero364" ]
         then
+                $BUILD vero3_2g_16g.dtb || $BUILD vero3_2g_16g.dtb
+                $BUILD vero3plus_2g_16g.dtb || $BUILD vero3plus_2g_16g.dtb
 		mkdir -p ../../files-image/boot #hack
                 # Special packaging for Android
 		./scripts/multidtb/multidtb -p scripts/dtc/ -o multi.dtb arch/arm64/boot/dts/amlogic --verbose --page-size 2048
-		./scripts/mkbootimg --kernel arch/arm64/boot/Image.gz --base 0x0 --kernel_offset 0x1080000 --ramdisk ../../initramfs-src/initrd.img.gz --second multi.dtb --output ../../files-image/boot/kernel-${VERSION}-${REV}-osmc.img
-                if [ $? != 0 ]; then echo "Building Android image for Vero 3 failed" && exit 1; fi
+		if [ $? != 0 ]; then echo "Packing DTB failed"; fi
+		DTB_FILE="multi.dtb"
+		if [ "$SIGN_KERNEL" -eq 1 ]
+		then
+			DTB_FILE="multi.dtb.encrypted"
+			scripts/amlogic/stool/sign.sh --sign-kernel -i multi.dtb -k $SIG_FILE_KERNELKEY -a $SIG_FILE_AES --iv $SIG_FILE_AESIV -o multi.dtb.encrypted || true
+			if [ $? != 0 ]; then echo "Signing Device Tree failed" && exit 1; fi
+			scripts/mkbootimg --kernel arch/arm64/boot/Image.gz --pagesize 2048 --header_version 1 --base 0x0 --kernel_offset 0x1080000 --ramdisk ../../initramfs-src/initrd.img.gz --second multi.dtb --output kernel.img
+			if [ $? != 0 ]; then echo "Creating boot image failed in secure mode" && exit 1; fi
+			scripts/amlogic/stool/sign.sh --sign-kernel -i kernel.img -k $SIG_FILE_KERNELKEY -a $SIG_FILE_AES --iv $SIG_FILE_AESIV -o ../../files-image/boot/kernel-${VERSION}-${REV}-osmc.img || true
+			if [ $? != 0 ]; then echo "Signing kernel image failed" && exit 1; fi
+		else
+			scripts/mkbootimg --kernel arch/arm64/boot/Image.gz --pagesize 2048 --header_version 1 --base 0x0 --kernel_offset 0x1080000 --ramdisk ../../initramfs-src/initrd.img.gz --second multi.dtb --output ../../files-image/boot/kernel-${VERSION}-${REV}-osmc.img
+			if [ $? != 0 ]; then echo "Creating boot image failed in non-secure mode" && exit 1; fi
+
+		fi
 		# Hacks for lack of ARM64 native in kernel-package for Jessie
-		cp -ar vmlinuz ../../files-image/boot/vmlinuz-${VERSION}-${REV}-osmc
 		# Device tree for uploading to eMMC
-		cp -ar multi.dtb ../../files-image/boot/dtb-${VERSION}-${REV}-osmc.img
+		cp -ar $DTB_FILE ../../files-image/boot/dtb-${VERSION}-${REV}-osmc.img
         fi
 	if [ "$1" == "rbp464" ]
 	then
-		# For Aarch64 we need to ensure installation on target
-		cp -ar vmlinuz ../../files-image/boot/vmlinuz-${VERSION}-${REV}-osmc
+		cp -ar arch/arm64/boot/zImage ../../files-image/boot/vmlinuz-${VERSION}-${REV}-osmc
 	fi
-	# Add out of tree modules that lack a proper Kconfig and Makefile
-	# Fix CPU architecture
-	ARCH=$(arch)
-    echo $ARCH | grep -q arm
-    if [ $? == 0 ]
-	then
-	    ARCH=$(echo $ARCH | tr -d v7l | tr -d v6)
-	fi
-	if [ $ARCH == "i686" ]; then ARCH="i386"; fi
-	if [ "$1" == "vero364" ]; then ARCH=arm64; fi
-	if [ "$1" == "rbp464" ]; then ARCH=arm64; fi
-	export ARCH
+        if [ "$1" == "rbp2" ]
+        then
+                cp -ar arch/arm/boot/zImage ../../files-image/boot/vmlinuz-${VERSION}-${REV}-osmc
+        fi
 		if [ "$1" == "vero2" ]
 		then
 		# Build RTL8812AU module
@@ -198,26 +227,20 @@ then
 		strip --strip-unneeded drivers/net/wireless/rtl8812au/*8812au.ko
 		cp drivers/net/wireless/rtl8812au/*8812au.ko ../../files-image/lib/modules/${VERSION}-${REV}-osmc/kernel/drivers/net/wireless/
 		fi
-		if [ "$1" == "rbp2" ] || [ "$1" == "vero2" ]
-		then
-                # Build MT7610U model
-                pushd drivers/net/wireless/mt7610u
-                $BUILD
-                if [ $? != 0 ]; then echo -e "Building kernel module failed" && exit 1; fi
-                popd
-                mkdir -p ../../files-image/lib/modules/${VERSION}-${REV}-osmc/kernel/drivers/net/wireless/
-		strip --strip-unneeded drivers/net/wireless/mt7610u/os/linux/mt7610u_sta.ko
-                cp drivers/net/wireless/mt7610u/os/linux/mt7610u_sta.ko ../../files-image/lib/modules/${VERSION}-${REV}-osmc/kernel/drivers/net/wireless/
-                fi
         if [ "$1" == "vero364" ]
 	then
 		# Build V4L2 modules for Vero 4K
 		$BUILD M=drivers/osmc/media_modules CONFIG_AMLOGIC_MEDIA_VDEC_OSMC=m
 		mkdir -p ../../files-image/lib/modules/${VERSION}-${REV}-osmc/kernel/drivers/osmc
-		for file in $(find drivers/osmc/media_modules/ -name "*.ko"); do cp $file ../../files-image/lib/modules/${VERSION}-${REV}-osmc/kernel/drivers/osmc; done
+		for file in $(find drivers/osmc/media_modules/ -name "*.ko"); do
+		sign_module $file
+		cp $file ../../files-image/lib/modules/${VERSION}-${REV}-osmc/kernel/drivers/osmc
+		done
 		# Build OpTEE modules for secureOSMC
 		$BUILD M=drivers/osmc/secureosmc
 		mkdir -p ../../files-image/lib/modules/${VERSION}-${REV}-osmc/kernel/drivers/osmc/secureosmc
+		sign_module "drivers/osmc/secureosmc/optee/optee_armtz.ko"
+		sign_module "drivers/osmc/secureosmc/optee.ko"
 		cp drivers/osmc/secureosmc/optee/optee_armtz.ko ../../files-image/lib/modules/${VERSION}-${REV}-osmc/kernel/drivers/osmc/secureosmc
 		cp drivers/osmc/secureosmc/optee.ko ../../files-image/lib/modules/${VERSION}-${REV}-osmc/kernel/drivers/osmc/secureosmc
 	fi
@@ -225,26 +248,87 @@ then
 	ARCH=$(arch)
 	export ARCH
 	popd
-	# Move all of the Debian packages so they are where we would expect them
-	mv src/${1}-*.deb .
-	# Disassemble kernel image package to add device tree overlays, additional out of tree modules etc
-	dpkg -x ${1}-image*.deb files-image/
-	dpkg-deb -e ${1}-image*.deb files-image/DEBIAN
-	if [ "$1" == "vero364" ]; then sed -ie 's/^Depends:.*$/&, vero3-bootloader-osmc:armhf (>= 1.0.0)/g' files-image/DEBIAN/control; fi
-	rm ${1}-image*.deb
+	echo "Package: ${1}-image-${VERSION}-${REV}-osmc" >> files-image/DEBIAN/control
+	echo "Version: ${VERSION}-${REV}-osmc" >> files-image/DEBIAN/control
+        echo "Package: ${1}-headers-${VERSION}-${REV}-osmc" >> files-headers/DEBIAN/control
+        echo "Version: ${VERSION}-${REV}-osmc" >> files-headers/DEBIAN/control
+        echo "Package: ${1}-debug-${VERSION}-${REV}-osmc" >> files-debug/DEBIAN/control
+        echo "Version: ${VERSION}-${REV}-osmc" >> files-debug/DEBIAN/control
+        echo "Package: ${1}-source-${VERSION}-${REV}-osmc" >> files-source/DEBIAN/control
+        echo "Version: ${VERSION}-${REV}-osmc" >> files-source/DEBIAN/control
+	tee files-image/DEBIAN/postinst << EOF
+#!/bin/sh
+set -e
+
+# Pass maintainer script parameters to hook scripts
+export DEB_MAINT_PARAMS="\$*"
+
+# Tell initramfs builder whether it's wanted
+export INITRD=Yes
+
+test -d /etc/kernel/postinst.d && run-parts --arg="${VERSION}-${REV}-osmc" --arg="/boot/vmlinuz-${VERSION}-${REV}-osmc" /etc/kernel/postinst.d
+
+# OSMC -- make sure any out of tree modules are picked up properly
+/sbin/depmod "${VERSION}-${REV}-osmc"
+
+exit 0
+EOF
+        tee files-image/DEBIAN/postrm << EOF
+#!/bin/sh
+set -e
+
+# Pass maintainer script parameters to hook scripts
+export DEB_MAINT_PARAMS="\$*"
+
+# Tell initramfs builder whether it's wanted
+export INITRD=Yes
+
+test -d /etc/kernel/postrm.d && run-parts --arg="${VERSION}-${REV}-osmc" --arg="/boot/vmlinuz-${VERSION}-${REV}-osmc" /etc/kernel/postrm.d
+exit 0
+EOF
+        tee files-image/DEBIAN/preinst << EOF
+#!/bin/sh
+set -e
+
+# Pass maintainer script parameters to hook scripts
+export DEB_MAINT_PARAMS="\$*"
+
+# Tell initramfs builder whether it's wanted
+export INITRD=Yes
+
+test -d /etc/kernel/preinst.d && run-parts --arg="${VERSION}-${REV}-osmc" --arg="/boot/vmlinuz-${VERSION}-${REV}-osmc" /etc/kernel/preinst.d
+exit 0
+EOF
+        tee files-image/DEBIAN/prerm << EOF
+#!/bin/sh
+set -e
+
+# Pass maintainer script parameters to hook scripts
+export DEB_MAINT_PARAMS="\$*"
+
+# Tell initramfs builder whether it's wanted
+export INITRD=Yes
+
+test -d /etc/kernel/prerm.d && run-parts --arg="${VERSION}-${REV}-osmc" --arg="/boot/vmlinuz-${VERSION}-${REV}-osmc" /etc/kernel/prerm.d
+exit 0
+EOF
+	chmod +x files-image/DEBIAN/post*
+	chmod +x files-image/DEBIAN/pre*
+        fix_arch_ctl "files-image/DEBIAN/control"
+        fix_arch_ctl "files-headers/DEBIAN/control"
+        fix_arch_ctl "files-debug/DEBIAN/control"
+	fix_arch_ctl "files-source/DEBIAN/control"
 	dpkg_build files-image ${1}-image-${VERSION}-${REV}-osmc.deb
-	# Disassemble kernel headers package to include full headers (upstream Debian bug...)
-	if [ "$ARCH" == "armv7l" ]
-	then
-		mkdir -p files-headers/
-		dpkg -x ${1}-headers*.deb files-headers/
-		dpkg-deb -e ${1}-headers*.deb files-headers/DEBIAN
-		rm ${1}-headers*.deb
-		cp -ar src/*linux*/arch/arm/include/ files-headers/usr/src/*-headers-${VERSION}-${REV}-osmc/include
-		dpkg_build files-headers ${1}-headers-${VERSION}-${REV}-osmc.deb
-	fi
+	dpkg_build files-headers ${1}-headers-${VERSION}-${REV}-osmc.deb
+	dpkg_build files-debug ${1}-image-debug-${VERSION}-${REV}-osmc.deb
+	dpkg_build files-source ${1}-source-${VERSION}-${REV}-osmc.deb
 	echo "Package: ${1}-kernel-osmc" >> files/DEBIAN/control
-	echo "Depends: ${1}-image-${VERSION}-${REV}-osmc" >> files/DEBIAN/control
+	if [ "$1" == "vero364" ]
+	then
+		echo "Depends: ${1}-image-${VERSION}-${REV}-osmc, vero3-bootloader-osmc:armhf (>=1.0.0)" >> files/DEBIAN/control
+	else
+		echo "Depends: ${1}-image-${VERSION}-${REV}-osmc" >> files/DEBIAN/control
+	fi
 	fix_arch_ctl "files/DEBIAN/control"
 	dpkg_build files/ ${1}-kernel-${VERSION}-${REV}-osmc.deb
 	build_return=$?
