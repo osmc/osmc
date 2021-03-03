0b857790fc77b908ceb30676d95a47e56b58eb9c
Sam Nazarko <email@samnazarko.co.uk>
2021-03-03 00:01:00 +0000

    [package] [kernel-osmc] Vero 4K: bump version
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

--- package/kernel-osmc/build.sh
+++ package/kernel-osmc/build.sh
@@ -15,7 +15,7 @@ test $1 == rbp2 && VERSION="5.10.3" && REV="1" && FLAGS_INITRAMFS=$(($INITRAMFS_
 test $1 == rbp464 && VERSION="5.10.3" && REV="1" && FLAGS_INITRAMFS=$(($INITRAMFS_BUILD + $INITRAMFS_EMBED)) && IMG_TYPE="zImage"
 test $1 == vero2 && VERSION="3.10.105" && REV="13" && FLAGS_INITRAMFS=$(($INITRAMFS_BUILD)) && IMG_TYPE="uImage"
 test $1 == pc && VERSION="4.2.3" && REV="16" && FLAGS_INITRAMFS=$(($INITRAMFS_BUILD + $INITRAMFS_EMBED)) && IMG_TYPE="zImage"
-test $1 == vero364 && VERSION="4.9.113" && REV="29" && FLAGS_INITRAMFS=$(($INITRAMFS_BUILD)) && IMG_TYPE="zImage"
+test $1 == vero364 && VERSION="4.9.113" && REV="30" && FLAGS_INITRAMFS=$(($INITRAMFS_BUILD)) && IMG_TYPE="zImage"
 if [ $1 == "rbp2" ] || [ $1 == "rbp464" ] || [ $1 == "pc" ]
 then
 	if [ -z $VERSION ]; then echo "Don't have a defined kernel version for this target!" && exit 1; fi
