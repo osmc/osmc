458859af72ef4c0f91275c4a63dc1803993b09ff
Ch1llb0/skin.osmc <36623079+Ch1llb0@users.noreply.github.com>
2021-03-19 21:14:16 +0100

    [package] [mediacenter-osmc-skin] skin.osmc: bump to ce8e283e7514c4494871764ecb47b32878807375; skin.osmc.scope: bump to 243cb3b3fd3805313e91691734d11bae37adcc8f

--- package/mediacenter-skin-osmc/build.sh
+++ package/mediacenter-skin-osmc/build.sh
@@ -20,8 +20,8 @@ then
 fi
 }
 
-REV="8408651067a6db78d5bf314a77e8a86feaa54987"
-SCOPE_REV="cf45b0ec516135e67c7230daa11ead0cc6692780"
+REV="ce8e283e7514c4494871764ecb47b32878807375"
+SCOPE_REV="243cb3b3fd3805313e91691734d11bae37adcc8f"
 echo -e "Building package mediacenter-skin-osmc"
 echo -e "Downloading skin"
 pull_source "https://github.com/osmc/skin.osmc/archive/${REV}.tar.gz" "$(pwd)/src"
