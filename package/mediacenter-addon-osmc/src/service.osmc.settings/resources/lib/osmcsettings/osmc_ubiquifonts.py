2dcc482b89b5b50166362f4fb18bd25e26fc0d3c
anxdpanic <anxdpanic@users.noreply.github.com>
2021-05-26 14:18:05 -0400

    [package] [mediacenter-addon-osmc] Import Ubiqui fonts to all fontsets provided by skin
    - previously these fonts were only added to the first fontset
    - now switching to another font -ie. Arial in skin.osmc- My OSMC will remain consistent

--- package/mediacenter-addon-osmc/src/service.osmc.settings/resources/lib/osmcsettings/osmc_ubiquifonts.py
+++ package/mediacenter-addon-osmc/src/service.osmc.settings/resources/lib/osmcsettings/osmc_ubiquifonts.py
@@ -44,6 +44,14 @@ class UbiquiFonts:
             self.resource_folder, 'lib', 'osmcsettings', 'fonts.txt'
         )
 
+        self.skin_folder = xbmcvfs.translatePath('special://skin')
+        self.log('Skin folder: %s' % self.skin_folder)
+
+        self.skin_font_folder = xbmcvfs.translatePath('special://skin/fonts/')
+
+        self._skin_xml_folder = None
+        self._skin_font_xml = None
+
     @property
     def addon_id(self):
         if not self._addon_id:
@@ -62,23 +70,30 @@ class UbiquiFonts:
             self._window = xbmcgui.Window(10000)
         return self._window
 
-    def _get_addon_folder(self, skin_folder):
+    @property
+    def skin_xml_folder(self):
+        if self._skin_xml_folder:
+            return self._skin_xml_folder
+
         folder = None
         height = None
         width = None
 
         try:
             # first check the addon for the folder location
-            tree = ElementTree.parse(os.path.join(skin_folder, 'addon.xml'))
+            tree = ElementTree.parse(os.path.join(self.skin_folder, 'addon.xml'))
             root = tree.getroot()
 
             for ext in root.iter('extension'):
                 res = ext.find('res')
                 if res is None:
                     continue
+
                 height = res.attrib['height']
                 width = res.attrib['width']
                 folder = res.attrib['folder']
+
+                self.log('Found skin\'s xml folder in addon.xml: %s' % folder)
                 break
 
         except Exception as e:
@@ -88,98 +103,119 @@ class UbiquiFonts:
         # failing that, use the folder search option
         if not folder:
 
-            possible_xml_locations = [('1080i', 1080, 1920), ('720p', 720, 1280),
-                                      ('1080p', 1080, 1920), ('16x9', None, None)]
+            xml_folder_candidates = [('1080i', 1080, 1920), ('720p', 720, 1280),
+                                     ('1080p', 1080, 1920), ('16x9', None, None)]
 
-            for location_folder, location_height, location_width in possible_xml_locations:
-                folder = os.path.join(skin_folder, location_folder)
-                self.log('POSSIBLE XML LOCATION = %s' % folder)
+            for candidate_folder, candidate_height, candidate_width in xml_folder_candidates:
+                folder = os.path.join(self.skin_folder, candidate_folder)
+                self.log('Checking if %s is skin\'s xml folder...' % folder)
                 if not os.path.isdir(folder):
                     continue
 
-                height = location_height
-                width = location_width
+                height = candidate_height
+                width = candidate_width
                 break
 
             else:
-                self.log('FAILED')
-                return
+                self.log('Failed to find skin\'s xml folder.')
+                return None
 
-            self.log('ACTUAL XML LOCATION = %s' % folder)
+            self.log('Found skin\'s xml folder: %s' % folder)
 
         if height and width:
-            self.window.setProperty("SkinHeight", str(height))
-            self.window.setProperty("SkinWidth", str(width))
+            self.window.setProperty('SkinHeight', str(height))
+            self.window.setProperty('SkinWidth', str(width))
 
-        return os.path.join(skin_folder, folder)
+        self._skin_xml_folder = os.path.join(self.skin_folder, folder)
+        self.log('Skin XML folder: %s' % self.skin_xml_folder)
+        return self._skin_xml_folder
+
+    @property
+    def skin_font_xml(self):
+        if self._skin_font_xml:
+            return self._skin_font_xml
+
+        self._skin_font_xml = os.path.join(self.skin_xml_folder, 'Font.xml')
+        self.log('Skin Font.xml: %s' % self._skin_font_xml)
+        return self._skin_font_xml
 
     def import_osmc_fonts(self):
-        skin_folder = xbmcvfs.translatePath('special://skin')
+        self.log('Importing Ubiqui fonts')
 
-        self.log('skin_folder: %s' % skin_folder)
-
-        fonts_folder = os.path.join(skin_folder, 'fonts/')
-
-        xml_folder = self._get_addon_folder(skin_folder)
-        self.log('ACTUAL XML LOCATION = %s' % xml_folder)
-
-        if not xml_folder:
+        if not self.skin_xml_folder:
             return 'failed'
-
-        font_xml = os.path.join(xml_folder, 'Font.xml')
-        self.log('font_xml = %s' % font_xml)
 
         # check whether the fonts are already in the font xml, if they are then simply return.
         # the previous solution of checking for a backup fonts file is pointless as an update
         # of the skin would overwrite the Font.xml and leave the backup in place
         lines = []
-        with open(font_xml, 'r', encoding='utf-8') as open_file:
+        fontset_elements = 0
+        osmc_font_count = 0
+        with open(self.skin_font_xml, 'r', encoding='utf-8') as open_file:
             line = open_file.readline()
-            if 'osmc_addon_XLarge' in line:
-                return 'ubiquited'
-            lines.append(line)
+
+            while line:
+                if '<fontset' in line or '</fontset' in line:
+                    fontset_elements += 1
+                if 'osmc_addon_XLarge' in line:
+                    osmc_font_count += 1
+
+                lines.append(line)
+                line = open_file.readline()
+
+        # fontset elements come in pairs, with a single osmc font block
+        # if fontset elements divided by osmc font count is 2, each fontset has been 'ubiquited'
+        try:
+            completed = (fontset_elements // osmc_font_count) == 2
+        except ZeroDivisionError:
+            completed = False
+
+        if completed:
+            self.log('Ubiqui fonts already exist.')
+            return 'ubiquited'
 
         # copy fonts to skins font folder
-        fonts = set(os.listdir(fonts_folder))
+        fonts = set(os.listdir(self.skin_font_folder))
         unique_files = set(os.listdir(self.font_folder)) - fonts
 
         for filename in unique_files:
-            subprocess.call(["sudo", "cp",
-                             os.path.join(self.font_folder, filename), fonts_folder])
+            subprocess.call([
+                'sudo', 'cp', os.path.join(self.font_folder, filename), self.skin_font_folder
+            ])
 
         with open(self.font_partials, 'r', encoding='utf-8') as open_file:
             font_partials_lines = open_file.readlines()
 
-        with open(font_xml, 'r', encoding='utf-8') as open_file:
+        with open(self.skin_font_xml, 'r', encoding='utf-8') as open_file:
             font_xml_lines = open_file.readlines()
 
-        new_lines = []
+        payload = []
         fonts_added = False
 
         for line in font_xml_lines:
+            if '<fontset' in line:
+                fonts_added = False
+
+            if 'osmc_addon_XLarge' in line:
+                fonts_added = True
 
             if '</fontset>' in line and not fonts_added:
+                payload += font_partials_lines
                 fonts_added = True
-                new_lines += font_partials_lines
-                new_lines.append(line)
 
-            else:
-                new_lines.append(line)
+            payload.append(line)
 
-        # make backup of original Font.xml
-        backup_file = os.path.join(fonts_folder, 'backup_Font.xml')
+        backup_file = os.path.join(self.skin_font_folder, 'backup_Font.xml')
+        self.log('Creating Font.xml backup: %s' % backup_file)
+        subprocess.call(['sudo', 'cp', self.skin_font_xml, backup_file])
 
-        self.log('BACKUP FILE: %s' % backup_file)
+        tmp_font_xml = '/tmp/Font.xml'
+        self.log('Writing temporary Font.xml: %s' % tmp_font_xml)
+        with open(tmp_font_xml, 'w', encoding='utf-8') as open_file:
+            open_file.writelines(payload)
 
-        subprocess.call(["sudo", "cp", font_xml, backup_file])
+        self.log('Moving new Font.xml to skin: %s' % self.skin_font_xml)
+        subprocess.call(['sudo', 'mv', tmp_font_xml, self.skin_font_xml])
 
-        with open('/tmp/Font.xml', 'w', encoding='utf-8') as open_file:
-            open_file.writelines(new_lines)
-
-        subprocess.call(["sudo", "mv", '/tmp/Font.xml', font_xml])
-
+        self.log('Ubiqui fonts import completed.')
         return 'reload_please'
-
-
-if __name__ == "__main__":
-    UbiquiFonts().import_osmc_fonts()
