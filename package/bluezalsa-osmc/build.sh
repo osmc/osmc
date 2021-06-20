b92423fd992d04d05137fe38a31029b5af7044e6
Sam Nazarko <email@samnazarko.co.uk>
2021-06-20 20:56:27 +0100

    [package] [bluez-alsa-osmc] Update package
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

--- package/bluezalsa-osmc/build.sh
+++ package/bluezalsa-osmc/build.sh
@@ -4,8 +4,7 @@
 #!/bin/bash
 
 . ../common.sh
-#REV="49ad348808a15485aa7cb2df0a4d13654cc0cee3"
-REV="de58004c440f34078b6b138f8c7155ae36b26a10"
+REV="b09f373ea7dbc6e3ecbcb74d7299f5230cdc6e59"
 pull_source "https://github.com/Arkq/bluez-alsa/archive/${REV}.tar.gz" "$(pwd)/src"
 if [ $? != 0 ]; then echo -e "Error fetching bluez-alsa source" && exit 1; fi
 # Build in native environment
@@ -28,20 +27,15 @@ then
 	handle_dep "pkg-config"
 	sed '/Package/d' -i files/DEBIAN/control
 	echo "Package: ${1}-bluezalsa-osmc" >> files/DEBIAN/control
-	pushd src
-	SOURCE=$(ls)
-	pushd $SOURCE
-	install_patch "../../patches" "all"
-	# aclocal
-	autoreconf --install
-	mkdir build && pushd build
-	../configure --enable-aac --with-alsaplugindir=/usr/lib/arm-linux-gnueabihf/alsa-lib/ --with-alsaconfdir=/usr/share/alsa/alsa.conf.d
+	pushd src/bluez-alsa*
+	autoreconf -vif .
+	./configure --enable-aac --with-alsaplugindir=/usr/lib/arm-linux-gnueabihf/alsa-lib/ --with-alsaconfdir=/usr/share/alsa/alsa.conf.d
 	if [ $? != 0 ]; then echo -e "Configure failed!" && umount /proc/ > /dev/null 2>&1 && exit 1; fi
 	$BUILD
 	if [ $? != 0 ]; then echo -e "Build failed!" && exit 1; fi
 	make install DESTDIR=${out}
-	popd; popd; popd
-	strip_files "${out}"
+	popd
+	#strip_files "${out}"
 	fix_arch_ctl "files/DEBIAN/control"
 	dpkg_build files/ ${1}-bluezalsa-osmc.deb
 	build_return=$?
