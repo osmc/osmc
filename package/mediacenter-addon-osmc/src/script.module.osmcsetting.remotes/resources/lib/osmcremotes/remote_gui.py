b29abc2ff610647b2f4bb104a7419badc1318bcd
Ch1llb0/skin.osmc <36623079+Ch1llb0@users.noreply.github.com>
2021-11-23 11:47:45 +0100

    [package] [mediacenter-addon-osmc] Convert all labels to be translatable

--- package/mediacenter-addon-osmc/src/script.module.osmcsetting.remotes/resources/lib/osmcremotes/remote_gui.py
+++ package/mediacenter-addon-osmc/src/script.module.osmcsetting.remotes/resources/lib/osmcremotes/remote_gui.py
@@ -204,7 +204,7 @@ class RemoteGui(xbmcgui.WindowXMLDialog):
         return self._path
 
     def onInit(self):
-        self.getControl(8).setLabel(self.lang(32014))  # Disable RC6
+        self.getControl(8).setLabel(self.lang(32012))  # Disable RC6
         self.getControl(62).setLabel(self.lang(32003))  # Browse
         self.getControl(7).setLabel(self.lang(32004))  # Exit
 
