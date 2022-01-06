4a1aa8dbbf3f0e76fab7e431b0263dd897d8d0bd
Sam Nazarko <email@samnazarko.co.uk>
2022-01-06 04:07:28 +0000

    [package] [bluez-osmc] Tidy up ./configure
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

--- package/bluez-osmc/build.sh
+++ package/bluez-osmc/build.sh
@@ -44,7 +44,7 @@ then
 	echo "Package: ${1}-bluez-osmc" >> files/DEBIAN/control
 	pushd src/bluez-$VERSION
     	install_patch "../../patches" "all"
-	./configure --prefix=/usr --sysconfdir=/etc --localstatedir=/var --with-udevdir=/lib/udev --with-systemdsystemunitdir=/lib/systemd/system --with-systemduserunitdir=/etc/systemd/user --enable-sixaxis
+	./configure --prefix=/usr --sysconfdir=/etc --localstatedir=/var --with-udevdir=/lib/udev --with-systemdsystemunitdir=/lib/systemd/system --with-systemduserunitdir=/etc/systemd/user --enable-sixaxis --disable-obex --disable-cups --enable-udev --disable-manpages
 	if [ $? != 0 ]; then echo -e "Configure failed!" && umount /proc/ > /dev/null 2>&1 && exit 1; fi
 	$BUILD
 	if [ $? != 0 ]; then echo -e "Build failed!" && exit 1; fi
