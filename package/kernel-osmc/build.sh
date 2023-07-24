6926c2b41acdda5ae142efc183cdb04dbe5a2275
Sam Nazarko <email@samnazarko.co.uk>
2023-07-25 00:56:51 +0100

    [package] [kernel-osmc] Raspberry Pi: update to 5.15.92; add support for BR_NETFILTER on RBP4
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

--- package/kernel-osmc/build.sh
+++ package/kernel-osmc/build.sh
@@ -24,8 +24,8 @@ fi
 }
 
 . ../common.sh
-test $1 == rbp2 && VERSION="5.15.83" && REV="3" && FLAGS_INITRAMFS=$(($INITRAMFS_BUILD + $INITRAMFS_EMBED)) && IMG_TYPE="zImage" && SIGN_KERNEL=0
-test $1 == rbp464 && VERSION="5.15.83" && REV="2" && FLAGS_INITRAMFS=$(($INITRAMFS_BUILD + $INITRAMFS_EMBED)) && IMG_TYPE="zImage" && SIGN_KERNEL=0
+test $1 == rbp2 && VERSION="5.15.92" && REV="1" && FLAGS_INITRAMFS=$(($INITRAMFS_BUILD + $INITRAMFS_EMBED)) && IMG_TYPE="zImage" && SIGN_KERNEL=0
+test $1 == rbp464 && VERSION="5.15.92" && REV="1" && FLAGS_INITRAMFS=$(($INITRAMFS_BUILD + $INITRAMFS_EMBED)) && IMG_TYPE="zImage" && SIGN_KERNEL=0
 test $1 == vero364 && VERSION="4.9.269" && REV="31" && FLAGS_INITRAMFS=$(($INITRAMFS_BUILD)) && IMG_TYPE="zImage" && SIGN_KERNEL=0
 test $1 == vero564 && VERSION="4.9.269" && REV="33" && FLAGS_INITRAMFS=$(($INITRAMFS_BUILD)) && IMG_TYPE="zImage" && SIGN_KERNEL=1
 if [ $1 == "rbp2" ] || [ $1 == "rbp464" ]
