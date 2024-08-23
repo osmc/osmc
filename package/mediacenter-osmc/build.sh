4ff472f8619a548f5289cc81d5f9b11eaee67252
Sam Nazarko <email@samnazarko.co.uk>
2024-08-23 16:37:14 +0100

    [package] [mediacenter-osmc] Bump to v21.1 final
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

--- package/mediacenter-osmc/build.sh
+++ package/mediacenter-osmc/build.sh
@@ -7,7 +7,7 @@
 
 if [ "$1" == "rbp2" ] || [ "$1" == "rbp4" ] || [ "$1" == "vero3" ] || [ "$1" == "vero5" ]
 then
-pull_source "https://github.com/xbmc/xbmc/archive/8adee68acddd62d915a3ab7b0279a71c8ddb6da0.tar.gz" "$(pwd)/src"
+pull_source "https://github.com/xbmc/xbmc/archive/183eb85f108c3d8b44a578de80f575f27afe9b9a.tar.gz" "$(pwd)/src"
 API_VERSION="21"
 else
 pull_source "https://github.com/xbmc/xbmc/archive/master.tar.gz" "$(pwd)/kodi"
