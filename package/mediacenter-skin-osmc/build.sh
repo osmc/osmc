1fd1d2d4d164b5042892ddda11be0300218746b1
Sam Nazarko <email@samnazarko.co.uk>
2024-09-14 20:08:27 +0100

    [package] [mediacenter-skin-osmc] Add 4:3 and 21:9 skins
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

--- package/mediacenter-skin-osmc/build.sh
+++ package/mediacenter-skin-osmc/build.sh
@@ -22,6 +22,8 @@ fi
 
 REV="8be827eafef0e100e05dc00b86929cf911edf22b"
 SCOPE_REV="a2a91416c852a6eca5a4940a7c478b92989b091b"
+FOURTOTHREE_REV="35301b1403a1ea545f64084d50caadf5ef4254d3"
+TWENTYONETONINE_REV="168364ed6cfbc355b7e049ec72f0dfe8072da551"
 echo -e "Building package mediacenter-skin-osmc"
 echo -e "Downloading skin"
 pull_source "https://github.com/osmc/skin.osmc/archive/${REV}.tar.gz" "$(pwd)/src"
@@ -29,6 +31,14 @@ if [ $? != 0 ]; then echo -e "Error downloading classic skin" && exit 1; fi
 process_skin "$REV" "skin.osmc"
 echo -e "Downloading scope skin"
 pull_source "https://github.com/osmc/skin.osmc/archive/${SCOPE_REV}.tar.gz" "$(pwd)/src"
-if [ $? != 0 ]; then echo -e "Error downloading classic skin" && exit 1; fi
+if [ $? != 0 ]; then echo -e "Error downloading scope skin" && exit 1; fi
 process_skin "$SCOPE_REV" "skin.osmc.scope"
+echo -e "Downloading 4:3 skin"
+pull_source "https://github.com/osmc/skin.osmc/archive/${FOURTOTHREE_REV}.tar.gz" "$(pwd)/src"
+if [ $? != 0 ]; then echo -e "Error downloading 4:3 skin" && exit 1; fi
+process_skin "$FOURTOTHREE_REV" "skin.osmc.4to3"
+echo -e "Downloading 21:9 skin"
+pull_source "https://github.com/osmc/skin.osmc/archive/${TWENTYONETONINE_REV}.tar.gz" "$(pwd)/src"
+if [ $? != 0 ]; then echo -e "Error downloading 21:9 skin" && exit 1; fi
+process_skin "$TWENTYONETONINE_REV" "skin.osmc.21to9"
 dpkg_build files/ mediacenter-skin-osmc.deb
