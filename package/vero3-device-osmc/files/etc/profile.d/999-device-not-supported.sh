24f1972d87da0eeed3ad54a2eec27bcaebb90278
Sam Nazarko <email@samnazarko.co.uk>
2025-02-28 00:32:05 +0000

    [package] [vero3-device-osmc] Add deprecation message to shell login
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

---
+++ package/vero3-device-osmc/files/etc/profile.d/999-device-not-supported.sh
@@ -0,0 +1,8 @@
+rt() {
+echo -e "\033[31m$1\033[0m"
+}
+
+rt "Notice: This device is no longer officially supported as of 31/01/2025."
+rt "As a result, you will no longer receive officialy supported updates on this device."
+rt "You may wish to exercise caution using apt to perform upgrades."
+rt "For more information: please see https://osmc.news/QTupI"
